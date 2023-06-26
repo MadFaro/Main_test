14919755cf4e5b296f2f778f1067873b72072f82
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-06-26 10:18:33 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,25 +1,40 @@
 import vosk
 import soundfile as sf
 import numpy as np
+import os
+import concurrent.futures
 
-model_path = r'C:\Users\TologonovAB\Desktop\audio_totext\vosk-model-ru-0.22'
-audio_file = 'audio\\audio.wav'
+model_path = r'vosk-model-ru-0.22'
+audio_directory = 'audio/'
 
-def find_words(audio_file , model_path):
+def load_audio_data(audio_file):
     audio_data, sample_rate = sf.read(audio_file)
     left_channel = audio_data[:, 0]
-    #left_channel_bytes = (left_channel * np.iinfo(np.int16).max).astype(np.int16).tobytes()
-    sf.write('audio\\audio2.wav', left_channel, samplerate=sample_rate)
-    with open('audio\\audio2.wav', 'rb') as file:
-        data = file.read()
+    left_channel_bytes = (left_channel * np.iinfo(np.int16).max).astype(np.int16).tobytes()
+    return left_channel_bytes, sample_rate
 
-    model = vosk.Model(model_path)
+def process_audio_file(audio_file, model):
+    audio_data, sample_rate = load_audio_data(audio_file)
     recognizer = vosk.KaldiRecognizer(model, sample_rate)
-    recognizer.AcceptWaveform(data)
+    recognizer.AcceptWaveform(audio_data)
     result = recognizer.FinalResult()
-
     return result
 
-found_words = find_words(audio_file=audio_file, model_path=model_path)
+def find_words(audio_files, model):
+    results = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for audio_file in audio_files:
+            future = executor.submit(process_audio_file, audio_file, model)
+            futures.append(future)
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            results.append(result)
+    return results
 
-print(found_words)
+if __name__ == '__main__':
+    model = vosk.Model(model_path)
+    audio_files = [os.path.join(audio_directory, filename) for filename in os.listdir(audio_directory) if filename.endswith('.wav')]
+    found_words = find_words(audio_files, model)
+    print(found_words)
+
