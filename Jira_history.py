36a5416105b3dae9d13fd9b2817658523169f0b1
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-10 14:42:18 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -3,6 +3,7 @@ import soundfile as sf
 import numpy as np
 import os
 import concurrent.futures
+import nltk
 
 model_path = r'vosk-model-ru-0.22'
 audio_directory = 'audio/'
@@ -13,19 +14,21 @@ def load_audio_data(audio_file):
     left_channel_bytes = (left_channel * np.iinfo(np.int16).max).astype(np.int16).tobytes()
     return left_channel_bytes, sample_rate
 
-def process_audio_file(audio_file, model):
+def process_audio_file(audio_file, model, phrases):
     audio_data, sample_rate = load_audio_data(audio_file)
     recognizer = vosk.KaldiRecognizer(model, sample_rate)
     recognizer.AcceptWaveform(audio_data)
     result = recognizer.FinalResult()
-    return result
+    text = result['text']
+    check_phrases(text, phrases)
+    return {'audio_file': audio_file, 'text': text, 'found_phrases': check_phrases(text, phrases)}
 
-def find_words(audio_files, model):
+def find_words(audio_files, model, phrases):
     results = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
         for audio_file in audio_files:
-            future = executor.submit(process_audio_file, audio_file, model)
+            future = executor.submit(process_audio_file, audio_file, model, phrases)
             futures.append(future)
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
@@ -35,6 +38,6 @@ def find_words(audio_files, model):
 if __name__ == '__main__':
     model = vosk.Model(model_path)
     audio_files = [os.path.join(audio_directory, filename) for filename in os.listdir(audio_directory) if filename.endswith('.wav')]
-    found_words = find_words(audio_files, model)
+    phrases = ["пример текста", "мы будем", "не найдена"]
+    found_words = find_words(audio_files, model, phrases)
     print(found_words)
-
