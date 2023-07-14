e4bc5d59d073937f06a9a6df8a481d8055ab605e
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-14 16:11:38 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,29 +1,37 @@
 Sub РаскраситьРезультаты()
     Dim rng As Range
     Dim dataRange As Range
+    Dim cell As Range
     Dim rowCount As Long
     Dim greenCount As Long
     Dim redCount As Long
     Dim yellowCount As Long
+    Dim minValue As Double
+    Dim maxValue As Double
     
     ' Выбор диапазона, в котором нужно раскрасить результаты
     Set rng = Selection
-    Set dataRange = rng.Value
+    Set dataRange = rng
     rowCount = dataRange.Rows.Count
     
-    ' Вычисление количества строк для каждого цвета
+    ' Определение минимального и максимального значения в выбранном диапазоне
+    minValue = WorksheetFunction.Min(dataRange)
+    maxValue = WorksheetFunction.Max(dataRange)
+    
+    ' Вычисление пороговых значений для каждого цвета
     greenCount = Round(rowCount * 0.3)
     redCount = Round(rowCount * 0.3)
     yellowCount = rowCount - greenCount - redCount
     
     ' Проверка и раскраска каждой строки в соответствии с процентами
-    For i = 1 To rowCount
-        If i <= greenCount Then
-            rng.Rows(i).Interior.Color = RGB(0, 255, 0) ' Зеленый цвет
-        ElseIf i > rowCount - redCount Then
-            rng.Rows(i).Interior.Color = RGB(255, 0, 0) ' Красный цвет
+    For Each cell In dataRange
+        If cell.Value >= maxValue - (maxValue - minValue) * 0.3 Then
+            cell.Interior.Color = RGB(255, 0, 0) ' Красный цвет
+        ElseIf cell.Value <= minValue + (maxValue - minValue) * 0.3 Then
+            cell.Interior.Color = RGB(0, 255, 0) ' Зеленый цвет
         Else
-            rng.Rows(i).Interior.Color = RGB(255, 255, 0) ' Желтый цвет
+            cell.Interior.Color = RGB(255, 255, 0) ' Желтый цвет
         End If
-    Next i
+    Next cell
 End Sub
+
