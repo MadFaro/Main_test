99b125a30f506810f69d20e2ac6c989bf2010d5a
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-15 01:37:15 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -5,27 +5,35 @@ Sub РаскраситьРезультаты()
     Dim rowCount As Long
     Dim percentile30 As Double
     Dim percentile70 As Double
+    Dim i As Long
     
     ' Выбор диапазона, в котором нужно раскрасить результаты
     Set rng = Selection
     Set dataRange = rng
     rowCount = dataRange.Rows.Count
     
-    ' Вычисление значений, соответствующих 30% лучшим и 30% худшим результатам
-    percentile30 = WorksheetFunction.Percentile(dataRange, 0.3)
-    percentile70 = WorksheetFunction.Percentile(dataRange, 0.7)
-    
     ' Проверка и раскраска каждой ячейки в соответствии с процентным значением
     For Each cell In dataRange
-        If cell.Value >= percentile70 Then
-            cell.Interior.Color = RGB(255, 0, 0) ' Красный цвет
-        ElseIf cell.Value <= percentile30 Then
-            cell.Interior.Color = RGB(0, 255, 0) ' Зеленый цвет
-        Else
-            cell.Interior.Color = RGB(255, 255, 0) ' Желтый цвет
+        If cell.Offset(0, -1).Value = "Опытный" Then
+            percentile30 = WorksheetFunction.PercentileIf(dataRange.Offset(0, 1), cell.Value, "<=" & "Опытный")
+            percentile70 = WorksheetFunction.PercentileIf(dataRange.Offset(0, 1), cell.Value, "<=" & "Опытный")
+        ElseIf cell.Offset(0, -1).Value = "Новичок" Then
+            percentile30 = WorksheetFunction.PercentileIf(dataRange.Offset(0, 1), cell.Value, "<=" & "Новичок")
+            percentile70 = WorksheetFunction.PercentileIf(dataRange.Offset(0, 1), cell.Value, "<=" & "Новичок")
         End If
+        
+        For i = 1 To rowCount
+            If cell.Value >= percentile70 Then
+                cell.Interior.Color = RGB(255, 0, 0) ' Красный цвет
+            ElseIf cell.Value <= percentile30 Then
+                cell.Interior.Color = RGB(0, 255, 0) ' Зеленый цвет
+            Else
+                cell.Interior.Color = RGB(255, 255, 0) ' Желтый цвет
+            End If
+        Next i
     Next cell
 End Sub
+
 
 
 
