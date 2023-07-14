c126c4b48b6b6a9ace3e6f5ac31bf9249aeed661
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-14 16:24:39 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -3,39 +3,29 @@ Sub РаскраситьРезультаты()
     Dim dataRange As Range
     Dim cell As Range
     Dim rowCount As Long
-    Dim greenCount As Long
-    Dim redCount As Long
-    Dim yellowCount As Long
-    Dim sortedRange As Range
     Dim percentile30 As Double
+    Dim percentile70 As Double
     
     ' Выбор диапазона, в котором нужно раскрасить результаты
     Set rng = Selection
     Set dataRange = rng
     rowCount = dataRange.Rows.Count
     
-    ' Определение числа строк, соответствующего 30% лучших значений
-    greenCount = Round(rowCount * 0.3)
+    ' Вычисление значений, соответствующих 30% лучшим и 30% худшим результатам
+    percentile30 = WorksheetFunction.Percentile(dataRange, 0.3)
+    percentile70 = WorksheetFunction.Percentile(dataRange, 0.7)
     
-    ' Сортировка диапазона по возрастанию
-    Set sortedRange = dataRange.Resize(, 1).Offset(, 1)
-    sortedRange.Value = dataRange.Value
-    sortedRange.Sort Key1:=sortedRange, Order1:=xlAscending, Header:=xlNo
-    
-    ' Определение значения, соответствующего 30% лучших значений
-    percentile30 = sortedRange.Cells(greenCount).Value
-    
-    ' Проверка и раскраска каждой строки в соответствии с процентным значением
+    ' Проверка и раскраска каждой ячейки в соответствии с процентным значением
     For Each cell In dataRange
-        If cell.Value >= percentile30 Then
+        If cell.Value >= percentile70 Then
             cell.Interior.Color = RGB(255, 0, 0) ' Красный цвет
-        ElseIf cell.Value < percentile30 Then
+        ElseIf cell.Value <= percentile30 Then
             cell.Interior.Color = RGB(0, 255, 0) ' Зеленый цвет
+        Else
+            cell.Interior.Color = RGB(255, 255, 0) ' Желтый цвет
         End If
     Next cell
-    
-    ' Очистка временного диапазона, используемого для сортировки
-    sortedRange.Clear
 End Sub
+
 
 
