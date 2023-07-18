109be500fdf2f3cba51a53769b8644783bbf366d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-19 02:11:28 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -7,17 +7,17 @@ Sub color_value_rating_division()
     Set rngFlags = selectedRange.Areas(1).Columns(1)
     
     Dim valuesRange As Range
-    If selectedRange.Areas.count > 1 Then
+    If selectedRange.Areas.Count > 1 Then
         Set valuesRange = selectedRange.Areas(2)
     Else
-        Set valuesRange = selectedRange.Offset(0, 1).Resize(selectedRange.Rows.count, selectedRange.Columns.count - 1)
+        Set valuesRange = selectedRange.Offset(0, 1).Resize(selectedRange.Rows.Count, selectedRange.Columns.Count - 1)
     End If
     
     Dim rowCount As Long
     Dim columnCount As Long
     
-    rowCount = valuesRange.Rows.count
-    columnCount = valuesRange.Columns.count
+    rowCount = valuesRange.Rows.Count
+    columnCount = valuesRange.Columns.Count
     
     Dim percentilesExp() As Variant
     Dim percentilesNov() As Variant
@@ -39,14 +39,14 @@ Sub color_value_rating_division()
         
         Dim i As Long
         For i = 1 To rowCount
-            If rngFlags.Cells(i, 1).value = "Опытный" Then
+            If rngFlags.Cells(i, 1).Value = "Опытный" Then
                 expCount = expCount + 1
                 ReDim Preserve expValues(1 To expCount) As Double
-                expValues(expCount) = currentColumn.Cells(i, 1).value
-            ElseIf rngFlags.Cells(i, 1).value = "Новичок" Then
+                expValues(expCount) = currentColumn.Cells(i, col).Value
+            ElseIf rngFlags.Cells(i, 1).Value = "Новичок" Then
                 novCount = novCount + 1
                 ReDim Preserve novValues(1 To novCount) As Double
-                novValues(novCount) = currentColumn.Cells(i, 1).value
+                novValues(novCount) = currentColumn.Cells(i, col).Value
             End If
         Next i
 
@@ -72,7 +72,7 @@ Sub color_value_rating_division()
     For col = 1 To columnCount
         For i = 1 To rowCount
             Dim flagValue As String
-            flagValue = rngFlags.Cells(i, 1).value
+            flagValue = rngFlags.Cells(i, 1).Value
             
             If flagValue = "Опытный" Then
                 ColorCell valuesRange.Cells(i, col), percentilesExp(col, 1), percentilesExp(col, 2)
@@ -82,92 +82,5 @@ Sub color_value_rating_division()
         Next i
     Next col
 End Sub
-Function GetPercentile(sortedValues() As Double, percentile As Double) As Double
-    Dim count As Long
-    
-    If Not IsEmpty(sortedValues) Then
-        count = Application.WorksheetFunction.count(sortedValues)
-    Else
-        count = 0
-    End If
-    
-    If count = 0 Then
-        GetPercentile = 0
-        Exit Function
-    End If
-    
-    Dim percentileIndex As Double
-    percentileIndex = Application.WorksheetFunction.Floor((percentile * count) + 1, 1)
-    
-    Dim k As Long
-    k = CLng(percentileIndex)
-    
-    If k >= count Then
-        GetPercentile = sortedValues(count)
-    ElseIf k <= 1 Then
-        GetPercentile = sortedValues(1)
-    Else
-        Dim d As Double
-        d = percentileIndex - k
-        
-        GetPercentile = sortedValues(k) + d * (sortedValues(k + 1) - sortedValues(k))
-    End If
-End Function
-
-Sub ColorCell(ByVal cell As Range, ByVal percentile30 As Double, ByVal percentile70 As Double)
-    Dim value As Double
-    value = cell.value
-
-    If value >= percentile70 Then
-        cell.Interior.Color = RGB(146, 208, 80)
-    ElseIf value <= percentile30 Then
-        cell.Interior.Color = RGB(255, 0, 0)
-    Else
-        cell.Interior.Color = RGB(255, 255, 0)
-    End If
-End Sub
-
-Sub QuickSort(arr() As Double, ByVal left As Long, ByVal right As Long)
-    Dim i As Long
-    Dim j As Long
-    Dim pivot As Double
-    Dim temp As Double
-    
-    i = left
-    j = right
-    
-    If left >= right Then Exit Sub
-    
-    If left + 1 = right Then
-        If arr(left) > arr(right) Then
-            temp = arr(left)
-            arr(left) = arr(right)
-            arr(right) = temp
-        End If
-        Exit Sub
-    End If
-    
-    pivot = arr((left + right) \ 2)
-    
-    While i <= j
-        While arr(i) < pivot
-            i = i + 1
-        Wend
-        While arr(j) > pivot
-            j = j - 1
-        Wend
-        If i <= j Then
-            temp = arr(i)
-            arr(i) = arr(j)
-            arr(j) = temp
-            i = i + 1
-            j = j - 1
-        End If
-    Wend
-    
-    If left < j Then QuickSort arr, left, j
-    If i < right Then QuickSort arr, i, right
-End Sub
-
 
 
