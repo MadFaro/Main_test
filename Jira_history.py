c7bbc0e0411ab79e067ce0c8b497db6813a66835
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-19 02:05:11 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,26 +1,42 @@
-Function GetPercentile(sortedValues() As Double, percentile As Double) As Double
-    Dim count As Long
-    count = UBound(sortedValues) - LBound(sortedValues) + 1
+Sub QuickSort(arr() As Double, ByVal left As Long, ByVal right As Long)
+    Dim i As Long
+    Dim j As Long
+    Dim pivot As Double
+    Dim temp As Double
     
-    If count = 0 Then
-        GetPercentile = 0 ' Значение по умолчанию для пустого массива
-        Exit Function
+    i = left
+    j = right
+    
+    If left >= right Then Exit Sub ' Дополнительная проверка, если left >= right
+    
+    If left + 1 = right Then
+        If arr(left) > arr(right) Then
+            temp = arr(left)
+            arr(left) = arr(right)
+            arr(right) = temp
+        End If
+        Exit Sub
     End If
     
-    Dim percentileIndex As Double
-    percentileIndex = Application.WorksheetFunction.Floor((percentile * count), 1)
+    pivot = arr((left + right) \ 2)
     
-    Dim k As Long
-    k = CLng(percentileIndex)
+    While i <= j
+        While arr(i) < pivot
+            i = i + 1
+        Wend
+        While arr(j) > pivot
+            j = j - 1
+        Wend
+        If i <= j Then
+            temp = arr(i)
+            arr(i) = arr(j)
+            arr(j) = temp
+            i = i + 1
+            j = j - 1
+        End If
+    Wend
     
-    If k >= count Then
-        GetPercentile = sortedValues(count)
-    ElseIf k <= 1 Then
-        GetPercentile = sortedValues(1)
-    Else
-        Dim d As Double
-        d = percentileIndex - k
-        
-        GetPercentile = sortedValues(k) + d * (sortedValues(k + 1) - sortedValues(k))
-    End If
-End Function
+    If left < j Then QuickSort arr, left, j
+    If i < right Then QuickSort arr, i, right
+End Sub
+
