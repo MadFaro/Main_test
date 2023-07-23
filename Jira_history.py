233e7fd65445a53d1ded1e4e845fd3e0e4eccb1f
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-07-23 13:06:32 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -3,19 +3,16 @@ Option Explicit
 Dim networkPath
 networkPath = "\\server\share\путь\к\файлу.xlsx" ' Замените на путь к вашему файлу на сетевом диске
 
-Dim fso, file
-Set fso = CreateObject("Scripting.FileSystemObject")
+Dim objWMIService, colLocks, objLock
+Set objWMIService = GetObject("winmgmts:\\.\root\CIMV2")
 
-' Пытаемся открыть файл на чтение
-On Error Resume Next
-Set file = fso.OpenTextFile(networkPath, 1, False) ' 1 - режим только чтение
-On Error GoTo 0
+' Получаем список блокировок для файла
+Set colLocks = objWMIService.ExecQuery("SELECT * FROM Win32_SMBShare WHERE Name = '" & networkPath & "'")
 
-If Err.Number = 0 Then
+If colLocks.Count > 0 Then
+    ' Файл заблокирован другим пользователем
+    WScript.Echo "Файл заблокирован для редактирования другим пользователем."
+Else
     ' Файл доступен для редактирования
     WScript.Echo "Файл доступен для редактирования."
-    file.Close
-Else
-    ' Файл заблокирован для редактирования
-    WScript.Echo "Файл заблокирован для редактирования."
 End If
