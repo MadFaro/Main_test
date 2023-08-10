dd07f9e03588c741a0c9989587d08851e0884ea6
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-10 15:48:34 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -7,17 +7,17 @@ Sub color_value_rating_division()
     Set rngFlags = selectedRange.Areas(1).Columns(1)
     
     Dim valuesRange As Range
-    If selectedRange.Areas.count > 1 Then
+    If selectedRange.Areas.Count > 1 Then
         Set valuesRange = selectedRange.Areas(2)
     Else
-        Set valuesRange = selectedRange.Offset(0, 1).Resize(selectedRange.Rows.count, selectedRange.Columns.count - 1)
+        Set valuesRange = selectedRange.Offset(0, 1).Resize(selectedRange.Rows.Count, selectedRange.Columns.Count - 1)
     End If
     
     Dim rowCount As Long
     Dim columnCount As Long
     
-    rowCount = valuesRange.Rows.count
-    columnCount = valuesRange.Columns.count
+    rowCount = valuesRange.Rows.Count
+    columnCount = valuesRange.Columns.Count
     
     Dim percentilesExp() As Variant
     Dim percentilesNov() As Variant
@@ -25,6 +25,11 @@ Sub color_value_rating_division()
     ReDim percentilesNov(1 To columnCount, 1 To 2) As Variant
     
     Dim col As Long
+    Dim expCountPerColumn() As Long
+    Dim novCountPerColumn() As Long
+    ReDim expCountPerColumn(1 To columnCount) As Long
+    ReDim novCountPerColumn(1 To columnCount) As Long
+    
     For col = 1 To columnCount
         Dim currentColumn As Range
         Set currentColumn = valuesRange.Columns(col)
@@ -39,14 +44,16 @@ Sub color_value_rating_division()
         
         Dim i As Long
         For i = 1 To rowCount
-            If rngFlags.Cells(i, 1).value = "Îïûòíûé" Then
+            If rngFlags.Cells(i, 1).Value = "Îïûòíûé" Then
                 expCount = expCount + 1
                 ReDim Preserve expValues(1 To expCount) As Double
-                expValues(expCount) = currentColumn.Cells(i, 1).value
-            ElseIf rngFlags.Cells(i, 1).value = "Íîâè÷îê" Then
+                expValues(expCount) = currentColumn.Cells(i, 1).Value
+                expCountPerColumn(col) = expCountPerColumn(col) + 1
+            ElseIf rngFlags.Cells(i, 1).Value = "Íîâè÷îê" Then
                 novCount = novCount + 1
                 ReDim Preserve novValues(1 To novCount) As Double
-                novValues(novCount) = currentColumn.Cells(i, 1).value
+                novValues(novCount) = currentColumn.Cells(i, 1).Value
+                novCountPerColumn(col) = novCountPerColumn(col) + 1
             End If
         Next i
 
@@ -72,95 +79,27 @@ Sub color_value_rating_division()
     For col = 1 To columnCount
         For i = 1 To rowCount
             Dim flagValue As String
-            flagValue = rngFlags.Cells(i, 1).value
+            flagValue = rngFlags.Cells(i, 1).Value
+            Dim value As Double
+            value = valuesRange.Cells(i, col).Value
             
-            If flagValue = "Îïûòíûé" Then
-                ColorCell valuesRange.Cells(i, col), percentilesExp(col, 1), percentilesExp(col, 2)
-            ElseIf flagValue = "Íîâè÷îê" Then
-                ColorCell valuesRange.Cells(i, col), percentilesNov(col, 1), percentilesNov(col, 2)
+            If expCountPerColumn(col) > novCountPerColumn(col) Then
+                If flagValue = "Îïûòíûé" Then
+                    ColorCell valuesRange.Cells(i, col), percentilesExp(col, 1), percentilesExp(col, 2)
+                ElseIf flagValue = "Íîâè÷îê" Then
+                    ColorCell valuesRange.Cells(i, col), percentilesNov(col, 1), percentilesNov(col, 2)
+                End If
+            ElseIf expCountPerColumn(col) < novCountPerColumn(col) Then
+                If flagValue = "Îïûòíûé" Then
+                    ColorCell valuesRange.Cells(i, col), percentilesExp(col, 1), percentilesExp(col, 2)
+                ElseIf flagValue = "Íîâè÷îê" Then
+                    ColorCell valuesRange.Cells(i, col), percentilesNov(col, 1), percentilesNov(col, 2)
+                End If
+            Else
+                ColorCell valuesRange.Cells(i, col), 0, 1
             End If
         Next i
     Next col
 End Sub
-Function GetPercentile(sortedValues() As Double, percentile As Double) As Double
-    Dim count As Long
-    
-    On Error Resume Next
-    count = UBound(sortedValues) - LBound(sortedValues) + 1
-    On Error GoTo 0
-    
-    If count <= 0 Then
-        GetPercentile = 0
-        Exit Function
-    End If
-    
-    Dim percentileIndex As Double
-    percentileIndex = percentile * count
-    Dim k As Long
-    k = CLng(percentileIndex)
-    
-    If k >= count Then
-        GetPercentile = sortedValues(count)
-    ElseIf k <= 1 Then
-        GetPercentile = sortedValues(1)
-    Else
-        Dim d As Double
-        d = percentileIndex - k
-        
-        GetPercentile = sortedValues(k) + d * (sortedValues(k + 1) - sortedValues(k))
-    End If
-End Function
 
-Sub ColorCell(ByVal cell As Range, ByVal percentile30 As Double, ByVal percentile70 As Double)
-    Dim value As Double
-    value = cell.value
-    If value > percentile70 Then
-        cell.Interior.Color = RGB(146, 208, 80)
-    ElseIf value < percentile30 Then
-        cell.Interior.Color = RGB(255, 0, 0)
-    Else
-        cell.Interior.Color = RGB(255, 255, 0)
-    End If
-End Sub
-
-Sub QuickSort(arr() As Double, ByVal left As Long, ByVal right As Long)
-    Dim i As Long
-    Dim j As Long
-    Dim pivot As Double
-    Dim temp As Double
-    
-    i = left
-    j = right
-    
-    If left >= right Then Exit Sub
-    
-    If left + 1 = right Then
-        If arr(left) > arr(right) Then
-            temp = arr(left)
-            arr(left) = arr(right)
-            arr(right) = temp
-        End If
-        Exit Sub
-    End If
-    
-    pivot = arr((left + right) \ 2)
-    
-    While i <= j
-        While arr(i) < pivot
-            i = i + 1
-        Wend
-        While arr(j) > pivot
-            j = j - 1
-        Wend
-        If i <= j Then
-            temp = arr(i)
-            arr(i) = arr(j)
-            arr(j) = temp
-            i = i + 1
-            j = j - 1
-        End If
-    Wend
-    
-    If left < j Then QuickSort arr, left, j
-    If i < right Then QuickSort arr, i, right
-End Sub
+' Остальной код остается без изменений
