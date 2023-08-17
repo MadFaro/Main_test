ed7a29cbcbd4ec155380210463c94e11a146360a
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-17 09:03:12 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,29 +1,12 @@
-import wave
-import numpy as np
-
-# Открываем WAV-файл для чтения
-with wave.open('your_file.wav', 'rb') as wav_file:
-    # Используем стандартные предположения
-    num_channels = 2  # Стерео
-    sample_width = 2  # 16 бит
-    frame_rate = 44100  # 44.1 кГц
-
-    # Чтение аудио данных
-    audio_data = bytearray()
-    chunk_size = 4096  # Размер блока для чтения
-
-    while True:
-        chunk = wav_file.readframes(chunk_size)
-        if not chunk:
-            break
-        audio_data.extend(chunk)
-
-# Преобразовываем байты в массив numpy
-audio_data = np.frombuffer(audio_data, dtype=np.int16)
-
-# Переформатируем данные в массив с двумя каналами
-num_samples = len(audio_data) // num_channels
-audio_data = np.reshape(audio_data, (num_samples, num_channels))
-
-# В этом месте вы можете работать с переменной audio_data,
-# которая содержит числовые данные аудио с предполагаемыми параметрами
+Traceback (most recent call last):
+  File "2.py", line 5, in <module>
+    with wave.open('240002039936997.wav', 'rb') as wav_file:
+  File "C:\Python38\lib\wave.py", line 510, in open
+    return Wave_read(f)
+  File "C:\Python38\lib\wave.py", line 164, in __init__
+    self.initfp(f)
+  File "C:\Python38\lib\wave.py", line 144, in initfp
+    self._read_fmt_chunk(chunk)
+  File "C:\Python38\lib\wave.py", line 269, in _read_fmt_chunk
+    raise Error('unknown format: %r' % (wFormatTag,))
+wave.Error: unknown format: 41216
