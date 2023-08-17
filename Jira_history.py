b8bff5dab9a595e8ba831ad4bcaae3db0b9c9b9b
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-17 08:56:38 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,11 +1,5 @@
-import soundfile as sf
-data, _ = sf.read('240002039934923.wav', channels=2, samplerate=8000)
-
-print(data)
-
-Traceback (most recent call last):
-  File "1.py", line 6, in <module>
-    data, _ = sf.read('240002039934923.wav', channels=2, samplerate=8000)
+  File "2.py", line 5, in <module>
+    data, _ = soundfile.read('240002039936997.wav', channels=2, samplerate=8000)
   File "C:\Python38\lib\site-packages\soundfile.py", line 285, in read
     with SoundFile(file, 'r', samplerate, channels,
   File "C:\Python38\lib\site-packages\soundfile.py", line 656, in __init__
@@ -13,4 +7,3 @@ Traceback (most recent call last):
   File "C:\Python38\lib\site-packages\soundfile.py", line 1483, in _create_info_struct
     raise TypeError("Not allowed for existing files (except 'RAW'): "
 TypeError: Not allowed for existing files (except 'RAW'): samplerate, channels, format, subtype, endian
-
