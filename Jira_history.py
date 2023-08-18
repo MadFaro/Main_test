d6e68038d66f2ad55f20ed6c47e12909767a43cd
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-18 11:10:36 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,8 +1,11 @@
-WARNING (VoskAPI:CheckMemoryUsage():determinize-lattice-pruned.cc:316) Did not reach requested beam in determinize-lattice: size exceeds maximum 50000000 bytes; (repo,arcs,elems) = (52002944,80864,63576), after rebuilding, repo size was 49319712, effective beam was 5.96073 vs. requested beam 6
-LOG (VoskAPI:RebuildRepository():determinize-lattice-pruned.cc:283) Rebuilding repository.
-WARNING (VoskAPI:CheckMemoryUsage():determinize-lattice-pruned.cc:316) Did not reach requested beam in determinize-lattice: size exceeds maximum 50000000 bytes; (repo,arcs,elems) = (50405440,74464,57648), after rebuilding, repo size was 47855520, effective beam was 5.66235 vs. requested beam 6
-LOG (VoskAPI:RebuildRepository():determinize-lattice-pruned.cc:283) Rebuilding repository.
-WARNING (VoskAPI:CheckMemoryUsage():determinize-lattice-pruned.cc:316) Did not reach requested beam in determinize-lattice: size exceeds maximum 50000000 bytes; (repo,arcs,elems) = (50113472,71488,55272), after rebuilding, repo size was 47694752, effective beam was 5.4613 vs. requested beam 6
-LOG (VoskAPI:RebuildRepository():determinize-lattice-pruned.cc:283) Rebuilding repository.
-LOG (VoskAPI:RebuildRepository():determinize-lattice-pruned.cc:283) Rebuilding repository.
-WARNING (VoskAPI:CheckMemoryUsage():determinize-lattice-pruned.cc:316) Did not reach requested beam in determinize-lattice: size exceeds maximum 50000000 bytes; (repo,arcs,elems) = (50015168,143552,116016), after rebuilding, repo size was 41095040, effective beam was 5.59155 vs. requested beam 6
+def check_phrases(text, phrases):
+    normal = pymorphy2.MorphAnalyzer()
+    tokens = nltk.word_tokenize(text)
+    normal_tokens = [normal.parse(token)[0].normal_form for token in tokens]
+    found_phrases = []
+    for phrase in phrases:
+        phrase_tokens = nltk.word_tokenize(phrase)
+        normal_phrase = [normal.parse(token)[0].normal_form for token in phrase_tokens]
+        if all(token in normal_tokens for token in normal_phrase):
+            found_phrases.append(phrase)
+    return found_phrases
