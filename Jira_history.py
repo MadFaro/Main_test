604c362f2de27755534a891176a561619293e108
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-18 11:16:13 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,11 +1,11 @@
 def check_phrases(text, phrases):
     normal = pymorphy2.MorphAnalyzer()
-    tokens = nltk.word_tokenize(text)
-    normal_tokens = [normal.parse(token)[0].normal_form for token in tokens]
+    normal_text = [normal.parse(token)[0].normal_form for token in nltk.word_tokenize(text)]
     found_phrases = []
+    
     for phrase in phrases:
-        phrase_tokens = nltk.word_tokenize(phrase)
-        normal_phrase = [normal.parse(token)[0].normal_form for token in phrase_tokens]
-        if all(token in normal_tokens for token in normal_phrase):
+        normal_phrase = [normal.parse(token)[0].normal_form for token in nltk.word_tokenize(phrase)]
+        if ' '.join(normal_phrase) in ' '.join(normal_text):
             found_phrases.append(phrase)
+    
     return found_phrases
