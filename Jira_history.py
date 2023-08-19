4a595f17a5a2e59219b14b17f972b4d86b2d1dcf
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-19 15:41:09 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,15 +1,34 @@
-  File "Nemo.py", line 22, in <module>
-    transcriptions = asr_model.transcribe([audio])
-  File "C:\Python38\lib\site-packages\torch\utils\_contextlib.py", line 115, in decorate_context
-    return func(*args, **kwargs)
-  File "C:\Python38\lib\site-packages\nemo\collections\asr\models\ctc_models.py", line 183, in transcribe
-    fp.write(json.dumps(entry) + '\n')
-  File "C:\Python38\lib\json\__init__.py", line 231, in dumps
-    return _default_encoder.encode(obj)
-  File "C:\Python38\lib\json\encoder.py", line 199, in encode
-    chunks = self.iterencode(o, _one_shot=True)
-  File "C:\Python38\lib\json\encoder.py", line 257, in iterencode
-    return _iterencode(o, 0)
-  File "C:\Python38\lib\json\encoder.py", line 179, in default
-    raise TypeError(f'Object of type {o.__class__.__name__} '
-TypeError: Object of type ndarray is not JSON serializable
+import nemo
+import nemo.collections.asr as nemo_asr
+import json
+import librosa
+import os
+
+# Путь к сохраненной модели
+model_checkpoint = 'путь_к_сохраненной_модели.nemo'
+
+# Инициализация NeMo ASR модели с использованием сохраненной модели
+asr_model = nemo_asr.models.EncDecCTCModel.restore_from(model_checkpoint)
+
+# Путь к аудиофайлу
+audio_file = 'путь_к_аудиофайлу.wav'
+
+# Загрузка аудиофайла
+if os.path.exists(audio_file):
+    audio, _ = librosa.load(audio_file, sr=16000)
+else:
+    print("Аудиофайл не найден.")
+    exit(1)
+
+# Преобразование массива NumPy в список Python
+audio_list = audio.tolist()
+
+# Распознавание речи
+transcriptions = asr_model.transcribe([audio_list])
+
+# Сохранение результатов в JSON
+output_file = 'результат.json'
+with open(output_file, 'w') as f:
+    json.dump(transcriptions, f)
+
+print("Результаты сохранены в", output_file)
