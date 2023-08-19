6cca412bfad29574a1ab065b1b87572ac3e6824e
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-19 16:33:37 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,26 +1,105 @@
-TologonovAB\Desktop\nemo_model\stt_ru_conformer_ctc_large.nemo.
-Transcribing:   0%|                                                                              | 0/1 [00:01<?, ?it/s]
-Traceback (most recent call last):
-  File "Nemo.py", line 4, in <module>
-    transcriptions = quartznet.transcribe(paths2audio_files=wave_file)
-  File "C:\Python38\lib\site-packages\torch\utils\_contextlib.py", line 115, in decorate_context
-    return func(*args, **kwargs)
-  File "C:\Python38\lib\site-packages\nemo\collections\asr\models\ctc_models.py", line 197, in transcribe
-    for test_batch in tqdm(temporary_datalayer, desc="Transcribing", disable=not verbose):
-  File "C:\Python38\lib\site-packages\tqdm\std.py", line 1178, in __iter__
-    for obj in iterable:
-  File "C:\Python38\lib\site-packages\torch\utils\data\dataloader.py", line 633, in __next__
-    data = self._next_data()
-  File "C:\Python38\lib\site-packages\torch\utils\data\dataloader.py", line 677, in _next_data
-    data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
-  File "C:\Python38\lib\site-packages\torch\utils\data\_utils\fetch.py", line 54, in fetch
-    return self.collate_fn(data)
-  File "C:\Python38\lib\site-packages\nemo\core\classes\common.py", line 1089, in __call__
-    instance._attach_and_validate_output_types(
-  File "C:\Python38\lib\site-packages\nemo\core\classes\common.py", line 353, in _attach_and_validate_output_types
-    self.__attach_neural_type(res, metadata, depth=0, name=out_types_list[ind][0])
-  File "C:\Python38\lib\site-packages\nemo\core\classes\common.py", line 441, in __attach_neural_type
-    raise TypeError(
-TypeError: Output shape mismatch occured for audio_signal in module AudioToBPEDataset :
-Output shape expected = (batch, time) |
-Output shape found : torch.Size([1, 3152160, 2])
+import glob, vosk, os, nltk, pymorphy2, time
+import soundfile as sf
+import numpy as np
+import pandas as pd
+
+
+
+
+# Получаем весь список файлов их папки
+folder_path = r'C:\Users\TologonovAB\Desktop\text\audio\audio'
+file_pattern = '*.wav'
+file_list = glob.glob(os.path.join(folder_path, file_pattern))
+
+# Загружаем модель и обозначем фразы для поиска
+model = vosk.Model('vosk-model-ru-0.22/')
+phrases_l = ["выиграть автомобиль", 
+             "подарок автомобиль", 
+             "приз автомобиль",
+             "от банка автомобиль"
+             "автомобиль подарок",
+             "автомобиль в подарок",
+             "автомобиль главный приз",
+             "выбрать автомобиль",
+             "автомобиль от банка",
+             "акция автомобиль",
+             "акция машина",
+             ]
+phrases_r = [""]
+
+# Функция для поиска фраз
+def check_phrases(text, phrases):
+    normal = pymorphy2.MorphAnalyzer()
+    tokens = nltk.word_tokenize(text)
+    normal_tokens = [normal.parse(token)[0].normal_form for token in tokens]
+    found_phrases = []
+    for phrase in phrases:
+        phrase_tokens = nltk.word_tokenize(phrase)
+        normal_phrase = [normal.parse(token)[0].normal_form for token in phrase_tokens]
+        if all(token in normal_tokens for token in normal_phrase):
+            found_phrases.append(phrase)
+    return found_phrases
+
+# Запускаем основной цикл
+for file in file_list:
+    results = [] # Пустой словарь для записи
+    start_time = time.time() # обозначаем начало выполнения цикла
+    audio_data, sample_rate = sf.read(file) # Читаем файл
+    left_channel = audio_data[:, 0] # Берем левый канал - то что говорит оператор
+    right_channel = audio_data[:, 1] # Берем правый канал - то что говорит клиент
+
+# Выравниваем громкость для левого канала
+    max_value_l = np.max(np.abs(left_channel))
+    if max_value_l > 0:
+        normalize_audio_l = left_channel / max_value_l
+    else:
+        normalize_audio_l = left_channel
+
+# Выравниваем громкость для правого канала
+    max_value_r = np.max(np.abs(right_channel))
+    if max_value_r > 0:
+        normalize_audio_r = right_channel / max_value_r
+    else:
+        normalize_audio_r = right_channel
+
+# Сохраняем каналы
+    sf.write(f'audio/left/{os.path.basename(file)}', normalize_audio_l, sample_rate) # Сохраняем левый канал
+    sf.write(f'audio/right/{os.path.basename(file)}', normalize_audio_r, sample_rate) # Сохраняем правый канал
+
+# Инициализация модели
+    recognizer = vosk.KaldiRecognizer(model, sample_rate)
+
+# Прогоняем левый канал через модельку и ищем фразы
+    with open(f'audio/left/{os.path.basename(file)}', 'rb') as file_l:
+        data_l = file_l.read()
+    recognizer.AcceptWaveform(data_l)
+    text_l = recognizer.FinalResult()
+    found_phrases_l = check_phrases(text_l, phrases_l)
+
+# Прогоняем правый канал через модельку и ищем фразы
+    with open(f'audio/right/{os.path.basename(file)}', 'rb') as file_r:
+        data_r = file_r.read()
+    recognizer.AcceptWaveform(data_r)
+    text_r = recognizer.FinalResult()
+#    found_phrases_r = check_phrases(text_r, phrases_r)
+
+# Записываем результат в словарь
+    end_time = time.time()
+    result_time = end_time - start_time
+    results.append({
+        'audio_file_name': os.path.basename(file), 
+        'text_operator': text_l, 
+        'found_phrases_operator': found_phrases_l,
+        'text_client': text_r, 
+#       'found_phrases_client': found_phrases_r,
+        'time_second' : result_time   
+        })
+
+# Чистим папки с аудио
+    os.remove(file)
+    os.remove(f'audio/left/{os.path.basename(file)}')
+    os.remove(f'audio/right/{os.path.basename(file)}')
+
+# Записываем данные в CSV файл
+    df = pd.DataFrame(results)
+    df.to_csv('text.csv', mode='a', header=False, index=False, encoding='ANSI', lineterminator='\r\n', sep=';')
