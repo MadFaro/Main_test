115048af13fd603c33a22b2d5596c1b3c8b43a08
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-19 22:05:32 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,4 @@
 import librosa
-from pydub import AudioSegment
 
 # Путь к вашему аудиофайлу
 audio_file_path = 'path_to_your_audio_file.wav'
@@ -7,13 +6,10 @@ audio_file_path = 'path_to_your_audio_file.wav'
 # Значение порога длительности паузы в секундах
 pause_threshold = 0.5
 
-# Загрузка аудиофайла и преобразование в формат, понятный librosa
-audio = AudioSegment.from_file(audio_file_path)
-samples = audio.raw_data
-sample_rate = audio.frame_rate
+# Загрузка аудиофайла с помощью librosa
+samples, sample_rate = librosa.load(audio_file_path, sr=None)
 
 # Обнаружение временных точек начала голосовых фрагментов
-samples, sample_rate = librosa.load(audio_file_path, sr=None)
 onset_frames = librosa.onset.onset_detect(y=samples, sr=sample_rate)
 
 # Путь к папке для сохранения результатов
@@ -24,12 +20,8 @@ for i in range(len(onset_frames) - 1):
     start_sample = librosa.frames_to_samples(onset_frames[i])
     end_sample = librosa.frames_to_samples(onset_frames[i + 1])
     
-    segment = AudioSegment(
-        data=samples[start_sample:end_sample],
-        sample_width=audio.sample_width,
-        frame_rate=sample_rate,
-        channels=audio.channels
-    )
+    segment = samples[start_sample:end_sample]
     
-    if len(segment) > pause_threshold * 1000:  # Исключаем короткие паузы
-        segment.export(f'{output_path}segment_{i + 1}.wav', format='wav')
+    if len(segment) > int(pause_threshold * sample_rate):  # Исключаем короткие паузы
+        librosa.output.write_wav(f'{output_path}segment_{i + 1}.wav', segment, sample_rate)
+
