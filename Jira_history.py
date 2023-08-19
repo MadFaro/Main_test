a207dc917b51635c4dd19470b6ff893baf6d2fdc
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-19 22:07:57 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,6 +1,28 @@
-Traceback (most recent call last):
-  File "wisper.py", line 26, in <module>
-    librosa.output.write_wav(f'{output_path}segment_{i + 1}.wav', segment, sample_rate)
-  File "C:\Python38\lib\site-packages\lazy_loader\__init__.py", line 89, in __getattr__
-    raise AttributeError(f"No {package_name} attribute {name}")
-AttributeError: No librosa attribute output
+import librosa
+import soundfile as sf
+
+# Путь к вашему аудиофайлу
+audio_file_path = 'path_to_your_audio_file.wav'
+
+# Значение порога длительности паузы в секундах
+pause_threshold = 0.5
+
+# Загрузка аудиофайла с помощью librosa
+samples, sample_rate = librosa.load(audio_file_path, sr=None)
+
+# Обнаружение временных точек начала голосовых фрагментов
+onset_frames = librosa.onset.onset_detect(y=samples, sr=sample_rate)
+
+# Путь к папке для сохранения результатов
+output_path = 'output_directory/'
+
+# Создание отдельных аудиофайлов для каждого голосового фрагмента
+for i in range(len(onset_frames) - 1):
+    start_sample = librosa.frames_to_samples(onset_frames[i])
+    end_sample = librosa.frames_to_samples(onset_frames[i + 1])
+    
+    segment = samples[start_sample:end_sample]
+    
+    if len(segment) > int(pause_threshold * sample_rate):  # Исключаем короткие паузы
+        output_file_path = f'{output_path}segment_{i + 1}.wav'
+        sf.write(output_file_path, segment, sample_rate)
