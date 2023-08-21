5a804ea55d7dd2c1a00b36045bb6ca66de1cabed
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-22 00:01:44 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,33 +1,20 @@
-import librosa
-import numpy as np
-import os
+import torch
+from transformers import pipeline
+from datasets import load_dataset
 
-input_audio_path = "input_audio.mp3"
-output_directory = "output_segments"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# Создаем папку для сегментов, если её нет
-os.makedirs(output_directory, exist_ok=True)
+pipe = pipeline(
+  "automatic-speech-recognition",
+  model="openai/whisper-tiny",
+  chunk_length_s=30,
+  device=device,
+)
 
-segment_duration = 30  # Длительность сегмента в секундах
-silence_threshold = 0.01  # Порог амплитуды для определения паузы
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+sample = ds[0]["audio"]
 
-# Загружаем аудио
-y, sr = librosa.load(input_audio_path, sr=None)
+prediction = pipe(sample.copy(), batch_size=8)["text"]
 
-# Находим индексы, где амплитуда меньше порога (паузы)
-pause_indices = np.where(y < silence_threshold)[0]
-
-# Создаем сегменты на основе пауз
-segments = []
-start_idx = 0
-for pause_idx in pause_indices:
-    if pause_idx - start_idx >= sr * segment_duration:
-        segments.append(y[start_idx:pause_idx])
-        start_idx = pause_idx + 1
-
-# Сохраняем сегменты
-for idx, segment in enumerate(segments):
-    output_path = os.path.join(output_directory, f"segment_{idx:03d}.wav")
-    librosa.output.write_wav(output_path, segment, sr=sr)
-
-print("Сегментация завершена.")
+# we can also return timestamps for the predictions
+prediction = pipe(sample.copy(), batch_size=8, return_timestamps=True)["chunks"]
