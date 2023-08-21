3946a0561346ac68a1610fac920bae568bfb28f4
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-21 22:22:55 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,29 +1,33 @@
-from pydub import AudioSegment
-from pydub.playback import play
+import librosa
+import numpy as np
+import os
 
-input_audio = AudioSegment.from_file("input_audio.mp3")
-segment_length = 30 * 1000  # 30 секунд в миллисекундах
+input_audio_path = "input_audio.mp3"
+output_directory = "output_segments"
 
+# Создаем папку для сегментов, если её нет
+os.makedirs(output_directory, exist_ok=True)
+
+segment_duration = 30  # Длительность сегмента в секундах
+silence_threshold = 0.01  # Порог амплитуды для определения паузы
+
+# Загружаем аудио
+y, sr = librosa.load(input_audio_path, sr=None)
+
+# Находим индексы, где амплитуда меньше порога (паузы)
+pause_indices = np.where(y < silence_threshold)[0]
+
+# Создаем сегменты на основе пауз
 segments = []
-
-i = 0
-while i < len(input_audio):
-    segment = input_audio[i:i+segment_length]
-
-    # Найдем индекс паузы в текущем сегменте
-    silence_threshold = -40  # Порог тишины в децибелах
-    pause_idx = segment.rms <= silence_threshold
-
-    if pause_idx != -1:
-        # Обрежем сегмент до индекса паузы
-        segment = segment[:pause_idx+1]
-
-    segments.append(segment)
-    i += len(segment)
+start_idx = 0
+for pause_idx in pause_indices:
+    if pause_idx - start_idx >= sr * segment_duration:
+        segments.append(y[start_idx:pause_idx])
+        start_idx = pause_idx + 1
 
 # Сохраняем сегменты
 for idx, segment in enumerate(segments):
-    output_path = f"output_segments/segment_{idx:03d}.mp3"
-    segment.export(output_path, format="mp3")
+    output_path = os.path.join(output_directory, f"segment_{idx:03d}.wav")
+    librosa.output.write_wav(output_path, segment, sr=sr)
 
 print("Сегментация завершена.")
