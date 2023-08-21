56283d3ab875daa401235bbf0cb3c81a53b386b5
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-22 01:50:01 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,20 +1,16 @@
-from pyAudioAnalysis import audioBasicIO, audioSegmentation
-import soundfile as sf
-
-# Загрузка аудиофайла
-audio_file = 'путь_к_вашему_аудиофайлу.wav'
-signal, sample_rate, _ = audioBasicIO.read_audio_file(audio_file)
-
-# Выполнение VAD
-segments = audioSegmentation.silence_removal(signal, sample_rate, 0.02, 0.02, smooth_window=1.0, weight=0.3)
-
-# Создание нового аудиофайла без пауз
-filtered_signal = []
-for segment in segments:
-    start_sample = int(segment[0] * sample_rate)
-    end_sample = int(segment[1] * sample_rate)
-    filtered_signal.extend(signal[start_sample:end_sample])
-
-# Сохранение отфильтрованных данных в новый аудиофайл
-output_file = 'путь_к_вашему_выходному_аудиофайлу.wav'
-sf.write(output_file, filtered_signal, sample_rate)
+import wave
+import struct
+ 
+ 
+def break_the_silence():
+    source = wave.open("in.wav", mode="rb")
+    dest = wave.open("out.wav", mode="wb")
+    dest.setparams(source.getparams())
+    frames_count = source.getnframes()
+    data = struct.unpack("<" + str(frames_count) + "h",
+                         source.readframes(frames_count))
+    newdata = list(filter(lambda x: abs(x) > 5, data))
+    newframes = struct.pack("<" + str(len(newdata)) + "h", *newdata)
+    dest.writeframes(newframes)
+    source.close()
+    dest.close()
