95d605ade4fb3f429b5a7a8a443c98aef1b586eb
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-21 19:50:08 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,48 +1,20 @@
-ffmpeg -i input_audio.mp3 -f segment -segment_time 30 -c copy output_segments/segment_%03d.mp3
-
-texts = []
-
-bw_ext_audio, sr = librosa.load(bw_file, sr = 16_000)
-
-current_prompt = None
-current_prefix = None
-
-for i, r in df.iterrows():
-    
-    bw_ext_segment = bw_ext_audio[int(r["start"]*sr) : int(r["end"]*sr)]
-    #results = model.transcribe(bw_ext_segment, language = "it")
-    results = model.transcribe(bw_ext_segment, 
-                               language = "it", 
-                               prompt = current_prompt,
-                               prefix = current_prefix,
-                               condition_on_previous_text = True)
-    
-    text = results["text"]
-    
-    current_prompt = text
-    current_prefix = text
-
-    texts.append(text)
-
-df["text"] = texts
-
-import torch
 from transformers import pipeline
-from datasets import load_dataset
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
+# Инициализация пайплайна для автоматического распознавания речи
 pipe = pipeline(
   "automatic-speech-recognition",
   model="openai/whisper-large-v2",
-  chunk_length_s=30,
-  device=device,
+  chunk_length_s=30
 )
 
-ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-sample = ds[0]["audio"]
+# Путь к аудиофайлу
+audio_path = "path_to_audio_file.wav"
 
-prediction = pipe(sample.copy(), batch_size=8)["text"]
+# Выполнение распознавания речи с разбивкой на сегменты по 30 секунд
+results = pipe(audio_path)
 
-# we can also return timestamps for the predictions
-prediction = pipe(sample.copy(), batch_size=8, return_timestamps=True)["chunks"]
+# Обработка результатов
+for i, result in enumerate(results):
+    transcript = result["alternatives"][0]["transcript"]
+    print(f"Segment {i+1}: {transcript}")
+
