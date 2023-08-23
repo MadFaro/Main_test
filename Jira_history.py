71c13ee569728f0116cdce62b69b769f2f782587
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-23 22:52:24 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,13 +1,4 @@
-import subprocess
-
-# Входной и выходной файлы
-input_file = 'input.mp3'
-output_file = 'output.mp3'
-
-# Команда FFmpeg для увеличения громкости в два раза
-volume_command = ['ffmpeg', '-i', input_file, '-af', 'volume=2.0', output_file]
-compand_command = ['ffmpeg', '-i', input_file, '-af', 'compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0', output_file]
-# Запуск команды с помощью subprocess
-subprocess.run(volume_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-print("Готово!")
+ffmpeg -i test.wav -af "equalizer=f=3000:width_type=h:width=200:g=5" test2.wav
+ffmpeg -i test.wav -af "highpass=f=100,highshelf=f=1000:width_type=o:w=200:g=5" test2.wav
+ffmpeg -i test.wav -af "dynaudnorm" test2.wav
+ffmpeg -i test.wav -af "acompressor" test2.wav
