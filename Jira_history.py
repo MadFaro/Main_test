0fc601ea225658d48d6f50afce542a661c9a8598
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-23 23:19:32 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,4 +1,1 @@
-ffmpeg -i test.wav -af "equalizer=f=3000:width_type=h:width=200:g=5" test2.wav
-ffmpeg -i test.wav -af "highpass=f=100,highshelf=f=1000:width_type=o:w=200:g=5" test2.wav
-ffmpeg -i test.wav -af "dynaudnorm" test2.wav
-ffmpeg -i test.wav -af "acompressor" test2.wav
+ffmpeg -i input.mp3 -filter:a "atempo=0.8" output_slower.mp3
