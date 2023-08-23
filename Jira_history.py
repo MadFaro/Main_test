64d01af142b00897f1dc2db4d7d03aff4345be8d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-23 12:02:32 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -6,15 +6,16 @@ audio_data, sample_rate = sf.read(file)
 left_channel = audio_data[:, 0]
 right_channel = audio_data[:, 1]
 
-# Нормализация каналов
-max_value_l = np.max(np.abs(left_channel))
-normalize_audio_l = left_channel / max_value_l if max_value_l > 0 else left_channel
+# RMS нормализация для левого канала
+rms_left = np.sqrt(np.mean(left_channel**2))
+normalized_audio_l = left_channel / rms_left
 
-max_value_r = np.max(np.abs(right_channel))
-normalize_audio_r = right_channel / max_value_r if max_value_r > 0 else right_channel
+# RMS нормализация для правого канала
+rms_right = np.sqrt(np.mean(right_channel**2))
+normalized_audio_r = right_channel / rms_right
 
 # Объединение нормализованных каналов обратно
-normalized_audio_data = np.column_stack((normalize_audio_l, normalize_audio_r))
+normalized_audio_data = np.column_stack((normalized_audio_l, normalized_audio_r))
 
 # Сохранение аудио данных в том же формате
 output_file = "normalized_audio.wav"
