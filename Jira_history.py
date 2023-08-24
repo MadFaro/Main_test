443dcdaed9464a66d9a8d4fdef36f0f01ae21d78
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-25 02:21:17 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,3 +1,4 @@
 ffmpeg -i test.wav -filter:a loudnorm -ar 8000 -c:a pcm_s16le test2.wav
+ffmpeg -i test.wav -filter:a loudnorm -ar 8000 -c:a pcm_mulaw -b:a 128k -ac 2 test2.wav
 ffmpeg -i test2.wav -af "volume=3.5" test3.wav
 ffmpeg -i test3.wav -filter:a "atempo=0.95" test4.wav
