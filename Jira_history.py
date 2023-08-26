927f2e78f5d8772262aeb86913ddd22542805013
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-26 17:10:31 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -5,7 +5,7 @@ ffmpeg -i test3.wav -filter:a "atempo=0.95" test4.wav
 
 
 ffmpeg -i input.wav -ar 16000 -acodec pcm_s16le output.wav
-ffmpeg -i input.wav -af "highpass=f=200, lowpass=f=3000" output.wav
-ffmpeg -i input.wav -af "volume=2.0" output.wav
-ffmpeg -i input.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output.wav
-ffmpeg -i input.wav -af "crystalizer" output.wav
+ffmpeg -i output.wav -af "highpass=f=200, lowpass=f=3000" output1.wav
+ffmpeg -i output1.wav -af "volume=2.0" output2.wav
+ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
+ffmpeg -i output3.wav -af "crystalizer" output4.wav
