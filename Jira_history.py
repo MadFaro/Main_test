86b3028567964022a69b3bd73bb5fde2efe86de5
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-08-31 16:51:30 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -9,3 +9,4 @@ ffmpeg -i output.wav -af "highpass=f=300, lowpass=f=3000" output1.wav
 ffmpeg -i output1.wav -af "volume=1.5" output2.wav
 ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
+=ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
