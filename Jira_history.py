f6f8a7b21d48cf360cf5519fe64c7643e31b6122
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-09-04 14:13:51 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -12,3 +12,67 @@ ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
 
 SUBSTR('Лимит по карте *1234 увеличен до 60000', INSTR('Лимит по карте *1234 увеличен до 60000', 'до ') + LENGTH('до ')) AS TextAfterDо
+
+import psycopg2
+import pandas as pd
+import datetime
+
+# Подключение к базе данных
+conn = psycopg2.connect(
+host = 'rc1c-fhrb9f1e0l9g611h.mdb.yandexcloud.net',
+port = 6432,
+dbname = 'hr-analytics',
+sslmode = 'require',
+user = 'analytics',
+password = 'HRanalytics'
+)
+
+# Создание курсора для выполнения запросов
+cur = conn.cursor()
+
+# Запрос на получение названий столбцов таблицы orders
+cur.execute('''
+with r as (
+select user_id
+,count (*)
+from orders
+group by user_id
+having count(*) >= 3
+),
+x as (
+select *
+,lead (ord.shipped_at) over (partition by ord.store_id order by ord.shipped_at) as next_time
+from orders as ord
+join r on r.user_id = ord.user_id
+),
+q as (
+select x.*
+,date_part('hour', x.next_time - x.shipped_at ) as hour_diff
+from x
+),
+a as (
+select
+store_id
+,avg(hour_diff) as avg_hour
+from q
+group by store_id
+)
+select store_id, avg_hour
+from a
+where avg_hour = (select max(avg_hour) from a)
+''')
+
+col_names = [desc[0] for desc in cur.description]
+
+# Получение результатов запроса
+sql_query = cur.fetchall()
+# Закрытие курсора и соединения с базой данных
+cur.close()
+conn.close()
+
+# Вывод названий столбцов
+
+#print(sql_query)
+
+df = pd.DataFrame(sql_query, columns=col_names)
+df.to_csv (r'G:\Test\2.csv', sep=';', encoding='utf-8-sig')
