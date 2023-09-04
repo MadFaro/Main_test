8fd880752b910ae1aa01ca6379471cd4fb2738e5
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-09-04 18:17:25 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,3 +1,4 @@
+
 ffmpeg -i test.wav -filter:a loudnorm -ar 8000 -c:a pcm_s16le test2.wav
 ffmpeg -i test.wav -filter:a loudnorm -ar 8000 -c:a pcm_s16le -b:a 128k -ac 2 test2.wav
 ffmpeg -i test2.wav -af "volume=3.5" test3.wav
@@ -10,3 +11,11 @@ ffmpeg -i output1.wav -af "volume=1.5" output2.wav
 ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
+
+
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.google.com/')
