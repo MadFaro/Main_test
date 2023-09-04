9b89e293b46b18470b465693181864c24519c5fa
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-09-04 15:05:44 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -12,33 +12,3 @@ ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
 
 SUBSTR('Лимит по карте *1234 увеличен до 60000', INSTR('Лимит по карте *1234 увеличен до 60000', 'до ') + LENGTH('до ')) AS TextAfterDо
-
-import psycopg2
-import pandas as pd
-import datetime
-
-# Подключение к базе данных
-conn = psycopg2.connect(
-host = '',
-port = ,
-database = '',
-user = '',
-password = ''
-)
-
-# Создание курсора для выполнения запросов
-cur = conn.cursor()
-
-# Запрос на получение названий столбцов таблицы orders
-cur.execute('''
-SELECT * FROM link_uralsib_in.mv_main_metrics
-''')
-
-col_names = [desc[0] for desc in cur.description]
-
-sql_query = cur.fetchall()
-cur.close()
-conn.close()
-
-df = pd.DataFrame(sql_query, columns=col_names)
-df.to_csv('2.csv', sep=';', encoding='utf-8-sig')
