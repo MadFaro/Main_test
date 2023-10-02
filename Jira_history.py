667a6e4c4d68d6c1f02d01691fbbb5cd9aa1647d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-10-02 11:42:18 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -13,41 +13,10 @@ ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
 
 
-import tkinter as tk
-from tkinter import filedialog
+# Преобразуйте столбец 'Дата и время' в формат datetime
+df['Дата и время'] = pd.to_datetime(df['Дата и время'])
 
-def open_file_dialog():
-    initial_dir = "C:/Users/tolog/Desktop/schedulerapp"  # Замените на нужный путь к папке
-    file_path = filedialog.askopenfilename(initialdir=initial_dir)
-    if file_path:
-        file_label.config(text="Выбранный файл: " + file_path)
-        selected_month = month_var.get()
-        month_label.config(text="Выбранный месяц: " + selected_month)
-    else:
-        file_label.config(text="Файл не выбран")
-        month_label.config(text="Месяц не выбран")
-
-app = tk.Tk()
-app.title("Выбор файла и месяца")
-app.geometry("400x250")  
-app.resizable(False, False)  
-
-open_button = tk.Button(app, text="Выбрать файл", command=open_file_dialog)
-open_button.pack(pady=20)
-
-file_label = tk.Label(app, text="Файл не выбран")
-file_label.pack()
-
-# Добавляем выпадающий список с месяцами
-months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-month_var = tk.StringVar()
-month_var.set(months[0])  # Устанавливаем начальное значение на Январь
-month_option = tk.OptionMenu(app, month_var, *months)
-month_option.pack()
-
-month_label = tk.Label(app, text="Месяц не выбран")
-month_label.pack()
-
-app.mainloop()
+# Измените формат даты в столбце на "YY-MM-DD HH:mm:ss"
+df['Дата и время'] = df['Дата и время'].dt.strftime('%y-%m-%d %H:%M:%S')
 
 
