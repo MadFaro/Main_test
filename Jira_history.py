cccf2ce3cdf1de32f81048fe290bd832320d964c
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-10-11 11:23:21 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -34,5 +34,36 @@ def browser_chrome():
         t = el.find_all('td')
         print(f'{t[0].text}|{t[1].text}|{t[2].text}')
 
-browser_chrome()
+browser_chrome()import webbrowser, selenium, time, datetime
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
 
+now1 = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%d')
+now2 = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%m')
+now3 = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y')
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x935')
+
+driver = webdriver.Chrome(executable_path="C:\\Users\\atologonov\\Desktop\\chromedriver.exe", chrome_options=options)
+driver.get('http://newclaim.euroset.lan/admin/report')
+
+id_box = driver.find_element_by_name('usr_id')
+id_box.send_keys('27771314')
+id_box2 = driver.find_element_by_name('password')
+id_box2.send_keys('inehetyYY5678!')
+driver.find_element_by_css_selector("button.btn.btn-primary.btn-block.btn-flat").click()
+time.sleep(15)
+
+select = Select(driver.find_element_by_name('type'))
+select.select_by_value('5')
+id_box = driver.find_element_by_name('range')
+time.sleep(3)
+id_box.send_keys('01.' + now2 + '.' + now3 + ' - ' + now1 + '.' + now2 + '.' + now3)
+time.sleep(3)
+driver.find_element_by_css_selector("button.applyBtn.btn.btn-sm.btn-success").click()
+driver.find_element_by_css_selector("button.btn.btn-success").click()
+time.sleep(5)
+
+driver.quit()
