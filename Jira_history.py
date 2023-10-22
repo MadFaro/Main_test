0a4157148f0acd94603f9d4e0666e019e20b39af
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-10-23 00:39:43 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -13,31 +13,37 @@ ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
 
 import os
-import time
-from multiprocessing import Process
+import shutil
+import random
 
-# Функция для обработки файла
-def process_file(file_path):
-    # Ваш код для обработки файла здесь
+# Путь к вашей папке с файлами
+source_folder = '/путь/к/вашей/папке'
 
-# Функция для мониторинга папки
-def monitor_folder(folder_path):
-    while True:
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                process_file(file_path)
-        time.sleep(1)  # Измените значение по желанию
+# Путь к папкам, в которые нужно распределить файлы
+destination_folders = ['/путь/к/папке1', '/путь/к/папке2', '/путь/к/папке3', '/путь/к/папке4', '/путь/к/папке5', '/путь/к/папке6', '/путь/к/папке7', '/путь/к/папке8', '/путь/к/папке9', '/путь/к/папке10']
 
-if __name__ == '__main':
-    folder1 = 'путь_к_папке_1'
-    folder2 = 'путь_к_папке_2'
+# Получить список файлов в исходной папке
+file_list = os.listdir(source_folder)
 
-    process1 = Process(target=monitor_folder, args=(folder1,))
-    process2 = Process(target=monitor_folder, args=(folder2,))
+# Перемешать список файлов в случайном порядке
+random.shuffle(file_list)
 
-    process1.start()
-    process2.start()
+# Рассчитать, сколько файлов должно быть в каждой папке
+files_per_folder = len(file_list) // len(destination_folders)
+remainder = len(file_list) % len(destination_folders)
 
-    process1.join()
-    process2.join()
+# Создать целевые папки, если они еще не существуют
+for folder in destination_folders:
+    os.makedirs(folder, exist_ok=True)
+
+# Распределить файлы по папкам и переместить их
+for i, folder in enumerate(destination_folders):
+    start = i * files_per_folder
+    end = (i + 1) * files_per_folder
+    if i < remainder:
+        end += 1
+    for file in file_list[start:end]:
+        source_path = os.path.join(source_folder, file)
+        destination_path = os.path.join(folder, file)
+        shutil.move(source_path, destination_path)
+
