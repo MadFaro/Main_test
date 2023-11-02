dc7b10030d24068c1ba34568bbba2409c998efa4
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-11-03 01:12:35 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -12,3 +12,22 @@ ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 =ЕСЛИОШИБКА((((@Agents($AH$2;$AI$2;I18;I68)/30)*22,5)/0,85)/I166;2)
 
+from pydub import AudioSegment
+import xml.etree.ElementTree as ET
+
+# Загрузите аудиофайл WAV
+audio = AudioSegment.from_file("audio.wav")
+
+# Загрузите XML-файл
+tree = ET.parse("metadata.xml")
+root = tree.getroot()
+
+# Извлеките необходимые метаданные из XML
+# Например, если метаданные хранятся в теге <metadata>:
+metadata = root.find("metadata").text
+
+# Объедините аудио и метаданные, добавив метаданные в конец аудио
+audio_with_metadata = audio + AudioSegment.from_file(BytesIO(metadata.encode("utf-8")))
+
+# Сохраните объединенный файл
+audio_with_metadata.export("combined_audio.wav", format="wav")
