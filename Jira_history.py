e72113e643abcda0f96dce79b4ad9146de8ef8b7
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-11-23 17:21:26 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -9,3 +9,38 @@ ffmpeg -i output1.wav -af "volume=1.5" output2.wav
 ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 
+from razdel import tokenize
+import pymorphy2
+
+def normalize_text(text):
+    morph = pymorphy2.MorphAnalyzer()
+    tokens = [morph.parse(token.text.lower())[0].normal_form for token in tokenize(text)]
+    return ' '.join(tokens)
+
+def search_phrases_and_derivatives(text, phrases):
+    morph = pymorphy2.MorphAnalyzer()
+    normalized_text = normalize_text(text)
+    results = []
+
+    for phrase in phrases:
+        normalized_phrase = normalize_text(phrase)
+        index = normalized_text.find(normalized_phrase)
+        
+        if index != -1:
+            start = max(0, index - 200)
+            end = min(len(normalized_text), index + len(normalized_phrase) + 200)
+            context_before = text[start:index]
+            context_after = text[index + len(normalized_phrase):end]
+            
+            results.append({
+                "phrase": phrase,
+                "context_before": context_before,
+                "context_after": context_after
+            })
+
+    return results
+
+text_to_search = "Не перебивайте меня. Не перебивайте его. Перебивайте меня, если что-то не так."
+phrases_to_search = ["не перебивайте", "что-то не так"]
+
+search_result = search_phrases_and_derivatives(text_to_search, phrases_to_search)
