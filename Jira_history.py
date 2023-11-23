de83cfe457f6209cb97c609a330d7d2ab96cc8f4
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-11-23 10:52:57 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -9,29 +9,5 @@ ffmpeg -i output1.wav -af "volume=1.5" output2.wav
 ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 
-from razdel import tokenize
-import pymorphy2
-
-def normalize_text(text):
-    morph = pymorphy2.MorphAnalyzer()
-    tokens = [morph.parse(token.text.lower())[0].normal_form for token in tokenize(text)]
-    return ' '.join(tokens)
-
-def search_phrases_and_derivatives(text, phrases):
-    normalized_text = normalize_text(text)
-    results = []
-
-    for phrase in phrases:
-        normalized_phrase = normalize_text(phrase)
-        if normalized_phrase in normalized_text:
-            results.append(phrase)
-
-    return results
-
-text_to_search = "не перебивай меня. перебивай его. Перебивайте меня, если что-то не так."
-phrases_to_search = ["не перебивайте", "что-то не так"]
-
-search_result = search_phrases_and_derivatives(text_to_search, phrases_to_search)
-
-print("Найденные совпадения:", search_result)
+round(100*(number(concat(0,/webim/stats[@dep='roznichnyi_biznes_obshchii']/sl))+0.00001) div (number(concat(0,/webim/stats[@dep='roznichnyi_biznes_obshchii']/all))+0.00001))
 
