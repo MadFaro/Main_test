1727bbf1d56c937e4dce7a5acaabd3e028ac4c5d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-11-25 09:38:38 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -10,25 +10,61 @@ ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 
 
-Traceback (most recent call last):
-  File "main.py", line 59, in <module>
-    process1.start()
-  File "C:\Program Files\Python38\lib\multiprocessing\process.py", line 121, in start
-    self._popen = self._Popen(self)
-  File "C:\Program Files\Python38\lib\multiprocessing\context.py", line 224, in _Popen
-    return _default_context.get_context().Process._Popen(process_obj)
-  File "C:\Program Files\Python38\lib\multiprocessing\context.py", line 326, in _Popen
-    return Popen(process_obj)
-  File "C:\Program Files\Python38\lib\multiprocessing\popen_spawn_win32.py", line 93, in __init__
-    reduction.dump(process_obj, to_child)
-  File "C:\Program Files\Python38\lib\multiprocessing\reduction.py", line 60, in dump
-    ForkingPickler(file, protocol).dump(obj)
-TypeError: cannot pickle 'ctranslate2._ext.Whisper' object
+import os
+import time
+import soundfile as sf
+import pandas as pd
+from multiprocessing import spawn, Manager
+from faster_whisper import WhisperModel
 
-C:\Users\TologonovAB\Desktop\ASR_W>Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-  File "C:\Program Files\Python38\lib\multiprocessing\spawn.py", line 116, in spawn_main
-    exitcode = _main(fd, parent_sentinel)
-  File "C:\Program Files\Python38\lib\multiprocessing\spawn.py", line 126, in _main
-    self = reduction.pickle.load(from_parent)
-EOFError: Ran out of input
+def transcribe_worker(file, folder, text_file, results_list):
+    model = WhisperModel(model_size_or_path=r"C:\Users\TologonovAB\Desktop\model_wisper\whisper-large-v2",
+                         device="cpu", compute_type="float32", cpu_threads=8, num_workers=3)
+    
+    print(f'Обработка {folder}')
+    file_record = os.path.basename(file).split("$")
+    start_time = time.time()
+    segments, _ = model.transcribe(file, language="ru", task="transcribe", vad_filter=False)
+    segments = list(segments)
+    df_text = pd.DataFrame.from_dict(segments)
+    text = ' '.join(df_text['text'])
+    end_time = time.time()
+    result_time = end_time - start_time
+    results_list.append({
+        'id': file_record[0],
+        'callid': file_record[1],
+        'calltype': file_record[2],
+        'networkid': file_record[3],
+        'agentname': file_record[4],
+        'agentid': file_record[5],
+        'text': text,
+        'time': result_time
+    })
+    os.remove(file)
+    df = pd.DataFrame(results_list)
+    df.to_csv(text_file, mode='a', header=False, index=False, encoding='ANSI', lineterminator='\r\n', sep=';')
+
+def monitor_folder(folder_path, text_file, results_list):
+    while True:
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path):
+                transcribe_worker(file_path, folder_path, text_file, results_list)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    manager = Manager()
+    results_list1 = manager.list()
+    results_list2 = manager.list()
+    results_list3 = manager.list()
+
+    folder1 = r'C:\Users\TologonovAB\Desktop\ASR_W\1'
+    file1 = r'C:\Users\TologonovAB\Desktop\ASR_W\text1.csv'
+    folder2 = r'C:\Users\TologonovAB\Desktop\ASR_W\2'
+    file2 = r'C:\Users\TologonovAB\Desktop\ASR_W\text2.csv'
+    folder3 = r'C:\Users\TologonovAB\Desktop\ASR_W\3'
+    file3 = r'C:\Users\TologonovAB\Desktop\ASR_W\text3.csv'
+
+    spawn.Process(target=monitor_folder, args=(folder1, file1, results_list1)).start()
+    spawn.Process(target=monitor_folder, args=(folder2, file2, results_list2)).start()
+    spawn.Process(target=monitor_folder, args=(folder3, file3, results_list3)).start()
