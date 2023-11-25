1fad2dee1703f0658c28369fc3c47eed5b16b4b5
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-11-25 10:15:11 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -11,63 +11,21 @@ ffmpeg -i output3.wav -af "crystalizer" output4.wav
 
 
 
-import os
-import time
-import soundfile as sf
-import pandas as pd
-from multiprocessing import get_context, Process, Manager
-from faster_whisper import WhisperModel
-
-def transcribe_worker(file, folder, text_file, results_list):
-    model = WhisperModel(model_size_or_path=r"C:\Users\TologonovAB\Desktop\model_wisper\whisper-large-v2",
-                         device="cpu", compute_type="float32", cpu_threads=8, num_workers=3)
-    
-    print(f'Обработка {folder}')
-    file_record = os.path.basename(file).split("$")
-    start_time = time.time()
-    segments, _ = model.transcribe(file, language="ru", task="transcribe", vad_filter=False)
-    segments = list(segments)
-    df_text = pd.DataFrame.from_dict(segments)
-    text = ' '.join(df_text['text'])
-    end_time = time.time()
-    result_time = end_time - start_time
+  File "C:\Program Files\Python38\lib\multiprocessing\process.py", line 313, in _bootstrap
+    self.run()
+  File "C:\Program Files\Python38\lib\multiprocessing\process.py", line 108, in run
+    self._target(*self._args, **self._kwargs)
+  File "C:\Users\TologonovAB\Desktop\ASR_W\main.py", line 40, in monitor_folder
+    transcribe_worker(file_path, folder_path, text_file, results_list)
+  File "C:\Users\TologonovAB\Desktop\ASR_W\main.py", line 21, in transcribe_worker
     results_list.append({
-        'id': file_record[0],
-        'callid': file_record[1],
-        'calltype': file_record[2],
-        'networkid': file_record[3],
-        'agentname': file_record[4],
-        'agentid': file_record[5],
-        'text': text,
-        'time': result_time
-    })
-    os.remove(file)
-    df = pd.DataFrame(results_list)
-    df.to_csv(text_file, mode='a', header=False, index=False, encoding='ANSI', lineterminator='\r\n', sep=';')
-
-def monitor_folder(folder_path, text_file, results_list):
-    while True:
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                transcribe_worker(file_path, folder_path, text_file, results_list)
-        time.sleep(1)
-
-if __name__ == "__main__":
-    context = get_context("spawn")
-    manager = context.Manager()
-    results_list1 = manager.list()
-    results_list2 = manager.list()
-    results_list3 = manager.list()
-
-    folder1 = r'C:\Users\TologonovAB\Desktop\ASR_W\1'
-    file1 = r'C:\Users\TologonovAB\Desktop\ASR_W\text1.csv'
-    folder2 = r'C:\Users\TologonovAB\Desktop\ASR_W\2'
-    file2 = r'C:\Users\TologonovAB\Desktop\ASR_W\text2.csv'
-    folder3 = r'C:\Users\TologonovAB\Desktop\ASR_W\3'
-    file3 = r'C:\Users\TologonovAB\Desktop\ASR_W\text3.csv'
-
-    Process(target=monitor_folder, args=(folder1, file1, results_list1)).start()
-    Process(target=monitor_folder, args=(folder2, file2, results_list2)).start()
-    Process(target=monitor_folder, args=(folder3, file3, results_list3)).start()
-
+  File "<string>", line 2, in append
+  File "C:\Program Files\Python38\lib\multiprocessing\managers.py", line 831, in _callmethod
+    self._connect()
+  File "C:\Program Files\Python38\lib\multiprocessing\managers.py", line 818, in _connect
+    conn = self._Client(self._token.address, authkey=self._authkey)
+  File "C:\Program Files\Python38\lib\multiprocessing\connection.py", line 500, in Client
+    c = PipeClient(address)
+  File "C:\Program Files\Python38\lib\multiprocessing\connection.py", line 701, in PipeClient
+    _winapi.WaitNamedPipe(address, 1000)
+FileNotFoundError: [WinError 2] Не удается найти указанный файл
