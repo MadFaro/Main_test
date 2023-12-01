4533c78ee25fe7693997d2a4c6890969f67791e4
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-12-01 19:36:58 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -10,21 +10,19 @@ ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
 
 
-
-from concurrent.futures import ProcessPoolExecutor
-
-def main():
-    folder1_path = r'C:\Users\TologonovAB\Desktop\model_wisper\move1'
-    folder2_path = r'C:\Users\TologonovAB\Desktop\model_wisper\move2'
-    model_path = r"C:\Users\TologonovAB\Desktop\model_wisper\whisper-int8-2"
-
-    result_file1 = 'text1.csv'
-    result_file2 = 'text2.csv'
-
-    with ProcessPoolExecutor() as executor:
-        executor.submit(process_files, folder1_path, model_path, result_file1)
-        executor.submit(process_files, folder2_path, model_path, result_file2)
-
-if __name__ == "__main__":
-    main()
-
+Args:
+  model_size_or_path: Size of the model to use (tiny, tiny.en, base, base.en,
+    small, small.en, medium, medium.en, large-v1, large-v2, or large), a path to a converted model directory, or a CTranslate2-converted Whisper model ID from the Hugging Face Hub. When a size or a model ID is configured, the converted model is downloaded from the Hugging Face Hub.
+  device: Device to use for computation ("cpu", "cuda", "auto").
+  device_index: Device ID to use.
+    The model can also be loaded on multiple GPUs by passing a list of IDs (e.g. [0, 1, 2, 3]). In that case, multiple transcriptions can run in parallel when transcribe() is called from multiple Python threads (see also num_workers).
+  compute_type: Type to use for computation.
+    See https://opennmt.net/CTranslate2/quantization.html.
+  cpu_threads: Number of threads to use when running on CPU (4 by default).
+    A non zero value overrides the OMP_NUM_THREADS environment variable.
+  num_workers: When transcribe() is called from multiple Python threads,
+    having multiple workers enables true parallelism when running the model (concurrent calls to self.model.generate() will run in parallel). This can improve the global throughput at the cost of increased memory usage.
+  download_root: Directory where the models should be saved. If not set, the models
+    are saved in the standard Hugging Face cache directory.
+  local_files_only: If True, avoid downloading the file and return the path to the
+    local cached file if it exists.
