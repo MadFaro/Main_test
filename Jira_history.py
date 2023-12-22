266c91e3bd716658970b21fd4b90a15e5db6602d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2023-12-22 10:46:43 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -8,15 +8,4 @@ ffmpeg -i output.wav -af "highpass=f=300, lowpass=f=3000" output1.wav
 ffmpeg -i output1.wav -af "volume=1.5" output2.wav
 ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav
-sd1.py:49: UserWarning: Parsing dates in %d.%m.%Y %H:%M:%S format when dayfirst=False (the default) was specified. Pass `dayfirst=True` or specify a format to silence this warning.
-  df_select[date_columns] = df_select[date_columns].apply(pd.to_datetime, errors='coerce')
-sd1.py:49: UserWarning: Parsing dates in %d.%m.%Y %H:%M:%S format when dayfirst=False (the default) was specified. Pass `dayfirst=True` or specify a format to silence this warning.
-  df_select[date_columns] = df_select[date_columns].apply(pd.to_datetime, errors='coerce')
-sd1.py:49: UserWarning: Parsing dates in %d.%m.%Y %H:%M:%S format when dayfirst=False (the default) was specified. Pass `dayfirst=True` or specify a format to silence this warning.
-  df_select[date_columns] = df_select[date_columns].apply(pd.to_datetime, errors='coerce')
-sd1.py:49: UserWarning: Parsing dates in %d.%m.%Y %H:%M:%S format when dayfirst=False (the default) was specified. Pass `dayfirst=True` or specify a format to silence this warning.
-  df_select[date_columns] = df_select[date_columns].apply(pd.to_datetime, errors='coerce')
-sd1.py:49: SettingWithCopyWarning:
-A value is trying to be set on a copy of a slice from a DataFrame.
-Try using .loc[row_indexer,col_indexer] = value instead
-
+https://us05web.zoom.us/j/9134161860?pwd=WE5xR0pGVFV2RVRKT2xpcTNCOGRSdz09
