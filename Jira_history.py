97c956ef79af853a72e818008757140d1dd72525
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-02-19 15:21:33 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -10,65 +10,10 @@ ffmpeg -i output2.wav -af "equalizer=f=1000:width_type=h:w=200:g=5" output3.wav
 ffmpeg -i output3.wav -af "crystalizer" output4.wav  
 
 
-CREATE OR REPLACE PROCEDURE p_service_level IS
-BEGIN
-    FOR cur_rec IN (
-        WITH thread_history_cte AS (
-            SELECT cth.threadid, cth.departmentid, cth.state, dtm
-            FROM chatthreadhistory cth
-            JOIN chatthread ct ON ct.threadid = cth.threadid
-            WHERE ct.offline = 0
-            AND dtm BETWEEN p_dtmfrom AND p_dtmto
-        )
-        SELECT threadid, departmentid, state, dtm
-        FROM thread_history_cte
-        ORDER BY threadid, dtm
-    ) LOOP
-        -- Reset for each new thread
-        IF cur_rec.threadid <> cur_rec.threadid THEN
-            v_start_time := NULL;
-            v_end_time := NULL;
-        END IF;
-
-        -- Consider queue time after bot if chat initially went to a bot
-        -- Do not consider dialogue if it was not in the chatting state and was closed by the operator
-        IF cur_rec.state IN ('chatting_with_robot', 'closed_by_operator') THEN
-            v_start_time := NULL;
-            v_end_time := NULL;
-        END IF;
-
-        IF cur_rec.state = 'queue' AND v_start_time IS NULL THEN
-            v_start_time := cur_rec.dtm;
-        END IF;
-
-        IF cur_rec.state = 'chatting' AND v_start_time IS NOT NULL THEN
-            SELECT MIN(created) INTO v_end_time
-            FROM chatmessage
-            WHERE threadid = cur_rec.threadid
-              AND kind IN (2, 10, 13)
-              AND created > cur_rec.dtm;
-
-            -- Handling if no records found
-            EXCEPTION
-                WHEN NO_DATA_FOUND THEN
-                    v_end_time := NULL;
-        END IF;
-
-        IF v_start_time IS NOT NULL AND v_end_time IS NOT NULL THEN
-            INSERT INTO tmp_stats_service_level (
-                threadid,
-                department_id,
-                got_into_common_queue_time,
-                start_chatting_time
-            ) VALUES (
-                cur_rec.threadid,
-                cur_rec.departmentid,
-                v_start_time,
-                v_end_time
-            );
-            v_start_time := NULL;
-            v_end_time := NULL;
-        END IF;
-    END LOOP;
-END;
-/
+(
+    p_dtmfrom IN TIMESTAMP,
+    p_dtmto IN TIMESTAMP,
+    p_departmentid IN INT,
+    p_locale IN CHAR(2)
+)
+IS
