299bb626821469ff6f9f18ce52c10bc0c476882f
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-02-28 13:25:30 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,9 +1,31 @@
-from pywebio.session import run_js, info
+import datetime
+import cx_Oracle
+import pandas as pd
+import win32com.client
+from sqlalchemy import create_engine
 
-print(json.dumps({
-    k: str(getattr(info, k))
-    for k in ['user_agent', 'user_language', 'server_host',
-              'origin', 'user_ip', 'backend', 'protocol', 'request']
-}, indent=4))
 
-import json
+def connect_str(): return cx_Oracle.connect(user='user',
+                                            password='pass', 
+                                            dsn='dsn')
+
+connect = create_engine("oracle://", creator=connect_str)
+
+sql = """
+select ACTION_DATE, ACTION_TYPE, RESULT_TYPE, TABLE_NAME, ROWS_COUNT, PROC_NAME from (
+select ACTION_DATE, ACTION_TYPE, RESULT_TYPE, TABLE_NAME, ROWS_COUNT, PROC_NAME,
+ROW_NUMBER() OVER (PARTITION BY TABLE_NAME ORDER BY ACTION_DATE desc) as rnk
+from job_logs
+where trunc(ACTION_DATE)=trunc(sysdate) and TABLE_NAME in (
+'tab1',
+'tab2',
+'tab3',
+'tab4',
+'tab5'
+))
+where rnk = 1
+    """
+
+data = pd.read_sql(sql, connect).to_html(index=False)
+
+print(data)
