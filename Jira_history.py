ee6f64496a3afe053f41e6536a8fe5f55be76378
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-02-29 18:06:08 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,9 @@
-
+   { margin: 0 auto;
+    width: 1400px;
+    z-index: 2;
+    background-color: #fff;
+    position: relative;
+    height: 100%;
+    box-shadow: 0 -10px 25px #afb2b7;
+    padding-top: 65px;
+}
