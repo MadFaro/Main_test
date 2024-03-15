f6357a142cc23192d15bba68e32c03d1f9abb88e
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-03-15 18:10:50 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,18 +1,1 @@
-SELECT 
-    ФИО_клиента,
-    TO_CHAR(дата_обращения, 'MM.YYYY') AS месяц,
-    MIN(CASE WHEN rn = 1 THEN дата_обращения END) AS первое_обращение,
-    MIN(CASE WHEN rn = 2 THEN дата_обращения END) AS второе_обращение,
-    MIN(CASE WHEN rn = 3 THEN дата_обращения END) AS третье_обращение,
-    COUNT(*) AS сумма_обращений
-FROM (
-    SELECT 
-        ФИО_клиента,
-        дата_обращения,
-        ROW_NUMBER() OVER (PARTITION BY ФИО_клиента, TO_CHAR(дата_обращения, 'MM.YYYY') ORDER BY дата_обращения) AS rn
-    FROM 
-        ваша_таблица
-)
-GROUP BY 
-    ФИО_клиента,
-    TO_CHAR(дата_обращения, 'MM.YYYY');
+
