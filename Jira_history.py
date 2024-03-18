01de7378253c21da6cdd9908c0779f3cc62ffb4d
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-03-18 18:02:15 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,12 @@
+# Определите функцию для преобразования значений в тип float
+def to_float(x):
+    try:
+        return float(x)
+    except ValueError:
+        return x  # Если не удаётся преобразовать, оставляем значение без изменений
 
+# Примените функцию ко всем столбцам, кроме первого
+df.iloc[:, 1:] = df.iloc[:, 1:].applymap(to_float)
+
+# Вывод преобразованного DataFrame
+print(df)
