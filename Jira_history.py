98b6b401cdbe72f8b8f5d900282adcfe461d8c99
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-04-01 18:19:00 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,1 @@
-# Преобразуем каждый объект времени в количество секунд
-df['time_as_seconds'] = df['time_column'].apply(lambda x: x.hour * 3600 + x.minute * 60 + x.second)
 
-# Преобразуем каждый объект времени во float (количество секунд) для удобства использования
-df['time_as_float'] = df['time_as_seconds'].astype(float)
