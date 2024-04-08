7ac6aad6fa3673677af93f69224a5a6c07d59114
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-04-09 01:25:23 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,6 +1,3 @@
-set App = createobject("excel.application")
-App.visible = false
-App.Workbooks.Open("C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия).xlsx"),,,,,255 
-App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").WorkSheets("Таблица").UsedRange.Value2 = App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").WorkSheets("Таблица").UsedRange.Value2 
-App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").SaveAs "C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия) итог.xlsx" 
-App.quit
+Sub удалитьФормулыСоВсегоЛиста()
+ActiveSheet.UsedRange.Value2 = ActiveSheet.UsedRange.Value2
+End Sub
