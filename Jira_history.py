8918a7c947d45cd52094c16a24e0b09ef2a865be
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-04-09 01:19:40 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,6 @@
-Const xlPasteValuesAndNumberFormats As Integer = -4144 ' Это значение для xlPasteValuesAndNumberFormats
+set App = createobject("excel.application")
+App.visible = false
+App.Workbooks.Open("C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия).xlsx"),,,,,255 
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").WorkSheets("Таблица").UsedRange.Value2 = App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").WorkSheets("Таблица").UsedRange.Value2 
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").SaveAs "C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия) итог.xlsx" 
+App.quit
