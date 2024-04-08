e46995f866ff60ae036fee69c1b7bb0051c3c4dd
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-04-09 01:00:55 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,11 @@
+set App = createobject("excel.application")
 
+App.visible = false
+App.Workbooks.Open("C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия).xlsx"),,,,,255
+'App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").RefreshAll
+'App.Calculate
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").Cells.Select
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").Cells.Copy
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").Cells.Selection.PasteSpecial Paste:=xlPasteValuesAndNumberFormats
+App.Workbooks("Динамика показателей ДТ(рабочая версия).xlsx").SaveAs "C:\Users\TologonovAB\Desktop\Динамика показателей ДТ(рабочая версия) итог.xlsx"
+App.quit
