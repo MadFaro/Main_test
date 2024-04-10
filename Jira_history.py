a573d855cfc72e8fe5536a3f396fe124c51a78d6
Alexey Tologonov <122479077+MadFaro@users.noreply.github.com>
2024-04-10 08:53:05 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,18 +1,1 @@
-WITH chats_with_gaps AS (
-    SELECT
-        chat_id,
-        visitor_id,
-        chat_date,
-        LAG(chat_date) OVER (PARTITION BY visitor_id, TRUNC(chat_date) ORDER BY chat_date) AS prev_chat_date
-    FROM
-        your_table_name
-)
-SELECT
-    TRUNC(chat_date) AS chat_date,
-    COUNT(CASE WHEN prev_chat_date IS NULL OR (chat_date - prev_chat_date) * 24 >= 2 THEN 1 END) AS chat_count
-FROM
-    chats_with_gaps
-GROUP BY
-    TRUNC(chat_date)
-ORDER BY
-    TRUNC(chat_date);
+
