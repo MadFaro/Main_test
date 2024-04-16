a5a3059181c2eb18b79e7de0bd17cf8277f2e817
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-16 13:59:05 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,9 @@
-SELECT a.OWNER as owner, a.object_name AS procedure_name
+SELECT a.OWNER as owner,
+       a.object_name AS procedure_name,
+       c.table_name
 FROM all_objects a
 JOIN all_source b ON a.object_name = b.name
-WHERE a.object_type = 'PROCEDURE' and (lower(a.object_name) like '%np%' or lower(a.object_name) like '%rap%')
-and b.text like '%@%'
+JOIN all_tab_columns c ON b.text LIKE '%' || c.table_name || '%@%'
+WHERE a.object_type = 'PROCEDURE'
+  AND (LOWER(a.object_name) LIKE '%np%' OR LOWER(a.object_name) LIKE '%rap%')
+  AND b.text LIKE '%@%';
