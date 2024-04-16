6675a458a5953c2834c8c19d02df4fb8e206ef94
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-16 15:00:29 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,14 +1,1 @@
-import re
 
-# Ваша запись
-record = "from table1@link1 sdsd или left join table2@link2 s или inner join table3@link3 или ничего"
-
-# Паттерн регулярного выражения
-pattern = r'\b\w+@\w+\b'
-
-# Ищем совпадения
-matches = re.findall(pattern, record)
-
-# Выводим найденные совпадения
-for match in matches:
-    print(match)
