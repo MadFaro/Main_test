90e57bd8b7344e929732da6f3561e4b48833bc60
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-16 14:09:01 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,1 @@
-SELECT a.OWNER as owner,
-       a.object_name AS procedure_name,
-       SUBSTR(b.text, INSTR(b.text, '@', 1) + 1, LENGTH(b.text) - INSTR(b.text, '@', 1)) AS table_name_right,
-       SUBSTR(b.text, 1, INSTR(b.text, '@', 1) - 1) AS table_name_left,
-       SUBSTR(b.text, 1, INSTR(b.text, '@', 1) - 1) || SUBSTR(b.text, INSTR(b.text, '@', 1) + 1, LENGTH(b.text) - INSTR(b.text, '@', 1)) AS table_name_full
-FROM all_objects a
-JOIN all_source b ON a.object_name = b.name
-WHERE a.object_type = 'PROCEDURE'
-  AND (LOWER(a.object_name) LIKE '%np%' OR LOWER(a.object_name) LIKE '%rap%')
-  AND b.text LIKE '%@%';
+
