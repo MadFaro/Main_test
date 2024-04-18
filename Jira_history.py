80cb1c9bf77903b45e7a79674f38c1f1ca9a0f80
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-18 17:20:19 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,17 +1,17 @@
 SELECT 
     TO_CHAR(h.hour_slot, 'YYYY-MM-DD HH24:MI') AS hour_slot,
-    COUNT(DISTINCT operator_id) AS operators_count
+    COUNT(DISTINCT operatorfullname) AS operators_count
 FROM
-    (SELECT TRUNC(entry_time, 'HH24') + (LEVEL - 1)/24 AS hour_slot
+    (SELECT TRUNC(dtm, 'HH24') + (LEVEL - 1)/24 AS hour_slot
      FROM dual
-     CONNECT BY TRUNC(entry_time, 'HH24') + (LEVEL - 1)/24 < TRUNC(SYSDATE) + 1) h
+     CONNECT BY TRUNC(dtm, 'HH24') + (LEVEL - 1)/24 < TRUNC(SYSDATE) + 1) h
 LEFT JOIN
-    (SELECT operator_id,
-            TRUNC(entry_time, 'HH24') + (LEVEL - 1)/24 AS hour_slot
+    (SELECT operatorfullname,
+            TRUNC(dtm, 'HH24') + (LEVEL - 1)/24 AS hour_slot
      FROM your_table_name
-     WHERE status = 'login'
-     CONNECT BY PRIOR operator_id = operator_id
-                AND PRIOR TRUNC(logout_time, 'HH24') + (LEVEL - 1)/24 < TRUNC(logout_time, 'HH24') + 1) logins
+     WHERE actiontype = 'login'
+     CONNECT BY PRIOR operatorfullname = operatorfullname
+                AND PRIOR TRUNC(dtm, 'HH24') + (LEVEL - 1)/24 < TRUNC(dtm, 'HH24') + 1) logins
 ON
     h.hour_slot = logins.hour_slot
 GROUP BY
