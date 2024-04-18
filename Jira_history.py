81552339f61a8d0136a4c7a681591107b7299579
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-18 17:31:58 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -16,15 +16,16 @@ TimeSlots AS (
 
 SELECT 
     TO_CHAR(TimeSlots.hour_slot, 'YYYY-MM-DD HH24:MI') AS hour_slot,
-    COUNT(DISTINCT CASE WHEN ACTIONTYPE = 'login' THEN OPERATORID END) AS operators_count
+    COUNT(DISTINCT CASE WHEN TimeSlots.hour_slot BETWEEN your_table_name.DTM AND your_table_name.logout_time THEN OPERATORID END) AS operators_count
 FROM
     TimeSlots
 LEFT JOIN
     your_table_name
 ON
-    TimeSlots.hour_slot BETWEEN your_table_name.DTM AND NVL((SELECT MIN(DTM) FROM your_table_name WHERE DTM > TimeSlots.hour_slot AND ACTIONTYPE = 'login'), SYSDATE)
+    TimeSlots.hour_slot BETWEEN your_table_name.DTM AND NVL(your_table_name.logout_time, SYSDATE)
 GROUP BY
     TimeSlots.hour_slot
 ORDER BY
     hour_slot;
+
 
