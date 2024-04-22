5d091bbabd4551b0739a88577860bd91b0060318
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-22 11:43:34 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,18 +1,1 @@
-SELECT
-    v1.date_time AS date_time_v1,
-    v1.phone_number AS phone_number_v1,
-    v2.date_time AS date_time_v2,
-    v2.phone_number AS phone_number_v2
-FROM
-    (SELECT
-        v1.date_time,
-        v1.phone_number,
-        v2.date_time AS date_time_v2,
-        v2.phone_number AS phone_number_v2,
-        ROW_NUMBER() OVER (PARTITION BY v1.date_time, v1.phone_number ORDER BY ABS(v1.date_time - v2.date_time)) AS rn
-    FROM
-        v1
-    LEFT JOIN v2 ON v1.phone_number = v2.phone_number) sub
-WHERE
-    sub.rn = 1;
 
