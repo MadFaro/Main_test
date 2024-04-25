c2152124943a0516a0282e628f282c8418596cb3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-25 12:45:59 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,2 +1,1 @@
-fatal error C1083: Cannot open include file: 'io.h': No such file or directory
-error: command 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\MSVC\\14.31.31103\\bin\\HostX86\\x64\\cl.exe' failed with exit status 2
+
