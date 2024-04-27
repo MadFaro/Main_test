c7fc7ab261830789585a4a37595d22ab2367a068
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 20:05:04 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,14 +1,1 @@
-# Преобразование текста в числовые векторы с помощью TF-IDF
-X_new = data_new['MSG']
-X_new_tfidf = tfidf_vectorizer.transform(X_new)
-X_new_tfidf.sort_indices()
 
-# Предсказание категорий
-predictions = model.predict(X_new_tfidf)
-predicted_categories = label_encoder.inverse_transform(predictions.argmax(axis=1))
-
-# Добавление предсказанных категорий в датафрейм
-data_new['CATEGORY'] = predicted_categories
-
-# Сохранение данных в новый файл Excel
-data_new.to_excel("itog.xlsx", index=False)
