c270ca5d77eafd9c3f18c061fa95d09e30bd9035
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:45:03 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,3 @@
-  File "adam_predict.py", line 20, in <module>
-    predictions = model.predict(X_new_tfidf)
-  File "C:\Python38\lib\site-packages\keras\utils\traceback_utils.py", line 70, in error_handler
-    raise e.with_traceback(filtered_tb) from None
-  File "C:\Python38\lib\site-packages\tensorflow\python\framework\ops.py", line 7262, in raise_from_not_ok_status
-    raise core._status_to_exception(e) from None  # pylint: disable=protected-access
-tensorflow.python.framework.errors_impl.InvalidArgumentError: {{function_node __wrapped__SerializeManySparse_device_/job:localhost/replica:0/task:0/device:CPU:0}} indices[1] = [0,963] is out of order. Many sparse ops require sorted indices.
-    Use `tf.sparse.reorder` to create a correctly ordered copy.
-
- [Op:SerializeManySparse]
+# Преобразование текста в числовые векторы с помощью TF-IDF
+X_new_tfidf = tfidf_vectorizer.transform(X_new)
+X_new_tfidf.sort_indices()  # Упорядочивание индексов
