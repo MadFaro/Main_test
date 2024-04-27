0cade5312b3cd7e3fd52d8f65125e88953d4b412
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:00:11 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,11 @@
-Traceback (most recent call last):
-  File "adam.py", line 33, in <module>
-    model = Sequential([
-  File "C:\Python38\lib\site-packages\tensorflow\python\trackable\base.py", line 205, in _method_wrapper
-    result = method(self, *args, **kwargs)
-  File "C:\Python38\lib\site-packages\keras\utils\traceback_utils.py", line 70, in error_handler
-    raise e.with_traceback(filtered_tb) from None
-  File "C:\Python38\lib\site-packages\keras\engine\input_spec.py", line 235, in assert_input_compatibility
-    raise ValueError(
-ValueError: Input 0 of layer "lstm" is incompatible with the layer: expected ndim=3, found ndim=2. Full shape received: (None, 1000)
+import numpy as np
+
+# Преобразование TF-IDF матрицы в трехмерный тензор
+X_tfidf_3d = X_tfidf.reshape(X_tfidf.shape[0], 1, X_tfidf.shape[1])
+
+
+model = Sequential([
+    LSTM(60, activation='relu', input_shape=(1, X_tfidf.shape[1]), return_sequences=True),
+    LSTM(120, activation='softmax'),
+    Dense(1)
+])
