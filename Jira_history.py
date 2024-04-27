46bc85afc52a5b98f18682d002582f1e89b365b3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:16:20 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,4 +1,28 @@
-import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tensorflow.keras.models import load_model
 
-# Преобразование TF-IDF матрицы в трехмерный массив
-X_tfidf_3d = np.reshape(X_tfidf.toarray(), (X_tfidf.shape[0], 1, X_tfidf.shape[1]))
+# Загрузка сохраненной модели
+model = load_model("model.h5")
+
+# Загрузка новых данных для предсказания
+data_new = pd.read_excel("test1.xlsx")
+X_new = data_new['MSG']
+
+# Преобразование текста в числовые векторы с помощью TF-IDF
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X_new_tfidf = tfidf_vectorizer.transform(X_new)
+
+# Предсказание категорий
+predictions = model.predict(X_new_tfidf)
+
+# Декодирование предсказанных меток категорий
+predicted_categories = label_encoder.inverse_transform(predictions.argmax(axis=1))
+
+# Добавление предсказанных категорий в новый DataFrame
+data_new['CATEGORY'] = predicted_categories
+
+# Запись данных в новый файл CSV
+data_new.to_csv("itog.csv", index=False)
+
+print("Предсказания сохранены в файл itog.csv")
