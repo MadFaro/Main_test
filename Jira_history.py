da4008d99e188ba9f05974b44cfdcb00781b2935
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:02:37 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,11 +1,8 @@
-import numpy as np
-
-# Преобразование TF-IDF матрицы в трехмерный тензор
-X_tfidf_3d = X_tfidf.reshape(X_tfidf.shape[0], 1, X_tfidf.shape[1])
-
-
-model = Sequential([
-    LSTM(60, activation='relu', input_shape=(1, X_tfidf.shape[1]), return_sequences=True),
-    LSTM(120, activation='softmax'),
-    Dense(1)
-])
+Traceback (most recent call last):
+  File "adam.py", line 21, in <module>
+    X_tfidf_3d = X_tfidf.reshape(X_tfidf.shape[0], 1, X_tfidf.shape[1])
+  File "C:\Python38\lib\site-packages\scipy\sparse\_base.py", line 158, in reshape
+    shape = check_shape(args, self.shape)
+  File "C:\Python38\lib\site-packages\scipy\sparse\_sputils.py", line 340, in check_shape
+    raise ValueError('matrix shape must be two-dimensional')
+ValueError: matrix shape must be two-dimensional
