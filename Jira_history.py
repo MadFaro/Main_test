102c092fe87e3be46037d598dcf969c6f1fcc0ca
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:55:11 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,34 @@
-, encoding='utf-8-sig'
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tensorflow.keras.models import load_model
+
+# Загрузка модели
+model = load_model("model.h5")
+
+# Загрузка TF-IDF векторизатора
+with open('tfidf_vectorizer.pkl', 'rb') as f:
+    tfidf_vectorizer = pickle.load(f)
+
+# Загрузка label_encoder
+with open('label_encoder.pkl', 'rb') as f:
+    label_encoder = pickle.load(f)
+
+# Загрузка новых данных для предсказания
+data_new = pd.read_excel("test1.xlsx")
+
+# Преобразование текста в числовые векторы с помощью TF-IDF
+X_new = data_new['MSG']
+X_new_tfidf = tfidf_vectorizer.transform(X_new)
+
+# Предсказание категорий
+predictions = model.predict(X_new_tfidf)
+
+# Декодирование предсказанных меток категорий
+predicted_categories = label_encoder.inverse_transform(predictions.argmax(axis=1))
+
+# Добавление предсказанных категорий в новый DataFrame
+data_new['CATEGORY'] = predicted_categories
+
+# Запись данных в новый файл CSV
+data_new[['ID', 'MSG', 'CATEGORY']].to_csv("itog.csv", index=False)
