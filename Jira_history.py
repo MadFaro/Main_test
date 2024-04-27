a6ae6638f5239c4c35e076dc86e8e810d8efcd6a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 18:53:05 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,8 +1,10 @@
-model = Sequential([
-    LSTM(60, activation='relu', input_shape=(X_tfidf.shape[1],), return_sequences=True),
-    LSTM(120, activation='softmax'),
-    Dense(1)
-])
-
-# Компиляция модели
-model.compile(optimizer=Adam(learning_rate=0.006), loss='mse', metrics=['mae'])
+Traceback (most recent call last):
+  File "adam.py", line 33, in <module>
+    model = Sequential([
+  File "C:\Python38\lib\site-packages\tensorflow\python\trackable\base.py", line 205, in _method_wrapper
+    result = method(self, *args, **kwargs)
+  File "C:\Python38\lib\site-packages\keras\utils\traceback_utils.py", line 70, in error_handler
+    raise e.with_traceback(filtered_tb) from None
+  File "C:\Python38\lib\site-packages\keras\engine\input_spec.py", line 235, in assert_input_compatibility
+    raise ValueError(
+ValueError: Input 0 of layer "lstm" is incompatible with the layer: expected ndim=3, found ndim=2. Full shape received: (None, 1000)
