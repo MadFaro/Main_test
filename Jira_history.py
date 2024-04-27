7de2615d86ddfa880ff3f27efa2ddf2fc12ee6ca
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 19:03:07 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,8 +1,4 @@
-Traceback (most recent call last):
-  File "adam.py", line 21, in <module>
-    X_tfidf_3d = X_tfidf.reshape(X_tfidf.shape[0], 1, X_tfidf.shape[1])
-  File "C:\Python38\lib\site-packages\scipy\sparse\_base.py", line 158, in reshape
-    shape = check_shape(args, self.shape)
-  File "C:\Python38\lib\site-packages\scipy\sparse\_sputils.py", line 340, in check_shape
-    raise ValueError('matrix shape must be two-dimensional')
-ValueError: matrix shape must be two-dimensional
+import numpy as np
+
+# Преобразование TF-IDF матрицы в трехмерный массив
+X_tfidf_3d = np.reshape(X_tfidf.toarray(), (X_tfidf.shape[0], 1, X_tfidf.shape[1]))
