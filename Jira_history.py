fbcbe27fa91e28c1111b6c1bb72ca455f16569f4
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 18:15:21 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,42 +1,15 @@
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import TfidfVectorizer
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+2024-04-27 18:14:57.087412: W tensorflow/core/framework/op_kernel.cc:1830] OP_REQUIRES failed at serialize_sparse_op.cc:389 : INVALID_ARGUMENT: indices[1] = [0,346] is out of order. Many sparse ops require sorted indices.
+    Use `tf.sparse.reorder` to create a correctly ordered copy.
 
-# Загрузка данных из файла Excel
-data = pd.read_excel("test.xlsx", sheet_name="свод")
 
-# Разделение данных на признаки (X) и метки (y)
-X = data['MSG']
-y = data['CATEGORY']
+Traceback (most recent call last):
+  File "adam.py", line 43, in <module>
+    model.fit(X_tfidf, y_encoded, epochs=100, batch_size=32, verbose=1, callbacks=callbacks)
+  File "C:\Python38\lib\site-packages\keras\utils\traceback_utils.py", line 70, in error_handler
+    raise e.with_traceback(filtered_tb) from None
+  File "C:\Python38\lib\site-packages\tensorflow\python\framework\ops.py", line 7262, in raise_from_not_ok_status
+    raise core._status_to_exception(e) from None  # pylint: disable=protected-access
+tensorflow.python.framework.errors_impl.InvalidArgumentError: {{function_node __wrapped__SerializeManySparse_device_/job:localhost/replica:0/task:0/device:CPU:0}} indices[1] = [0,346] is out of order. Many sparse ops require sorted indices.
+    Use `tf.sparse.reorder` to create a correctly ordered copy.
 
-# Преобразование текста в числовые векторы с помощью TF-IDF
-tfidf_vectorizer = TfidfVectorizer(max_features=1000)
-X_tfidf = tfidf_vectorizer.fit_transform(X)
-
-# Кодирование меток категорий
-label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
-
-# Создание нейронной сети
-model = Sequential([
-    Dense(512, activation='relu', input_shape=(X_tfidf.shape[1],)),
-    Dropout(0.5),
-    Dense(256, activation='relu'),
-    Dropout(0.5),
-    Dense(len(label_encoder.classes_), activation='softmax')
-])
-
-# Компиляция модели
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=Adam(learning_rate=0.001),
-              metrics=['accuracy'])
-
-# Обучение модели на всем наборе данных
-model.fit(X_tfidf, y_encoded, epochs=10, batch_size=32)
-
-# Сохранение модели
-model.save("model.h5")
-print("Модель сохранена в файл model.h5")
+ [Op:SerializeManySparse]
