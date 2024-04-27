d0a0f8c7f76de9bd0a06e626ddad5d5074369608
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 18:51:54 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,8 @@
-model = Sequential()
-model.add(LSTM(60, activation='relu', input_shape=(X_tfidf.shape[1],), return_sequences = True))
-model.add(LSTM(120, activation='softmax'))
-model.add(Dense(1))
+model = Sequential([
+    LSTM(60, activation='relu', input_shape=(X_tfidf.shape[1],), return_sequences=True),
+    LSTM(120, activation='softmax'),
+    Dense(1)
+])
+
+# Компиляция модели
 model.compile(optimizer=Adam(learning_rate=0.006), loss='mse', metrics=['mae'])
