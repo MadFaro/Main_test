9d4b2cc2e04856c8dc5c940698faddc8679fb516
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-27 18:12:53 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,23 +1,42 @@
-Traceback (most recent call last):
-  File "C:\Python38\lib\site-packages\sklearn\utils\_encode.py", line 224, in _encode
-    return _map_to_integer(values, uniques)
-  File "C:\Python38\lib\site-packages\sklearn\utils\_encode.py", line 164, in _map_to_integer
-    return np.array([table[v] for v in values])
-  File "C:\Python38\lib\site-packages\sklearn\utils\_encode.py", line 164, in <listcomp>
-    return np.array([table[v] for v in values])
-  File "C:\Python38\lib\site-packages\sklearn\utils\_encode.py", line 158, in __missing__
-    raise KeyError(key)
-KeyError: '   РБ_Подключение_Уралсиб-Бонус'
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
 
-During handling of the above exception, another exception occurred:
+# Загрузка данных из файла Excel
+data = pd.read_excel("test.xlsx", sheet_name="свод")
 
-Traceback (most recent call last):
-  File "adam.py", line 26, in <module>
-    y_test_encoded = label_encoder.transform(y_test)
-  File "C:\Python38\lib\site-packages\sklearn\utils\_set_output.py", line 140, in wrapped
-    data_to_wrap = f(self, X, *args, **kwargs)
-  File "C:\Python38\lib\site-packages\sklearn\preprocessing\_label.py", line 139, in transform
-    return _encode(y, uniques=self.classes_)
-  File "C:\Python38\lib\site-packages\sklearn\utils\_encode.py", line 226, in _encode
-    raise ValueError(f"y contains previously unseen labels: {str(e)}")
-ValueError: y contains previously unseen labels: '   РБ_Подключение_Уралсиб-Бонус'
+# Разделение данных на признаки (X) и метки (y)
+X = data['MSG']
+y = data['CATEGORY']
+
+# Преобразование текста в числовые векторы с помощью TF-IDF
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X_tfidf = tfidf_vectorizer.fit_transform(X)
+
+# Кодирование меток категорий
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+# Создание нейронной сети
+model = Sequential([
+    Dense(512, activation='relu', input_shape=(X_tfidf.shape[1],)),
+    Dropout(0.5),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(len(label_encoder.classes_), activation='softmax')
+])
+
+# Компиляция модели
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=Adam(learning_rate=0.001),
+              metrics=['accuracy'])
+
+# Обучение модели на всем наборе данных
+model.fit(X_tfidf, y_encoded, epochs=10, batch_size=32)
+
+# Сохранение модели
+model.save("model.h5")
+print("Модель сохранена в файл model.h5")
