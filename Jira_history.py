b0a0d54e75f13e3c9efcca0b86984c781ed56f55
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-28 13:38:16 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,1 @@
-  File "adam.py", line 81, in <module>
-    loss, accuracy = model.evaluate(X_test_tfidf, y_test_encoded, verbose=0)
-  File "C:\Program Files\Python38\lib\site-packages\keras\src\utils\traceback_utils.py", line 70, in error_handler
-    raise e.with_traceback(filtered_tb) from None
-  File "C:\Program Files\Python38\lib\site-packages\tensorflow\python\framework\ops.py", line 6656, in raise_from_not_ok_status
-    raise core._status_to_exception(e) from None  # pylint: disable=protected-access
-tensorflow.python.framework.errors_impl.InvalidArgumentError: {{function_node __wrapped__SerializeManySparse_device_/job:localhost/replica:0/task:0/device:CPU:0}} indices[1] = [0,4766] is out of order. Many sparse ops require sorted indices.
-    Use `tf.sparse.reorder` to create a correctly ordered copy.
-
- [Op:SerializeManySparse] name:
+X_test_tfidf = tfidf_vectorizer.transform(X_test, vocabulary=tfidf_vectorizer.vocabulary_)
