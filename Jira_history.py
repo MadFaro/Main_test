efefa8ddc6afecb33a6644b7f125b9e5b85d8cfd
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-28 13:40:53 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,4 @@
-X_test_tfidf = tfidf_vectorizer.transform(X_test, vocabulary=tfidf_vectorizer.vocabulary_)
+Traceback (most recent call last):
+  File "test.py", line 72, in <module>
+    X_test_tfidf = tfidf_vectorizer.transform(X_test, vocabulary=tfidf_vectorizer.vocabulary_)
+TypeError: transform() got an unexpected keyword argument 'vocabulary'
