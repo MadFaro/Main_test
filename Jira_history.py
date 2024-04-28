e6fcb3d44e24f4c9bf4e556b397d815752345ecd
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-28 16:10:15 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import pickle
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
-
 
 model_neiro_1 = 256
 model_neiro_2 = 128
@@ -18,15 +17,28 @@ learning_r = 0.001
 batch_s = 16
 
 data = pd.read_excel("test.xlsx", sheet_name="Свод")
-X = data['MSG']
-y = data['CATEGORY']
+
+# Разделение на обучающую и тестовую выборки
+train_data, test_data = train_test_split(data, test_size=0.2, stratify=data['CATEGORY'])
+
+X_train = train_data['MSG']
+y_train = train_data['CATEGORY']
+
+X_test = test_data['MSG']
+y_test = test_data['CATEGORY']
 
 tfidf_vectorizer = TfidfVectorizer(max_features=5000)
-X_tfidf = tfidf_vectorizer.fit_transform(X)
-X_tfidf.sort_indices()
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+X_train_tfidf.sort_indices()
+X_test_tfidf.sort_indices()
 
 label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
+
+# Используем label_encoder только на обучающих данных, чтобы избежать утечки данных
+y_train_encoded = label_encoder.fit_transform(y_train)
+y_test_encoded = label_encoder.transform(y_test)
 
 with open('model/tfidf_vectorizer.pkl', 'wb') as f:
     pickle.dump(tfidf_vectorizer, f)
@@ -38,7 +50,7 @@ with open('model/param.txt', 'w') as f:
     print(model_neiro_1, model_neiro_2, model_f_act_1, model_f_act_2, model_last_sloi, learning_r, batch_s, file=f)
 
 model = Sequential([
-    Dense(model_neiro_1, activation=model_f_act_1, input_shape=(X_tfidf.shape[1],)),
+    Dense(model_neiro_1, activation=model_f_act_1, input_shape=(X_train_tfidf.shape[1],)),
     Dense(model_neiro_2, activation=model_f_act_2),
     Dense(len(label_encoder.classes_), activation=model_last_sloi)
 ])
@@ -51,15 +63,7 @@ early = EarlyStopping(monitor='loss', min_delta=learning_r, patience=5, verbose=
 check = ModelCheckpoint('model', monitor='loss', verbose=2, save_best_only=False, mode='auto', save_format='tf')
 callbacks = [early, check]
 
-model.fit(X_tfidf, y_encoded, epochs=1000, batch_size=batch_s, verbose=1, callbacks=callbacks)
-
-data_test = pd.read_excel("test1.xlsx", sheet_name="Свод")
-X_test = data_test['MSG']
-y_test = data_test['CATEGORY']
-
-X_test_tfidf = tfidf_vectorizer.transform(X_test)
-X_test_tfidf.sort_indices()
-y_test_encoded = label_encoder.transform(y_test)
+model.fit(X_train_tfidf, y_train_encoded, epochs=1000, batch_size=batch_s, verbose=1, callbacks=callbacks)
 
 model = load_model('model')
 loss, accuracy = model.evaluate(X_test_tfidf, y_test_encoded, verbose=0)
