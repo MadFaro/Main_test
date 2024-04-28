0f0c927eb6650112142e1cf095942f2c61b47a0c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-28 11:36:13 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,2 +1,1 @@
-2024-04-28 11:23:50.684066: I tensorflow/core/platform/cpu_feature_guard.cc:182] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
-To enable the following instructions: SSE SSE2 SSE3 SSE4.1 SSE4.2 AVX AVX2 FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags
+
