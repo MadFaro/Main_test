6055791d380e594c56e9dd8aba77f7ac82de02e7
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-29 14:32:55 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,7 +1,1 @@
-from tensorflow.keras.callbacks import TensorBoard
-
-# Создание обратного вызова TensorBoard
-tensorboard_callback = TensorBoard(log_dir='./logs', histogram_freq=1)
-
-# Запуск обучения модели с использованием обратного вызова TensorBoard
-model.fit(X_train, y_train, epochs=10, callbacks=[tensorboard_callback])
+tensorboard --logdir=./logs
