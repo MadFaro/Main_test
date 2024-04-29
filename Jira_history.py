c70c657013bdbced4262a8731ddd6443fcbc3ac0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-29 14:26:40 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,3 +1,6 @@
+from tensorflow.keras.utils import plot_model
+
+# Построение модели
 model = Sequential([
     Dense(len(label_encoder.classes_), activation='linear', input_shape=(X_train_tfidf.shape[1],)),
     Dense(256, activation='relu'),
@@ -5,3 +8,6 @@ model = Sequential([
     Dense(64, activation='tanh'),
     Dense(len(label_encoder.classes_), activation='softmax')
 ])
+
+# Сохранение визуализации модели в файл 'model_architecture.jpg'
+plot_model(model, to_file='model_architecture.jpg', show_shapes=True)
