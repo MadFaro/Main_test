de3ce50755df915f206195c716a05bf7c308947a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-29 19:38:39 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,5 @@
-tensorboard --logdir=./logs
+Epoch 32: loss improved from 0.10076 to 0.09981, saving model to model
+213/213 [==============================] - 5s 25ms/step - loss: 0.0998 - accuracy: 0.9616
+Epoch 32: early stopping
+Loss на тестовой выборке: 0.41985389590263367
+Accuracy на тестовой выборке: 0.9030311703681946
