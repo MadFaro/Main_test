eaa3f665104546fc09b8afa7f857f000f73b11c1
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-30 12:29:25 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,3 @@
-
+        # Записываем данные в файл по мере их обработки
+        df = pd.DataFrame({'MSG': [text, augmented_text_translate], 'CATEGORY': [category, category]})
+        df.to_excel(writer, index=False, header=False, startrow=len(df) + 1 if len(df) > 0 else 0)
