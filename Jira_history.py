9fbad97834e7507c42fb5a2c19539694aacbf05e
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-04-30 11:57:34 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,44 +1,47 @@
+Special tokens have been added in the vocabulary, make sure the associated word embeddings are fine-tuned or trained.
+Token indices sequence length is longer than the specified maximum sequence length for this model (2244 > 512). Running this sequence through the model will result in indexing errors
+
 import pandas as pd
-from googletrans import Translator
-import random
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-# Загрузка данных из файла
-data = pd.read_excel("test.xlsx", sheet_name="Свод")
+device = 'cpu'
+model_name = 'trans/'
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+model.to(device)
+tokenizer = T5Tokenizer.from_pretrained(model_name)
 
-# Выделение текстовых данных и соответствующих им категорий
+
+data = pd.read_excel("test1.xlsx", sheet_name="Свод")
 texts = data['MSG']
 categories = data['CATEGORY']
 
-# Инициализация объекта Translator для перевода
-translator = Translator()
 
-# Функция для аугментации текста: перевод на английский, испанский и обратно на русский
 def augment_text_translate(text):
     # Перевод текста на английский
-    translated_en = translator.translate(text, dest='en').text
-    # Перевод текста на испанский
-    translated_es = translator.translate(translated_en, dest='es').text
-    # Перевод текста обратно на русский
-    translated_ru = translator.translate(translated_es, dest='ru').text
-    return translated_ru
+    prefix_1 = 'translate to en: '
+    src_text_1 = prefix_1 + text
+    input_ids_1 = tokenizer(src_text_1, return_tensors="pt")
+    generated_tokens_1 = model.generate(**input_ids_1.to(device))
+    result_1 = tokenizer.batch_decode(generated_tokens_1, skip_special_tokens=True)
+    # Перевод текста на русский
+    prefix_2 = 'translate to ru: '
+    src_text_2 = prefix_2 + result_1[0]
+    input_ids_2 = tokenizer(src_text_2, return_tensors="pt")
+    generated_tokens_2 = model.generate(**input_ids_2.to(device))
+    result_2 = tokenizer.batch_decode(generated_tokens_2, skip_special_tokens=True)
+    return result_2
 
-# Список для хранения аугментированных текстов и соответствующих им категорий
 augmented_texts = []
 augmented_categories = []
 
-# Проход по каждой паре текст-категория и создание аугментированных данных
 for text, category in zip(texts, categories):
-    # Оригинальный текст и его категория
     augmented_texts.append(text)
     augmented_categories.append(category)
     
-    # Аугментация текста: перевод на английский, испанский и обратно на русский
     augmented_text_translate = augment_text_translate(text)
     augmented_texts.append(augmented_text_translate)
     augmented_categories.append(category)
 
-# Создание нового DataFrame с исходными и аугментированными данными
 augmented_data = pd.DataFrame({'MSG': augmented_texts, 'CATEGORY': augmented_categories})
 
-# Сохранение аугментированных данных в новый файл
 augmented_data.to_excel("augmented_data.xlsx", index=False)
