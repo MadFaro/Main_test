40e478059cf7b3e19a221b785162c22591a8f5f6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-04 11:38:59 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,34 @@
+Traceback (most recent call last):
+  File "kan.py", line 7, in <module>
+    from kan import *
+  File "C:\Users\TologonovAB\Desktop\Модель для общей линии\kan.py", line 45, in <module>
+    model = KAN(width=[2,5,1], grid=5, k=3, seed=0)
+NameError: name 'KAN' is not defined
 
+
+__init__.py
+import torch
+from .KAN import *
+
+torch.set_default_dtype(torch.float64)
+
+KAN.py
+import torch
+import torch.nn as nn
+import numpy as np
+from .KANLayer import *
+from .Symbolic_KANLayer import *
+from .LBFGS import *
+import os
+import glob
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import random
+import copy
+
+
+class KAN(nn.Module):
+    '''
+    KAN class
+    
+    Attributes:
