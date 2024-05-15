ee32f6f71832b9fb7c493279c86b281de50329fb
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-15 21:02:07 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,13 @@
+from pywebio.platform.tornado_http import start_server
+from pywebio import STATIC_PATH
 
+# Путь к локальным статическим файлам PyWebIO
+LOCAL_STATIC_PATH = '/путь/к/папке/с/статическими/ресурсами/PyWebIO'
+
+# Запуск сервера PyWebIO с использованием локальных статических файлов
+start_server(
+    lambda: None, 
+    static_dir=STATIC_PATH, 
+    use_builtin_static=False, 
+    local_static_dir=LOCAL_STATIC_PATH
+)
