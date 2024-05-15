2f1d5aeddd8a6babfe72956949b8d52618247bc0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-15 20:38:10 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,38 @@
-
+DEBUG:asyncio:Using proactor: IocpProactor
+Running on http://localhost:50908/
+INFO:tornado.access:200 GET / (::1) 2.00ms                                                                              INFO:tornado.access:304 GET / (::1) 1.00ms
+INFO:tornado.access:200 GET /?_pywebio_cdn=false (::1) 1.00ms
+INFO:tornado.access:200 GET /css/markdown.min.css (::1) 73.38ms
+INFO:tornado.access:200 GET /css/toastify.min.css (::1) 3.09ms
+INFO:tornado.access:200 GET /css/bootstrap-select.min.css (::1) 5.09ms
+INFO:tornado.access:200 GET /css/bs-theme/yeti.min.css (::1) 9.08ms
+INFO:tornado.access:200 GET /css/app.css?v=1.8.3 (::1) 10.08ms                                                          INFO:tornado.access:200 GET /js/mustache.min.js (::1) 12.02ms
+INFO:tornado.access:200 GET /css/codemirror.min.css (::1) 4.00ms
+INFO:tornado.access:200 GET /js/codemirror.min.js (::1) 5.99ms
+INFO:tornado.access:200 GET /codemirror/addons.js (::1) 10.00ms
+INFO:tornado.access:200 GET /js/prism.min.js (::1) 12.00ms
+INFO:tornado.access:200 GET /js/FileSaver.min.js (::1) 14.00ms
+INFO:tornado.access:200 GET /js/jquery.min.js (::1) 18.21ms                                                             INFO:tornado.access:200 GET /js/popper.min.js (::1) 5.20ms
+INFO:tornado.access:200 GET /js/bootstrap.min.js (::1) 4.00ms
+INFO:tornado.access:200 GET /js/toastify.min.js (::1) 6.00ms
+INFO:tornado.access:200 GET /js/bs-custom-file-input.min.js (::1) 8.01ms
+INFO:tornado.access:200 GET /js/purify.min.js (::1) 10.01ms
+INFO:tornado.access:200 GET /js/bootstrap-select.min.js (::1) 12.01ms
+INFO:tornado.access:200 GET /js/pywebio.min.js?v=1.8.3 (::1) 8.40ms
+INFO:tornado.access:200 GET /js/require.min.js (::1) 3.00ms
+WARNING:tornado.access:404 GET /sm/40f738e33ed5dbe7907b48c3be4b63e977eab6cb49c8df4f76f3edc3f1f2fb0d.map (::1) 1.00ms
+WARNING:tornado.access:404 GET /js/FileSaver.min.js.map (::1) 1.00ms
+WARNING:tornado.access:404 GET /js/popper.min.js.map (::1) 0.00ms
+WARNING:tornado.access:404 GET /js/bootstrap.min.js.map (::1) 1.00ms
+WARNING:tornado.access:404 GET /js/bs-custom-file-input.min.js.map (::1) 1.01ms
+WARNING:tornado.access:404 GET /js/pywebio.min.js.map (::1) 0.99ms
+WARNING:tornado.access:404 GET /sm/b2692762f762f02a544ce708819ce22427514c155203d0627f14174806ee9f38.map (::1) 1.00ms
+INFO:tornado.access:101 GET /?app=index&session=NEW (::1) 1.00ms
+DEBUG:pywebio.platform.adaptor.ws:WebSocket opened
+DEBUG:pywebio.session.coroutinebased:Task[_start_main_task-9PxeuA84Nm] created
+DEBUG:pywebio.platform.adaptor.ws:session id: 9NSv2eiSK8uAPZPVAeHE5KId
+Привет
+DEBUG:pywebio.session.coroutinebased:del self.coros[_start_main_task-9PxeuA84Nm]
+DEBUG:pywebio.session.coroutinebased:Task[_start_main_task-9PxeuA84Nm] closed
+DEBUG:pywebio.session.coroutinebased:Task[_start_main_task-9PxeuA84Nm] finished
+DEBUG:pywebio.platform.adaptor.ws:WebSocket closed
