ad185af31ec086bd875411ae3367b332d69583aa
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-15 20:52:21 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,21 @@
-
+GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/app.css?v=1.8.3 net::ERR_CONNECTION_TIMED_OUT
+localhost/:14 
+        
+        
+       GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/toastify.min.css net::ERR_CONNECTION_TIMED_OUT
+localhost/:15 
+        
+        
+       GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/bs-theme/default.min.css net::ERR_CONNECTION_TIMED_OUT
+localhost/:16 
+        
+        
+       GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/bootstrap-select.min.css net::ERR_CONNECTION_TIMED_OUT
+localhost/:12 
+        
+        
+       GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/markdown.min.css net::ERR_CONNECTION_TIMED_OUT
+localhost/:13 
+        
+        
+       GET https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.8.3/css/codemirror.min.css net::ERR_CONNECTION_TIMED_OUT
