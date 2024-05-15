dc3473fd57c24a0ce3b919b487d38f49e44630a4
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-15 10:50:51 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,16 +1,2 @@
-SELECT
-    s.username,
-    s.sid,
-    s.serial#,
-    SUM(ss.value) AS cpu_usage
-FROM
-    v$session s
-    JOIN v$sesstat ss ON s.sid = ss.sid
-    JOIN v$statname st ON ss.statistic# = st.statistic#
-WHERE
-    st.name = 'CPU used by this session'
-GROUP BY
-    s.username, s.sid, s.serial#
-ORDER BY
-    cpu_usage DESC;
+
 
