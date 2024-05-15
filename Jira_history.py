dcc0747022e75e8daeec2eff0c964c7d95bb19eb
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-15 20:30:44 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,26 +1,4 @@
-C:\Users\TologonovAB\Desktop\logging-0.4.9.6>Python setup.py install
-running install
-running build
-running build_py
-running install_lib
-byte-compiling C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\config.py to config.cpython-38.pyc
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\config.py", line 216
-    raise NotImplementedError, "listen() needs threading to work"
-                             ^
-SyntaxError: invalid syntax
-
-byte-compiling C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\handlers.py to handlers.cpython-38.pyc
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\handlers.py", line 757
-    print "The Python Win32 extensions for NT (service, event "\
-          ^
-SyntaxError: invalid syntax
-
-byte-compiling C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\__init__.py to __init__.cpython-38.pyc
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging\__init__.py", line 618
-    raise NotImplementedError, 'emit must be implemented '\
-                             ^
-SyntaxError: invalid syntax
-
-running install_egg_info
-Removing C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging-0.4.9.6-py3.8.egg-info
-Writing C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\Lib\site-packages\logging-0.4.9.6-py3.8.egg-info
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    start_server(hello_world, host='127.0.0.1', port=8080)
