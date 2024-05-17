de45beb890d8d66bd2e9ef42926ff20088fa710f
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-17 16:43:55 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,2 +1,16 @@
-=INT(A1)*3600 + (A1-INT(A1))*100*3600
+from flask import Flask, request
+from pywebio import start_server
+from pywebio.output import put_text
 
+app = Flask(__name__)
+
+@app.route('/get_ip')
+def get_ip():
+    user_ip = request.remote_addr
+    return f'Your IP address is: {user_ip}'
+
+def main():
+    put_text("Check your IP address:").append(put_text('/get_ip'))
+
+if __name__ == '__main__':
+    start_server([main], port=8080, debug=True)
