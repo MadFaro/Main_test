adf16087f7ebbb44f471f7ce7ff30f1ac56f97f6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-29 13:35:30 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,35 +1,34 @@
 Function ErlangC(Servers As Double, Intensity As Double) As Double
     Dim B As Double, C As Double
     Dim Count As Long
+    Dim Numbers() As Double
     
     If Servers <= 0 Or Intensity <= 0 Then
         ErlangC = 0
         Exit Function
     End If
     
-    B = 1
+    ReDim Numbers(1 To Servers)
     For Count = 1 To Servers
-        B = B * (Intensity / Count)
+        Numbers(Count) = Count
     Next Count
     
-    C = B / (WorksheetFunction.SumProduct(WorksheetFunction.Power(Intensity, Application.Transpose(Evaluate("row(1:" & Servers & ")"))) / WorksheetFunction.Fact(Application.Transpose(Evaluate("row(1:" & Servers & ")")))) + B)
+    B = 1
+    For Count = 1 To Servers
+        B = B * (Intensity / Numbers(Count))
+    Next Count
+    
+    Dim PowerArray() As Double
+    Dim FactArray() As Double
+    ReDim PowerArray(1 To Servers)
+    ReDim FactArray(1 To Servers)
+    
+    For Count = 1 To Servers
+        PowerArray(Count) = Intensity
+        FactArray(Count) = Count
+    Next Count
+    
+    C = B / (WorksheetFunction.SumProduct(PowerArray) / WorksheetFunction.SumProduct(FactArray) + B)
     
     ErlangC = C
-End Function
-
-Function CalculateSL(CallsPerHour As Double, Agents As Double, Utilization As Double, AHT As Integer, SLTime As Integer) As Double
-    Dim TrafficIntensity As Double
-    Dim ErlangCValue As Double
-    
-    ' Расчет интенсивности трафика (Traffic Intensity)
-    TrafficIntensity = CallsPerHour * AHT / 3600
-    
-    ' Расчет количества операторов (Agents)
-    Agents = Application.WorksheetFunction.Ceiling(TrafficIntensity / Utilization, 1)
-    
-    ' Расчет Erlang C
-    ErlangCValue = ErlangC(Agents, TrafficIntensity)
-    
-    ' Расчет SL
-    CalculateSL = 1 - ErlangCValue * Exp(-SLTime / AHT)
 End Function
