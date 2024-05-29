f76725be124553281c42dbe330d5f528481c07b7
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-29 15:50:03 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,12 +1,12 @@
 WITH ranked_data AS (
     SELECT event_time, 
-           event_type_id,
+           type_ecent,
            call_id,
            LEAD(event_time) OVER (PARTITION BY call_id ORDER BY event_time) AS next_event_time,
            RANK() OVER (PARTITION BY call_id ORDER BY event_time) AS event_rank
     FROM your_existing_table_name -- замените your_existing_table_name на имя вашей таблицы
 )
-SELECT event_type_id,
+SELECT type_ecent,
        SUM(EXTRACT(DAY FROM (next_event_time - event_time)) * 86400 + 
            EXTRACT(HOUR FROM (next_event_time - event_time)) * 3600 +
            EXTRACT(MINUTE FROM (next_event_time - event_time)) * 60 +
@@ -14,5 +14,4 @@ SELECT event_type_id,
        COUNT(DISTINCT call_id) AS total_clients
 FROM ranked_data
 WHERE event_rank = 1
-GROUP BY event_type_id;
-
+GROUP BY type_ecent;
