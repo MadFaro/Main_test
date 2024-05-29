86afca14a6c4c112654bf94f53419d6b8f8e8b99
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-05-29 15:50:37 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,17 +1,5 @@
-WITH ranked_data AS (
-    SELECT event_time, 
-           type_ecent,
-           call_id,
-           LEAD(event_time) OVER (PARTITION BY call_id ORDER BY event_time) AS next_event_time,
-           RANK() OVER (PARTITION BY call_id ORDER BY event_time) AS event_rank
-    FROM your_existing_table_name -- замените your_existing_table_name на имя вашей таблицы
-)
-SELECT type_ecent,
-       SUM(EXTRACT(DAY FROM (next_event_time - event_time)) * 86400 + 
-           EXTRACT(HOUR FROM (next_event_time - event_time)) * 3600 +
-           EXTRACT(MINUTE FROM (next_event_time - event_time)) * 60 +
-           EXTRACT(SECOND FROM (next_event_time - event_time))) AS total_time,
-       COUNT(DISTINCT call_id) AS total_clients
-FROM ranked_data
-WHERE event_rank = 1
-GROUP BY type_ecent;
+ORA-30076: неверное поле выборки для источника выборки
+30076. 00000 -  "invalid extract field for extract source"
+*Cause:    The extract source does not contain the specified extract field.
+*Action:
+Error at Line: 11 Column: 70
