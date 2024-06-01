1383e054957ee8021e17c659d1686992f5fd377a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-01 22:16:26 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,3 +1,4 @@
 img_des = open('img/logo2.png', 'rb').read()
               put_image(img_des, width='auto', height='auto').style('place-self: center;'),
               put_html('<br><br>'),
+&#x2714
