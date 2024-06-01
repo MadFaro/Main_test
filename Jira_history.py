13510232cb2ec7bbc0ae3488a9b9f6d907f22ad6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-01 14:30:38 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,1 @@
-import sqlite3
 
-db_path = r'C:\Users\tolog\Desktop\shop_app\Convert\db\shop.db'
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
-cursor.execute('DELETE FROM session')
-conn.commit()
-conn.close()
-
-print("Все записи из таблицы 'session' успешно удалены.")
