9fa06c561412da8f8d7147e24e106fac6be51758
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-06 16:41:58 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,10 +1,1 @@
-            imgs = await file_upload("Select some pictures:", accept="image/*", multiple=False)
-            image = Image.open(BytesIO(imgs['content']))
-            if image.width < 300:
-                image.save(f"img/{imgs['filename']}")
-            else:
-                new_width = 300
-                aspect_ratio = image.height / image.width
-                new_height = int(new_width * aspect_ratio)
-                resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
-                resized_image.save(f"img/{imgs['filename']}")
+
