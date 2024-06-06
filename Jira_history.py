5f703551cad52b4ac87ece8dfa45d73566b408ee
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-06 11:13:01 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,54 @@
-ORA-30076: неверное поле выборки для источника выборки
-30076. 00000 -  "invalid extract field for extract source"
-*Cause:    The extract source does not contain the specified extract field.
-*Action:
-Error at Line: 36 Column: 47
+-- Определяем временную таблицу для active_time
+WITH ActiveTime AS (
+    SELECT
+        operatorid,
+        threadid,
+        MIN(created) AS start_time,
+        MAX(created) AS end_time,
+        (MAX(created) - MIN(created)) * 24 * 60 * 60 AS active_seconds
+    FROM
+        chat_logs
+    WHERE
+        kind = 2
+    GROUP BY
+        operatorid,
+        threadid
+),
+-- Определяем временную таблицу для workload_time
+WorkloadTimeStep1 AS (
+    SELECT
+        operatorid,
+        threadid,
+        created,
+        LEAD(created, 1) OVER (PARTITION BY threadid, operatorid ORDER BY created) AS next_created
+    FROM
+        chat_logs
+    WHERE
+        kind = 2
+),
+WorkloadTime AS (
+    SELECT
+        operatorid,
+        threadid,
+        SUM(
+            CASE 
+                WHEN next_created IS NOT NULL 
+                THEN (TO_NUMBER(next_created - created) * 24 * 60 * 60) 
+                ELSE 0 
+            END
+        ) AS workload_seconds
+    FROM
+        WorkloadTimeStep1
+    GROUP BY
+        operatorid,
+        threadid
+)
+SELECT
+    a.operatorid,
+    (SUM(w.workload_seconds) / SUM(a.active_seconds)) * 100 AS net_agent_occupancy
+FROM
+    ActiveTime a
+JOIN
+    WorkloadTime w ON a.operatorid = w.operatorid AND a.threadid = w.threadid
+GROUP BY
+    a.operatorid;
