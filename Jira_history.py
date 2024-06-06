7b34df1962b0c700c96e2c770a50cba889073e8b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-06 16:27:55 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,10 @@
-
+from PIL import Image
+image = Image.open(BytesIO(imgs['content']))
+            if image.width < 300:
+                image.save(f"img/{imgs['filename']}")
+            else:
+                new_width = 300
+                aspect_ratio = image.height / image.width
+                new_height = int(new_width * aspect_ratio)
+                resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+                resized_image.save(f"img/{imgs['filename']}")
