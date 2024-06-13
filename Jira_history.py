33d7c84888695db01d1c7d90be01ad913bf05ea8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-13 17:30:28 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+# Текущая дата
+current_date = datetime.now()
+
+# Дата два месяца назад
+two_months_ago = current_date - relativedelta(months=2)
+
+# Первый день месяца два месяца назад
+first_day_of_month_two_months_ago = two_months_ago.replace(day=1)
+
+# Форматирование в строку для SQL-запроса
+formatted_date = first_day_of_month_two_months_ago.strftime('%d.%m.%Y')
+
+print(formatted_date)
 
