dabc4c04f26deec7d5c2a2eb6f14b3346e33c918
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-17 12:49:03 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,7 @@
+.btn-light:hover {
+    color: #222;
+    background-color: #dbdbdb00;
+    border-color: #d5d5d500;
+}
 
+put_button("⬆", onclick=lambda:run_js('window.scrollTo({top: 0, behavior: "smooth"});'), color='light').style('position:fixed;right:1%;top:95%;z-index:2147483647')
