460b8b5564f359b027bade88b78f5f037e58f076
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-19 13:36:42 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,5 +1,2 @@
-put_button("⬆", onclick=lambda:run_js('window.scrollTo({top: 0, behavior: "smooth"});'), color='light').style('position:fixed;right:0%;bottom:0%;z-index:21474836')
-
-.btn-light {
-            background-color: #eee0;
-            }
+def strikethrough(text):
+    return ''.join([char + '\u0336' for char in text])
