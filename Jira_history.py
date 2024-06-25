50000e56ef791fa23d1ed4c679d865d6e3123644
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-25 18:34:03 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,4 +1,1 @@
-sheet.PageSetup.Orientation = 2  # 2 для ландшафтной ориентации, 1 для портретной
-sheet.PageSetup.Zoom = False
-sheet.PageSetup.FitToPagesTall = False
 sheet.PageSetup.FitToPagesWide = 1 
