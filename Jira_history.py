128afbe7e44c65a5f1eb40b8dadb59d287626089
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-25 18:24:39 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,14 +1,21 @@
-Traceback (most recent call last):
-  File "w.py", line 11, in <module>
-    xlsx2html(wb, f)
-  File "C:\Python38\lib\site-packages\xlsx2html\core.py", line 380, in xlsx2html
-    wb = openpyxl.load_workbook(filepath, data_only=True)
-  File "C:\Python38\lib\site-packages\openpyxl\reader\excel.py", line 344, in load_workbook
-    reader = ExcelReader(filename, read_only, keep_vba,
-  File "C:\Python38\lib\site-packages\openpyxl\reader\excel.py", line 123, in __init__
-    self.archive = _validate_archive(fn)
-  File "C:\Python38\lib\site-packages\openpyxl\reader\excel.py", line 77, in _validate_archive
-    file_format = os.path.splitext(filename)[-1].lower()
-  File "C:\Python38\lib\ntpath.py", line 196, in splitext
-    p = os.fspath(p)
-TypeError: expected str, bytes or os.PathLike object, not Workbook
+import win32com.client
+
+# Задайте путь к вашему Excel файлу и имя выходного PDF файла
+excel_file = 'your_excel_file.xlsx'
+pdf_file = 'output.pdf'
+
+# Создайте экземпляр приложения Excel
+excel = win32com.client.Dispatch("Excel.Application")
+
+# Откройте рабочую книгу
+workbook = excel.Workbooks.Open(excel_file)
+
+# Задайте лист, который нужно сохранить в PDF
+sheet = workbook.Sheets[1]  # Измените индекс, если нужно сохранить другой лист
+
+# Сохраните лист в PDF
+sheet.ExportAsFixedFormat(0, pdf_file)
+
+# Закройте рабочую книгу и завершите приложение Excel
+workbook.Close(SaveChanges=False)
+excel.Quit()
