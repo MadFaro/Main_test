3de3127e656752c163a2cdc89cdf9c30b954266a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-25 18:38:14 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,5 @@
-sheet.PageSetup.FitToPagesWide = 1 
+# Задайте виртуальный PDF-принтер (например, Microsoft Print to PDF)
+excel.ActivePrinter = "Microsoft Print to PDF"
+
+# Путь для сохранения PDF файла
+sheet.PrintOut(PrintToFile=True, PrToFileName=pdf_file)
