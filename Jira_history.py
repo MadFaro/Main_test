b1a1b52fcf876a3456a89968442ada37e46cfa2d
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-27 21:16:25 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,2 +1,1 @@
-df_select[date_columns] = df_select[date_columns].apply(pd.to_datetime, errors='coerce', dayfirst=True)
-df_select[numeric_columns] = df_select[numeric_columns].apply(pd.to_numeric, errors='coerce')
+
