fb73f0549321bdb70860bc7c97d3818ce0f29236
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-28 10:45:24 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,12 +1,25 @@
- set DATE_BEGIN_CRIF_FIX=DATE_IN_WORK_FIX-
- (180-((extract (hour from DATE_BEGIN_CRIF_FIX)-17)*60+extract (minute from DATE_BEGIN_CRIF_FIX))+
- case when to_char(DATE_IN_WORK_FIX, ''Day'') in (''Воскресенье'', ''Суббота    '') 
- then (extract (hour from DATE_IN_WORK_FIX)-8)*60+extract (minute from DATE_IN_WORK_FIX)
- else (extract (hour from DATE_IN_WORK_FIX)-7)*60+extract (minute from DATE_IN_WORK_FIX) end)/24/60
- where extract (hour from (DATE_IN_WORK_FIX-DATE_BEGIN_CRIF_FIX))>2
- and extract (hour from (DATE_IN_WORK_FIX-DATE_BEGIN_CRIF_FIX))<18
- and extract (hour from DATE_BEGIN_CRIF_FIX)>=17
- and 180-((extract (hour from DATE_BEGIN_CRIF_FIX)-17)*60+extract (minute from DATE_BEGIN_CRIF_FIX))+
- case when to_char(DATE_IN_WORK_FIX, ''Day'') in (''Воскресенье'', ''Суббота    '') 
- then (extract (hour from DATE_IN_WORK_FIX)-8)*60+extract (minute from DATE_IN_WORK_FIX)
- else (extract (hour from DATE_IN_WORK_FIX)-7)*60+extract (minute from DATE_IN_WORK_FIX) end<180';
+select a.*,
+case when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=8 and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<8 then  trunc(DATE_BEGIN_CRIF)+8/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=20 then  trunc(DATE_BEGIN_CRIF)+1+8/24 
+ 
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=7 and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<7 then  trunc(DATE_BEGIN_CRIF)+7/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=20 then  trunc(DATE_BEGIN_CRIF)+1+7/24 
+  else null end as DATE_BEGIN_CRIF_fix,
+
+case when DATE_IN_WORK>=DATE_BEGIN_CRIF and DATE_IN_WORK<case when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=8 and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<8 then  trunc(DATE_BEGIN_CRIF)+8/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=20 then  trunc(DATE_BEGIN_CRIF)+1+8/24 
+ 
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=7 and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<7 then  trunc(DATE_BEGIN_CRIF)+7/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=20 then  trunc(DATE_BEGIN_CRIF)+1+7/24 
+  else null end then 
+  case when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<8 then trunc(DATE_BEGIN_CRIF)+8/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=8 then trunc(DATE_BEGIN_CRIF)+1+8/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))<7 then trunc(DATE_BEGIN_CRIF)+7/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day', 'NLS_DATE_LANGUAGE = ''RUSSIAN''') not in ('Воскресенье', 'Суббота    ') and extract (hour from cast(DATE_BEGIN_CRIF as timestamp))>=7 then trunc(DATE_BEGIN_CRIF)+1+7/24+1/(24*60) 
+   end
+   else DATE_IN_WORK end as DATE_IN_WORK_FIX
+ from analytics.kdi_ipoteka_final a
