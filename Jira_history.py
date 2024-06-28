181a63594cee46dfbee04513a52c54cbaf491f66
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-28 10:13:23 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,4 +1,25 @@
-case
-    when to_number(to_char(a.DATE_BEGIN_CRIF, 'HH24')) >= 7 and to_number(to_char(a.DATE_BEGIN_CRIF, 'HH24')) < 20 then 'Worktime (7:00 -20:00 МСК)'
-    else 'Night (20:00-7:00 МСК)'
-end as work_time
+select a.*,
+case when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=8 and extract (hour from DATE_BEGIN_CRIF)<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<8 then  trunc(DATE_BEGIN_CRIF)+8/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=20 then  trunc(DATE_BEGIN_CRIF)+1+8/24 
+ 
+ when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=7 and extract (hour from DATE_BEGIN_CRIF)<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<7 then  trunc(DATE_BEGIN_CRIF)+7/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=20 then  trunc(DATE_BEGIN_CRIF)+1+7/24 
+  else null end as DATE_BEGIN_CRIF_fix,
+
+case when DATE_IN_WORK>=DATE_BEGIN_CRIF and DATE_IN_WORK<case when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=8 and extract (hour from DATE_BEGIN_CRIF)<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<8 then  trunc(DATE_BEGIN_CRIF)+8/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=20 then  trunc(DATE_BEGIN_CRIF)+1+8/24 
+ 
+ when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=7 and extract (hour from DATE_BEGIN_CRIF)<20 then  DATE_BEGIN_CRIF  
+ when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<7 then  trunc(DATE_BEGIN_CRIF)+7/24 
+  when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=20 then  trunc(DATE_BEGIN_CRIF)+1+7/24 
+  else null end then 
+  case when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<8 then trunc(DATE_BEGIN_CRIF)+8/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day') in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=8 then trunc(DATE_BEGIN_CRIF)+1+8/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)<7 then trunc(DATE_BEGIN_CRIF)+7/24+1/(24*60) 
+  when to_char(DATE_BEGIN_CRIF, 'Day') not in ('Воскресенье', 'Суббота    ') and extract (hour from DATE_BEGIN_CRIF)>=7 then trunc(DATE_BEGIN_CRIF)+1+7/24+1/(24*60) 
+   end
+   else DATE_IN_WORK end as DATE_IN_WORK_FIX
+ from analytics.kdi_ipoteka_final a
