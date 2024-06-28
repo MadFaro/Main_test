ba4ea0be0c809529844da2fe507affb51d16270b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-28 18:01:27 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,21 @@
-
+create or replace PROCEDURE             "KUZ_SEND_MAIL"(addressee in varchar2, text in varchar2, theme in varchar2)
+is
+cnt number;
+begin
+  
+    SYS.SEND_MAIL_REL2(p_to => addressee,
+  p_from => 'an@an.ru',
+  p_subject => theme,
+  p_text_msg => text,
+  p_smtp_host => '10'); 
+  
+        exception
+        when others then                      
+                SYS.SEND_MAIL_REL2(p_to => 'an@an.ru',
+                  p_from => 'an@an.ru',
+                  p_subject => theme,
+                  p_text_msg => text,
+                  p_smtp_host => '10'); 
+        
+  
+end kuz_send_mail;
