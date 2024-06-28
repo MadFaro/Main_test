d4143445a02510f33f6d691e14727d971b8e3532
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-28 18:11:30 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,3 +1,1 @@
-No connection could be made because the target machine actively refused it
 
-Failed to send email. Error: [WinError 10060] A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
