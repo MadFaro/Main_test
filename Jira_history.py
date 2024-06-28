4a6c8408fa48af8045813362152dd06bce109bec
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-06-28 10:42:32 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,12 @@
-
+ set DATE_BEGIN_CRIF_FIX=DATE_IN_WORK_FIX-
+ (180-((extract (hour from DATE_BEGIN_CRIF_FIX)-17)*60+extract (minute from DATE_BEGIN_CRIF_FIX))+
+ case when to_char(DATE_IN_WORK_FIX, ''Day'') in (''Воскресенье'', ''Суббота    '') 
+ then (extract (hour from DATE_IN_WORK_FIX)-8)*60+extract (minute from DATE_IN_WORK_FIX)
+ else (extract (hour from DATE_IN_WORK_FIX)-7)*60+extract (minute from DATE_IN_WORK_FIX) end)/24/60
+ where extract (hour from (DATE_IN_WORK_FIX-DATE_BEGIN_CRIF_FIX))>2
+ and extract (hour from (DATE_IN_WORK_FIX-DATE_BEGIN_CRIF_FIX))<18
+ and extract (hour from DATE_BEGIN_CRIF_FIX)>=17
+ and 180-((extract (hour from DATE_BEGIN_CRIF_FIX)-17)*60+extract (minute from DATE_BEGIN_CRIF_FIX))+
+ case when to_char(DATE_IN_WORK_FIX, ''Day'') in (''Воскресенье'', ''Суббота    '') 
+ then (extract (hour from DATE_IN_WORK_FIX)-8)*60+extract (minute from DATE_IN_WORK_FIX)
+ else (extract (hour from DATE_IN_WORK_FIX)-7)*60+extract (minute from DATE_IN_WORK_FIX) end<180';
