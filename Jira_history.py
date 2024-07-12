9ddae27963ab0a1274ecee452a99a6b223812377
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-12 09:12:17 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,7 +1,3 @@
-ORA-00600: код внутренней ошибки, аргументы: [HO define: Long fetch], [], [], [], [], [], [], [], [], [], [], []
-00600. 00000 -  "internal error code, arguments: [%s], [%s], [%s], [%s], [%s], [%s], [%s], [%s], [%s], [%s], [%s], [%s]"
-*Cause:    This is the generic internal error number for Oracle program
-           exceptions. It indicates that a process has encountered a low-level,
-           unexpected condition. The first argument is the internal message
-           number. This argument and the database version number are critical in
-           identifying the root cause and the potential impact to your system.
+SELECT * 
+FROM your_table 
+WHERE ROWNUM <= 1000;
