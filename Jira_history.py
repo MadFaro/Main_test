9dda4e7cf5637a403d54cb159ade19894dc19b63
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-12 09:13:49 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,6 +1,3 @@
-ORA-02070: база данных BK_NIGHT не поддерживает ROWNUM в этом контексте
-02070. 00000 -  "database %s%s does not support %s in this context"
-*Cause:    The remote database does not support the named capability in
-           the context in which it is used.
-*Action:   Simplify the SQL statement.
-Error at Line: 8 Column: 7
+SELECT *
+FROM your_table@BK_NIGHT
+FETCH FIRST 1000 ROWS ONLY;
