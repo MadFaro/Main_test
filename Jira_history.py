3a1cf071d2e98255d424786dd6eafb54d6940711
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-16 17:33:21 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,31 +1,19 @@
-WITH tab_2 AS (
-    SELECT trunc(min(sysdate-1)) AS min_time, 
-           trunc(min(sysdate-1)) + interval '24' hour AS max_time
-    FROM dual
-),
-tab_3 AS (
-    SELECT min_time + (LEVEL - 1)/24 AS hour_slot
-    FROM tab_2
-    CONNECT BY min_time + (LEVEL - 1)/24 < max_time
-),
-employee_schedules AS (
-    SELECT
-        FIO,
-        TO_DATE(START_SCHEDULE, 'HH24:MI') AS START_TIME,
-        TO_DATE(END_SCHEDULE, 'HH24:MI') AS END_TIME,
-        DATE_SCHEDULE
-    FROM ANALYTICS.TOLOG_OPERATORS_SCHEDULE
-    WHERE DATE_SCHEDULE >= trunc(sysdate-1)
-)
-SELECT
-    sh.hour_slot,
-    COUNT(es.FIO) AS EMPLOYEE_COUNT
-FROM
-    tab_3 sh
-LEFT JOIN employee_schedules es
-    ON sh.hour_slot BETWEEN es.DATE_SCHEDULE + (TO_NUMBER(TO_CHAR(es.START_TIME, 'HH24')) / 24) 
-    AND es.DATE_SCHEDULE + (TO_NUMBER(TO_CHAR(es.END_TIME, 'HH24')) / 24)
-GROUP BY
-    sh.hour_slot
-ORDER BY
-    sh.hour_slot;
+FIO, PLATFORM, POS_JOB, PN, SCHEDULE, MANAGER_FIO, DTM, DURATION, WORKDAY
+Баюнкина Дарья Николаевна	Чат	3	5\2	11:00-20:00	Рахимова Элина	15.07.24 00:00:00	9	Yes
+Лапухина Анна Владимировна	Чат	1	5\2	10:00-19:00	Каплин Даниил	15.07.24 00:00:00	9	Yes
+Шахман Любовь Борисовна 	Чат	1	5\2	10:00-19:00	Каплин Даниил	15.07.24 00:00:00	9	Yes
+Мурзагалиева Елена Николаевна	Чат	7	5\2	11:00-20:00	Шарипова Ирина	15.07.24 00:00:00	9	Yes
+Ахметшин Азат Рустамович 	Чат	7	5\2	09:00-18:00	Шарипова Ирина	15.07.24 00:00:00	9	Yes
+Ульянов Михаил Витальевич	Чат	4	5\2	12:00-21:00	Тутаркова Валентина	15.07.24 00:00:00	9	Yes
+Суяргулова Камила Ильмировна 	Чат	1	5\2	12:00-21:00	Каплин Даниил	15.07.24 00:00:00	9	Yes
+Зиннатулуллина Эльвина Наркисовна 	Чат		5\2	09:00-18:00	Тартынская Екатерина	15.07.24 00:00:00	9	Yes
+
+FIO	CLOB
+PLATFORM	CLOB
+POS_JOB	CLOB
+PN	CLOB
+SCHEDULE	CLOB
+MANAGER_FIO	CLOB
+DTM	DATE
+DURATION	NUMBER(19,0)
+WORKDAY	CLOB
