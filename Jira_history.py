4e93b833733f95e2ca3385c374247d332c0207da
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-16 17:28:04 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,11 +1,13 @@
-WITH tab_2 as (
-select trunc(min(sysdate-1)) as min_time, 
-trunc(min(sysdate-1)) + interval '24' hour as max_time
-from dual),
-tab_3 as (
-select min_time + (level - 1)/24 as hour_slot
-from tab_2
-connect by min_time + (level - 1)/24 < max_time),
+WITH tab_2 AS (
+    SELECT trunc(min(sysdate-1)) AS min_time, 
+           trunc(min(sysdate-1)) + interval '24' hour AS max_time
+    FROM dual
+),
+tab_3 AS (
+    SELECT min_time + (LEVEL - 1)/24 AS hour_slot
+    FROM tab_2
+    CONNECT BY min_time + (LEVEL - 1)/24 < max_time
+),
 employee_schedules AS (
     SELECT
         FIO,
@@ -13,7 +15,7 @@ employee_schedules AS (
         TO_DATE(END_SCHEDULE, 'HH24:MI') AS END_TIME,
         DATE_SCHEDULE
     FROM ANALYTICS.TOLOG_OPERATORS_SCHEDULE
-    where DATE_SCHEDULE >= trunc(sysdate-1)
+    WHERE DATE_SCHEDULE >= trunc(sysdate-1)
 )
 SELECT
     sh.hour_slot,
@@ -21,16 +23,9 @@ SELECT
 FROM
     tab_3 sh
 LEFT JOIN employee_schedules es
-    ON sh.hour_slot BETWEEN es.DATE_SCHEDULE + (EXTRACT(HOUR FROM es.START_TIME) / 24) 
-    AND es.DATE_SCHEDULE + (EXTRACT(HOUR FROM es.END_TIME) / 24)
+    ON sh.hour_slot BETWEEN es.DATE_SCHEDULE + (TO_NUMBER(TO_CHAR(es.START_TIME, 'HH24')) / 24) 
+    AND es.DATE_SCHEDULE + (TO_NUMBER(TO_CHAR(es.END_TIME, 'HH24')) / 24)
 GROUP BY
     sh.hour_slot
 ORDER BY
     sh.hour_slot;
-
-    ORA-30076: неверное поле выборки для источника выборки
-30076. 00000 -  "invalid extract field for extract source"
-*Cause:    The extract source does not contain the specified extract field.
-*Action:
-Error at Line: 24 Column: 67
-
