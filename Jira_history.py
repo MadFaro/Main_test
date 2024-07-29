20b8599bc0877986098aef8a4380c85bcd70fe65
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-29 14:32:18 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,8 @@
-
+put_collapse('Collapse title', [
+    'text',
+    put_markdown('~~Strikethrough~~'),
+    put_table([
+        ['Commodity', 'Price'],
+        ['Apple', '5.5'],
+    ])
+], open=True)
