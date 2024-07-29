a6d58d0140319f491b84d8a1582f3a0b59702fa4
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-29 15:04:13 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,17 +1,1 @@
-async def my_rate(sdep, tab, fio, id):
-    filename = 'Команда № 1 Братский круг.pptx'
-    filepath = 'ppt/Команда № 1 Братский круг.pptx'
-    popup('ЗАДАНИЯ', [
-        put_collapse('Задание 1', [
-            put_table([
-                ['Команда', 'Презентация'],
-                ['БРАТСКИЙ КРУГ', put_button("Скачать", onclick=lambda: download_action(filepath, filename))]
-            ])
-        ], open=True)
-    ], size='large')
-
-async def download_action(filepath, filename):
-    """Асинхронная функция для скачивания файла"""
-    with open(filepath, 'rb') as file:
-        content = file.read()
-    return put_file(filename, content)
+put_button('Click to download', lambda: download('hello-world.txt', b'hello world!'))
