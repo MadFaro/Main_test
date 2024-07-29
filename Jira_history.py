98692b594ceecfb94d5397bad275381c0d3e8b7d
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-07-29 14:45:57 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,8 +1,1 @@
-put_collapse('Collapse title', [
-    'text',
-    put_markdown('~~Strikethrough~~'),
-    put_table([
-        ['Commodity', 'Price'],
-        ['Apple', '5.5'],
-    ])
-], open=True)
+put_file('hello-world.txt', content, 'download me')
