56828d4ed3f16da7368cbc2a35db6c90d63e8b90
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-03 15:11:20 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,1 +1,17 @@
+    df_product = pd.read_sql(sql.sql_product, connect("Convert/db/shop.db"))
+    img_ban = open('img/ban.jpg', 'rb').read()
+    img_logo = open('img/logo.png', 'rb').read()
+    product_cards = [await render_product_card(product, sdep, tab, fio) for _, product in df_product.iterrows()]
 
+    sql_product =   """
+               SELECT 
+                    id,
+                    name,
+                    price,
+                    description,
+                    img,
+                    color,
+                    size,
+                    prom
+               FROM product
+               ORDER BY prom desc, price asc
