e0e821a9565cfc725912e8f55ff8ea781ff8b770
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-04 14:37:13 +0300

    Update Jira_history.py

--- Jira_history.py
+++ Jira_history.py
@@ -1,8 +1,18 @@
-    marquee_html = """
-    <marquee behavior="scroll" direction="left" scrollamount="5" style="font-size: 24px; color: blue; margin-bottom: 20px;">
-        Это пример бегущей строки! Здесь можно разместить любое сообщение.
-    </marquee>
-    """
+async def box_question(sdep, tab, fio, id):
+    popup('Задать вопрос',[
+        put_textarea(name='box_question', label='Напиши свой вопрос', maxlength=1000, minlength=100),
+        put_select(name='question_category', label='Выберите категорию:', options=['Заработная плата', 'Рабочие процессы', 'Корпоративная жизнь', 'Условия труда', 'Другое']),
+        put_html('<br>'),
+        put_button("ОТПРАВИТЬ ВОПРОС", onclick=lambda: box_question_send(sdep, tab, fio, id), color='danger', outline=True)
+        ], size='normal')
     
-    # Отображение бегущей строки
-    put_html(marquee_html)
+# Отправить вопрос
+async def box_question_send(sdep, tab, fio, id):
+    try:
+        close_popup()
+    except:
+        pass
+    question_categoru = await pin['question_category']
+    question_text = await pin['box_question']
+    BotDS.add_box_question(fio=fio, msg=question_text, state='Создан', category = question_categoru)
+    toast('Вопрос отправлен')
