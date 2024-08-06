bdc09147ad5b720d2cbb2a02452181c8277a84ca
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-06 23:23:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-tpl = '''
-<div class="product-inner">
-  <div class="product-wrap">
-    <img src="{{image_data_url}}" alt="Product Image">
-    <div class="actions">
-      {{& add_to_cart_button}}
-    </div>
-  </div>
-  <div class="product-info">
-    <h3 class="product-title"><a href="{{product_link}}">{{product_title}}</a></h3>
-    <span class="price">{{price}}</span>
-  </div>
-</div>
-'''
+
