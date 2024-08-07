24c0dd967c6adff96cf226e56aecdbcd65a12ab2
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-07 16:48:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-def create_login_chart(dates, login_counts):
-    line = (
-        Line()
-        .add_xaxis(dates)
-        .add_yaxis("Logins", login_counts, is_smooth=True)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="Logins Over Time"),
-            tooltip_opts=opts.TooltipOpts(trigger="axis"),
-            xaxis_opts=opts.AxisOpts(type_="category"),
-            yaxis_opts=opts.AxisOpts(type_="value"),
-        )
-    )
-    return line
-from pyecharts.charts import Line
-from pyecharts import options as opts
-    dates = df['date'].tolist()
-    login_counts = df['cnt'].tolist()
+chart = create_login_chart(dates, login_counts)
+    
+    # Отображение графика в веб-интерфейсе
+    put_html(chart.render_notebook())
