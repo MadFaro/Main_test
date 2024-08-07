b99ecab38b5a22f42f9ee0c9a844c3f9c27306a0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-07 16:41:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-SELECT 
-       date(date_time) AS date,
-       count(*) as cnt
-FROM log
-where login != 'ADMIN' and type_log ='Вход'
-group by date(date_time)
-order by date(date_time);
+def create_login_chart(dates, login_counts):
+    line = (
+        Line()
+        .add_xaxis(dates)
+        .add_yaxis("Logins", login_counts, is_smooth=True)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="Logins Over Time"),
+            tooltip_opts=opts.TooltipOpts(trigger="axis"),
+            xaxis_opts=opts.AxisOpts(type_="category"),
+            yaxis_opts=opts.AxisOpts(type_="value"),
+        )
+    )
+    return line
