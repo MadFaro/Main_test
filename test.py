feb8719b8afe72663b380a9d796336eed3ec27d9
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-07 17:01:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-def create_login_chart(dates, login_counts):
-    line = (
-        Line()
-        .add_xaxis(dates)
-        .add_yaxis("Logins", login_counts, is_smooth=True)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="Logins Over Time"),
-            tooltip_opts=opts.TooltipOpts(trigger="axis"),
-            xaxis_opts=opts.AxisOpts(type_="category"),
-            yaxis_opts=opts.AxisOpts(type_="value"),
-            datazoom_opts=[
-                opts.DataZoomOpts(type_="slider"),  # Добавление элемента зума
-                opts.DataZoomOpts(type_="inside")  # Внутренний зум для скролла
-            ],
-            legend_opts=opts.LegendOpts(is_show=True)  # Отображение легенды
-        )
-    )
-    return line
+SELECT 
+    date(date_time) AS date,
+    COUNT(CASE WHEN type_log = 'Сделал заказ' THEN 1 END) AS cnt_orders,
+    COUNT(CASE WHEN type_log = 'Отменил заказ' THEN 1 END) AS cnt_cancellations
+FROM log
+WHERE login != 'ADMIN'
+GROUP BY date(date_time)
+ORDER BY date(date_time);
