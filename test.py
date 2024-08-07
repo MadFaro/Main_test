cb60686d1decc008b8f54d5e8fdc58a4e089d8c9
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-07 16:52:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-chart = create_login_chart(dates, login_counts)
-    
-    # Отображение графика в веб-интерфейсе
-    put_html(chart.render_notebook())
+def create_login_chart(dates, login_counts):
+    line = (
+        Line()
+        .add_xaxis(dates)
+        .add_yaxis("Logins", login_counts, is_smooth=True)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="Logins Over Time"),
+            tooltip_opts=opts.TooltipOpts(trigger="axis"),
+            xaxis_opts=opts.AxisOpts(type_="category"),
+            yaxis_opts=opts.AxisOpts(type_="value"),
+            datazoom_opts=[
+                opts.DataZoomOpts(type_="slider"),  # Добавление элемента зума
+                opts.DataZoomOpts(type_="inside")  # Внутренний зум для скролла
+            ],
+            legend_opts=opts.LegendOpts(is_show=True)  # Отображение легенды
+        )
+    )
+    return line
