dfc669b292eaaf4cfdec4e5467b535daf2ec9e58
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-12 09:31:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import msoffcrypto
-import io
-
-# Откройте защищенный файл
-with open('защищенный_файл.xlsx', 'rb') as file:
-    decrypted = io.BytesIO()
-    office_file = msoffcrypto.OfficeFile(file)
-    office_file.load_key(password="2011")  # Укажите пароль
-    office_file.decrypt(decrypted)
-
-    # Сохраните файл без пароля
-    with open('файл_без_пароля.xlsx', 'wb') as output_file:
-        output_file.write(decrypted.getvalue())
 
