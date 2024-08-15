d2e0bd62f005924157545a477932cfc2c7f10e17
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-15 11:53:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-
+ORA-01830: шаблон формата даты завершается перед преобразованием всей строки ввода
+01830. 00000 -  "date format picture ends before converting entire input string"
+*Cause:    
+*Action:
