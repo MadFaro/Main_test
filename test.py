11405fcfb4a0afb2b0a90de27f642681eb9a9abb
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-15 14:09:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 copy_button_html = '''
 <button onclick="copyTextToClipboard()" style="background-color: transparent; color: orange; border: 2px solid orange; padding: 20px 20px 10px 20px; font-size: 16px; cursor: pointer; text-align: center; border-radius: 5px; position: relative; overflow: visible; transition: all 0.3s ease;">
     <div style="font-size: 24px; position: absolute; top: -20px; left: 50%; transform: translateX(-50%); transition: all 0.3s ease;">
-        ☎️
+        📧
     </div>
-    <span style="display: block;">9 999 999 99 99</span>
+    <span style="display: block;">name@mail.ru</span>
 </button>
 <script>
     function copyTextToClipboard() {
-        var text = "9 999 999 99 99";
+        var text = "name@mail.ru";
         navigator.clipboard.writeText(text).then(function() {
             alert("Текст скопирован: " + text);
         }, function(err) {
