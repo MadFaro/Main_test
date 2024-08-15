d8d7da7e94ad55a7f4cc55a7c329dcc9e65cb9dd
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-15 13:52:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-
+copy_button_html = '''
+<button onclick="copyTextToClipboard()">Копировать</button>
+<script>
+    function copyTextToClipboard() {
+        var text = "Пример текста";
+        navigator.clipboard.writeText(text).then(function() {
+            alert("Текст скопирован: " + text);
+        }, function(err) {
+            alert("Ошибка копирования: " + err);
+        });
+    }
+</script>
+'''
