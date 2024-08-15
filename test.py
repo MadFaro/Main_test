41ac8f03fbfca835e0c07792f510f531fb47b800
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-15 10:55:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+set app = createobject("excel.application")
 
+app.visible = false
+app.workbooks.open("C:\Users\TologonovAB\Desktop\DT\interim\interim.xlsx")
+app.ActiveWorkbook.Connections("Запрос — Светофор").Refresh
+app.workbooks("interim.xlsx").close(true)
+
+app.quit
