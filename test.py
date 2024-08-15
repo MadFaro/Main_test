c33162a4d4647e749728e9a30d75c4e4124e491a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-15 11:57:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-ORA-01830: шаблон формата даты завершается перед преобразованием всей строки ввода
-01830. 00000 -  "date format picture ends before converting entire input string"
-*Cause:    
-*Action:
+SELECT
+trunc(REPORT_MONTH, 'mm') as REPORT_MONTH, MOTIV_TABNUM, PRODUKT, COUNT(DISTINCT DOG) as dog_cnt
+FROM BALKHOVITIAV.VIEW_MOTIV_PRAIC_LICT_OPROS
+WHERE trunc(REPORT_MONTH, 'mm') >= add_months(trunc(sysdate-1, 'mm'),-3)
+GROUP BY trunc(REPORT_MONTH, 'mm'), MOTIV_TABNUM, PRODUKT
