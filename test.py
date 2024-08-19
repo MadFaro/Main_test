c04546fff88ed804f1d6ced6ceee4c156d3d6bf8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-19 15:49:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-
+tpl = '''
+<div class="product-wrap">
+    <div class="product-item">
+        {{& pywebio_output_parse}}  <!-- Вставка изображения -->
+    </div>
+    <div class="product-buttons">
+        {{& pywebio_output_parse}}  <!-- Вставка кнопки -->
+    </div>
+    <div class="product-title">
+        {{& pywebio_output_parse}}  <!-- Вставка названия продукта -->
+        {{& pywebio_output_parse}}  <!-- Вставка цены продукта -->
+    </div>
+</div>
+'''
