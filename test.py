86040ea1597e515386e23a8de2af6252505942a0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-19 15:54:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-"pywebio_output_parse":
+put_column([
+        # Вставляем изображение
+        put_image(image_url, width='100%'),
+
+        # Вставляем название продукта и цену
+        put_column([
+            put_text(product_name, inline=True),  # Название продукта
+            put_text(f"₽ {price}", inline=True),  # Цена продукта
+        ], size='auto'),
+
+        # Кнопка "В корзину"
+        put_button(button_text, onclick=button_action)
+    ], size='auto')
