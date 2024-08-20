005c1b8baaa1c169757cc7054ba7cb1c276da2bc
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-20 19:12:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-
+.transparent-text {
+  font-family: 'Rubik One', sans-serif;
+  font-size: 50px;
+  text-transform: uppercase;
+  background: #2E2F45;
+  display: table;
+  margin: 20px auto;
+  color: white;
+  border-radius: 5px;
+  mix-blend-mode: multiply;
+  padding: 5px 10px;
+}
+<p class="transparent-text">Прозрачный текст</p>
