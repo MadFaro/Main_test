6318eb6b5fd96f9bfa0866747996f663d144e9a5
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-20 15:29:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-with open(css_path, 'r') as f:
-        css_content = f.read()
 
-    # Подключаем CSS стили в приложении PyWebIO
-    put_html(f'<style>{css_content}</style>')
-put_html('<i class="fa-regular fa-window"></i> This is a check circle icon!')
