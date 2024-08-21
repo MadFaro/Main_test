1227d71020a9ff372d52d2c03dbf967f702e9e1b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-21 12:35:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-select trunc(DT, 'mm') as created, sum(case when FCR=1 then 1 else 0 end)/count(*) as fcr from (
-select 
-a.call_id, a.OPERATOR,
-a.DT,
-a.CLIENT_ID,
-COUNT(TRUNC(DT)) OVER(PARTITION BY TRUNC(DT), a.CLIENT_ID) AS FCR
-from ANALYTICS.KDI_SIEBEL_PAST a
-where a.DT>=date'2024-01-01' and a.CLIENT_ID is not null and a.TYPE = 'Вызов - входящий' and DIRECTION = 'ДДО')
-group by trunc(DT, 'mm')
-order by trunc(DT, 'mm')
+SELECT trunc(DT, 'mm') AS created, 
+       SUM(CASE WHEN FCR = 1 THEN 1 ELSE 0 END) / COUNT(*) AS fcr
+FROM (
+    SELECT 
+        a.call_id, 
+        a.OPERATOR, 
+        a.DT, 
+        a.CLIENT_ID,
+        COUNT(DISTINCT a.OPERATOR) OVER (PARTITION BY a.call_id) AS operator_count,
+        COUNT(TRUNC(DT)) OVER (PARTITION BY TRUNC(DT), a.CLIENT_ID) AS FCR
+    FROM 
+        ANALYTICS.KDI_SIEBEL_PAST a
+    WHERE 
+        a.DT >= DATE '2024-01-01' 
+        AND a.CLIENT_ID IS NOT NULL 
+        AND a.TYPE = 'Вызов - входящий' 
+        AND a.DIRECTION = 'ДДО'
+) 
+WHERE operator_count = 1 -- Исключаем звонки с переводом на другого оператора
+GROUP BY trunc(DT, 'mm')
+ORDER BY trunc(DT, 'mm');
