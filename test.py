2699e3cd56ac04925b16eab4118a78cda71e3b1b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-21 16:17:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-Classification Report:
-               precision    recall  f1-score   support
 
-           0       0.96      1.00      0.98       249
-           1       0.00      0.00      0.00        11
-
-    accuracy                           0.96       260
-   macro avg       0.48      0.50      0.49       260
-weighted avg       0.92      0.96      0.94       260
