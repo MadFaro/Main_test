195b889c8060eeddbed371a6e15075fb1472c5c6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-21 15:04:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-    border-bottom: 1px solid black;
-    transition: width 0.3s ease-in-out, left 0.3s ease-in-out; /* Анимация изменения ширины и позиции */
-}
 
-.centered-border:hover::after {
-    width: 100%;  /* Граница расширяется до полной ширины */
-    left: 0; /* Начало границы смещается влево, чтобы занять всю ширину */
-    transform: translateX(0); /* Убираем смещение */
-}
