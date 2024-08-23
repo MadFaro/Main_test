24693b3d0fa27bb65bf867eb85206d481653f1d3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-23 13:45:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+🚷 U+1F6B7 — No Pedestrians
+↩ U+21A9 — Left Arrow with Hook
+🏃 U+1F3C3 — Person Running
+➡️ U+27A1 — Right Arrow
+➖ U+2796 — Heavy Minus Sign
+🅾️ U+1F17E — O Button (often used to denote exit or cancel)
+⬅️ U+2B05 — Left Arrow
+❎ U+274E — Negative Squared Cross Mark
+🔓 U+1F513 — Open Lock
+⏹️ U+23F9 — Stop Button
 
+🔽 U+1F53D — Downwards Triangle
+🚫 U+1F6AB — Prohibited
+🔒 U+1F512 — Locked
+🔻 U+1F53B — Red Triangle Pointed Down
+🡸 U+1F868 — Leftwards Arrow
+🗙 U+1F5D9 — Cancellation X
+↘️ U+21A4 — Downwards Arrow with Right Hook
+⤶ U+21B6 — Anticlockwise Top Semicircle Arrow
+🔲 U+1F532 — Black Square Button
+🆑 U+1F191 — CL Button (used for "clear" or "cancel")
