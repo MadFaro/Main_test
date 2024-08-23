fdc7ac056d6478bdabf90768902a7e0f9b431b36
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-23 10:33:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+/* Подсветка верхнего левого угла */
+.card::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50px; /* ширина подсветки */
+  height: 50px; /* высота подсветки */
+  background: linear-gradient(to bottom right, rgba(255, 255, 0, 0.5), transparent);
+}
 
+/* Подсветка нижнего правого угла */
+.card::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 50px; /* ширина подсветки */
+  height: 50px; /* высота подсветки */
+  background: linear-gradient(to top left, rgba(255, 255, 0, 0.5), transparent);
+}
