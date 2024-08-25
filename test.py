0ac0ef57e3cea8447f3edfd918a7e42937c8f793
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-25 21:15:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
+          tpl = '''
+        <div style="text-align: center;">
+        <div class="bell">🔔
+              {{#contents}}
+                  {{& pywebio_output_parse}}
+              {{/contents}}
+        </div>
+        <div class="bell_cnt">1</div>
+        <style>
+        .bell {
+            font-size: 30px;
+            width:50px;
+            display: inline-block;
+            margin: auto;
+            animation: ring 1s ease-in-out infinite;
+        }
 
+        @keyframes ring {
+            0% { transform: rotate(0); }
+            20% { transform: rotate(-15deg); }
+            40% { transform: rotate(10deg); }
+            60% { transform: rotate(-5deg); }
+            80% { transform: rotate(5deg); }
+            100% { transform: rotate(0); }
+        }
+    </style>
