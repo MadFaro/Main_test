3a5346d7599c436071ac1d77a74a60c82de01be5
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-25 20:29:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-from ftplib import FTP
-
-ftp = FTP('your_server_ip')
-ftp.login(user='username', passwd='password')
-
-# Загрузка файла
-with open('local_file.txt', 'rb') as f:
-    ftp.storbinary('STOR remote_file.txt', f)
-
-ftp.quit()
 
