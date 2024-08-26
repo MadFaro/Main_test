da6ada8e85b1f73f7fe1d98725e896eb25cd2020
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-26 09:29:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import phonenumbers
-from phonenumbers import geocoder
-
-# Пример номера телефона
-phone_number = "+79876543210"
-
-# Парсинг номера телефона
-parsed_number = phonenumbers.parse(phone_number, "RU")  # "RU" - страна по умолчанию (Россия)
-
-# Определение региона
-region = geocoder.description_for_number(parsed_number, "ru")  # "ru" - код языка
-
-print(f"Регион: {region}")
 
