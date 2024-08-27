04433620277090bb6d8ffadb3a0866e7cbe6cba0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-27 13:50:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-
+cursor = conn.cursor()
+cursor.execute("select VERSION from ANALYTICS.TOLOG_TEST1 where PLATFORM = 'ANDROID' and APP_ACT = 1")
+# Переменная с текстом по умолчанию
+last_app_android = cursor.fetchall()[0]
+cursor.execute("select VERSION from ANALYTICS.TOLOG_TEST1 where PLATFORM = 'IOS' and APP_ACT = 1")
+last_app_ios = cursor.fetchall()[0]
+conn.close()
