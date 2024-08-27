77ce2998e91ae8e2bbb23022348933b901f7e1b7
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-27 14:09:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import cx_Oracle, datetime, win32com.client
 
-# Переменные с текстом по умолчанию для каждой платформы
-last_app_android = "1.1.1.1"
-last_app_ios = "2.2.2.2"
+conn = ""
+cursor = conn.cursor()
+cursor.execute("SELECT PLATFORM,VERSION FROM ANALYTICS.TOLOG_TEST1 WHERE PLATFORM IN ('ANDROID', 'IOS') AND APP_ACT = 1")
+result = cursor.fetchall()
+conn.close()
+
+# Обрабатываем результат
+last_app_android = 0.0
+last_app_ios = 0.0
+
+for row in result:
+    if row[0] == 'ANDROID':
+        last_app_android = row[1]
+    elif row[0] == 'IOS':
+        last_app_ios = row[1]
 
 # Функция для обновления поля "Enter Text" в зависимости от выбора платформы
 def update_entry_text(combo_box, entry):
@@ -18,14 +31,24 @@ def update_entry_text(combo_box, entry):
 
 # Функция для удаления старой версии
 def on_remove_button_click():
+    conn = ""
+    cursor = conn.cursor()
     platform = remove_combo_box.get()  # Получаем выбранную платформу для удаления
     user_input = remove_entry.get()  # Получаем текст для удаления
+    cursor.execute(f"update ANALYTICS.TOLOG_TEST1 set APP_ACT = 0 where platform = '{platform}' and version = '{user_input}'")
+    conn.commit()
+    conn.close()
     messagebox.showinfo("Remove Version", f"Version removed - Platform: {platform}, Text: {user_input}")
 
 # Функция для добавления новой версии
 def on_add_button_click():
+    conn = ""
+    cursor = conn.cursor()
     platform = add_combo_box.get()  # Получаем выбранную платформу для добавления
     user_input = add_entry.get()  # Получаем текст для добавления
+    cursor.execute(f"insert into ANALYTICS.TOLOG_TEST1 (platform, version, app_act) values ('{platform}', '{user_input}', 1)")
+    conn.commit()
+    conn.close()
     messagebox.showinfo("Add Version", f"Version added - Platform: {platform}, Text: {user_input}")
 
 # Создаем основное окно приложения
@@ -33,11 +56,11 @@ root = tk.Tk()
 root.title("Version Management App")
 
 # Устанавливаем фиксированный размер окна
-root.geometry("300x400")
+root.geometry("400x630")
 root.resizable(False, False)  # Отключаем возможность изменения размера окна
 
 # Секция для отображения актуальной версии приложения
-version_label = tk.Label(root, text="Latest Versions:\nANDROID: 4.543.3\nIOS: 5.323.2", font=("Arial", 10, "bold"))
+version_label = tk.Label(root, text=f"Latest Versions:\nANDROID: {last_app_android}\nIOS: {last_app_ios}", font=("Arial", 10, "bold"))
 version_label.pack(pady=10)
 
 # Разделитель после информации о версии
@@ -45,7 +68,7 @@ separator_label = tk.Label(root, text="---------------------------", font=("Aria
 separator_label.pack(pady=10)
 
 # Секция для удаления старой версии
-remove_label = tk.Label(root, text="Remove Old Version")
+remove_label = tk.Label(root, text="Remove Old Version",  font=("Arial", 10, "bold"))
 remove_label.pack(pady=10)
 
 remove_platform_label = tk.Label(root, text="Choose Platform:")
@@ -74,7 +97,7 @@ separator = tk.Label(root, text="---------------------------")
 separator.pack(pady=10)
 
 # Секция для добавления новой версии
-add_label = tk.Label(root, text="Add New Version")
+add_label = tk.Label(root, text="Add New Version",  font=("Arial", 10, "bold"))
 add_label.pack(pady=10)
 
 add_platform_label = tk.Label(root, text="Choose Platform:")
@@ -100,4 +123,3 @@ add_combo_box.bind("<<ComboboxSelected>>", lambda event: update_entry_text(add_c
 
 # Запускаем главное событие-цикл приложения
 root.mainloop()
-
