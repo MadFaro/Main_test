4cae7b1d9ad3db51c82b4509f0b098d12b3587f8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-27 14:30:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,1 @@
-The 'pathlib' package is an obsolete backport of a standard library package and is incompatible with PyInstaller. Please remove this package (located in C:\Python38\lib\site-packages) using
-    "C:\Python38\python.exe" -m pip uninstall pathlib
-then try again.
+
