da76700da96f1fc600f86e20982467cc7eff51cc
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-27 18:23:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-https://www.rustore.ru/catalog/app/ru.bankuralsib.mb.android/versions
+import requests
+
+
+url = 'https://www.rustore.ru/catalog/app/ru.bankuralsib.mb.android/versions'
+
+
+headers={
+'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+'accept-encoding':'gzip, deflate, br, zstd',
+'accept-language':'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+'connection':'keep-alive',
+'host':'www.rustore.ru',
+'sec-ch-ua':'"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+'sec-ch-ua-mobile':'?0',
+'sec-ch-ua-platform':'"Windows"',
+'sec-fetch-dest':'document',
+'sec-fetch-mode':'navigate',
+'sec-fetch-site':'none',
+'sec-fetch-user':'?1',
+'upgrade-insecure-requests':'1',
+'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'                  
+                                                        }
+
+conten = requests.get(url, headers=headers, verify=False)
+print(conten.content)
