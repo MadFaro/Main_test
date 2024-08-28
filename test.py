34663a78dc7a25c2a868bf2a3213a96e388ec1b1
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-28 14:21:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+import customtkinter as ctk
+
+# Установка темной темы
+ctk.set_appearance_mode("light")  # или "light"
+ctk.set_default_color_theme("dark-blue")  # также можно использовать "blue", "green", "dark-blue"
 
