47176102e987c20ccc6d1491bdca751abbdd5a79
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 19:26:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-import plotly.graph_objects as go
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import HoverTool
 import pandas as pd
-from pywebio.output import put_column, put_row, put_html, put_text, put_button, put_datatable, clear
+from pywebio.output import put_html, put_text, put_button, put_row, put_column, put_datatable
+from pywebio.platform import clear
 
 async def dashboard_user(df_user, sdep, tab, fio, id):
     try:
@@ -9,74 +12,47 @@ async def dashboard_user(df_user, sdep, tab, fio, id):
         pass
 
     def create_login_chart(dates, login_counts):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=dates, y=login_counts, mode='lines+markers', name='Кол-во'))
-        fig.update_layout(
-            title='Входы',
-            xaxis_title='Дата',
-            yaxis_title='Количество',
-            template='plotly_white',
-            margin=dict(l=20, r=20, t=40, b=20),
-            width=800,  # можно настроить ширину
-            height=400,  # и высоту графика
-            xaxis=dict(tickmode='linear'),
-            yaxis=dict(tickmode='linear')
-        )
-        return fig
+        p = figure(x_range=dates, title="Входы", plot_width=800, plot_height=400,
+                   toolbar_location=None, tools="hover", tooltips="@x: @y")
+        p.line(x=dates, y=login_counts, line_width=2, legend_label="Кол-во", line_color="blue")
+        p.circle(x=dates, y=login_counts, size=8, color="blue")
+        p.legend.location = "top_left"
+        p.xaxis.axis_label = "Дата"
+        p.yaxis.axis_label = "Количество"
+        return p
 
     def create_multi_chart(dates, shop, game, box):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=dates, y=shop, mode='lines+markers', name='Зашел в магазин'))
-        fig.add_trace(go.Scatter(x=dates, y=game, mode='lines+markers', name='Зашел в гейм'))
-        fig.add_trace(go.Scatter(x=dates, y=box, mode='lines+markers', name='Воспользовался боксом'))
-        fig.update_layout(
-            title='Сервисы',
-            xaxis_title='Дата',
-            yaxis_title='Количество',
-            template='plotly_white',
-            margin=dict(l=20, r=20, t=40, b=20),
-            width=800,
-            height=400,
-            xaxis=dict(tickmode='linear'),
-            yaxis=dict(tickmode='linear')
-        )
-        return fig
+        p = figure(x_range=dates, title="Сервисы", plot_width=800, plot_height=400,
+                   toolbar_location=None, tools="hover", tooltips="@x: @y")
+        p.line(x=dates, y=shop, line_width=2, legend_label="Зашел в магазин", line_color="green")
+        p.line(x=dates, y=game, line_width=2, legend_label="Зашел в гейм", line_color="orange")
+        p.line(x=dates, y=box, line_width=2, legend_label="Воспользовался боксом", line_color="red")
+        p.legend.location = "top_left"
+        p.xaxis.axis_label = "Дата"
+        p.yaxis.axis_label = "Количество"
+        return p
 
     def create_box_chart(dates, quest, offer, mood):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=dates, y=quest, mode='lines+markers', name='Задал вопрос'))
-        fig.add_trace(go.Scatter(x=dates, y=offer, mode='lines+markers', name='Предложил идею'))
-        fig.add_trace(go.Scatter(x=dates, y=mood, mode='lines+markers', name='Отправил настроение'))
-        fig.update_layout(
-            title='Бокс ОС',
-            xaxis_title='Дата',
-            yaxis_title='Количество',
-            template='plotly_white',
-            margin=dict(l=20, r=20, t=40, b=20),
-            width=800,
-            height=400,
-            xaxis=dict(tickmode='linear'),
-            yaxis=dict(tickmode='linear')
-        )
-        return fig
+        p = figure(x_range=dates, title="Бокс ОС", plot_width=800, plot_height=400,
+                   toolbar_location=None, tools="hover", tooltips="@x: @y")
+        p.line(x=dates, y=quest, line_width=2, legend_label="Задал вопрос", line_color="purple")
+        p.line(x=dates, y=offer, line_width=2, legend_label="Предложил идею", line_color="brown")
+        p.line(x=dates, y=mood, line_width=2, legend_label="Отправил настроение", line_color="pink")
+        p.legend.location = "top_left"
+        p.xaxis.axis_label = "Дата"
+        p.yaxis.axis_label = "Количество"
+        return p
 
     def create_orders_cancellations_chart(dates, order_counts, cancellation_counts):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=dates, y=order_counts, mode='lines+markers', name='Сделал'))
-        fig.add_trace(go.Scatter(x=dates, y=cancellation_counts, mode='lines+markers', name='Отменил'))
-        fig.update_layout(
-            title='Заказы',
-            xaxis_title='Дата',
-            yaxis_title='Количество',
-            template='plotly_white',
-            margin=dict(l=20, r=20, t=40, b=20),
-            width=800,
-            height=400,
-            xaxis=dict(tickmode='linear'),
-            yaxis=dict(tickmode='linear')
-        )
-        return fig
-
+        p = figure(x_range=dates, title="Заказы", plot_width=800, plot_height=400,
+                   toolbar_location=None, tools="hover", tooltips="@x: @y")
+        p.line(x=dates, y=order_counts, line_width=2, legend_label="Сделал", line_color="blue")
+        p.line(x=dates, y=cancellation_counts, line_width=2, legend_label="Отменил", line_color="red")
+        p.legend.location = "top_left"
+        p.xaxis.axis_label = "Дата"
+        p.yaxis.axis_label = "Количество"
+        return p
+    
     # Получение данных из базы данных
     df = pd.read_sql(sql.sql_dash_one, connect("Convert/db/shop.db"))
     df_online = pd.read_sql(sql.sql_online, connect("Convert/db/shop.db"))
@@ -98,26 +74,33 @@ async def dashboard_user(df_user, sdep, tab, fio, id):
     chart3 = create_box_chart(dates, cnt_quest, cnt_offer, cnt_mood)
     chart4 = create_orders_cancellations_chart(dates, cnt_orders, cnt_cancel)
 
+    # Получение HTML компонентов для вставки на страницу
+    script1, div1 = components(chart)
+    script2, div2 = components(chart2)
+    script3, div3 = components(chart3)
+    script4, div4 = components(chart4)
+
     # Отображение контента на странице
     put_column([
+
         put_row([
             None,
             put_text("Статистика").style("text-align: center; font-size: xx-large;"),
-            put_button("     back      ", onclick=lambda: admin('admin', tab, fio, id), color='primary', outline=True).style('width:100%; height:100%; padding:0.5em;')
+            put_button("     back      ", onclick=lambda: admin('admin', tab, fio, id), color='primary', outline=True).style('width:100%; height:100%; padding:0.5em;')
         ]).style('grid-template-columns: 0.1fr 1fr 0.1fr;'),
 
         None,
 
         put_row([
-            put_html(chart.to_html(full_html=False)).style('width:100%'),
-            put_html(chart2.to_html(full_html=False)).style('width:100%'),
+            put_html(div1).style('width:100%'),
+            put_html(div2).style('width:100%'),
         ]).style('display: flex; flex-direction: row; gap: 1em;'),
 
         None,
 
         put_row([
-            put_html(chart3.to_html(full_html=False)).style('width:100%'),
-            put_html(chart4.to_html(full_html=False)).style('width:100%'),
+            put_html(div3).style('width:100%'),
+            put_html(div4).style('width:100%'),
         ]).style('display: flex; flex-direction: row; gap: 1em;'),
 
         put_text("Активные сессии").style("text-align: center; font-size: xx-large;"),
@@ -132,3 +115,9 @@ async def dashboard_user(df_user, sdep, tab, fio, id):
             ).style('width:100%')
         ]).style('display: flex; justify-content: center;'),
     ])
+
+    # Отображение скриптов для интерактивности графиков
+    put_html(f"<script>{script1}</script>")
+    put_html(f"<script>{script2}</script>")
+    put_html(f"<script>{script3}</script>")
+    put_html(f"<script>{script4}</script>")
