76675412c22539330d6019f3beed71a222e84952
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 21:13:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,7 @@
-def json_to_html(json_obj):
-    html = '<html><body>'
-    html += '<h1>Ваш отчет по продуктам</h1>'
-    html += '<table border="1" cellpadding="5" cellspacing="0">'
-    html += '<tr>'
-    html += '<th>ID продукта</th>'
-    html += '<th>Название</th>'
-    html += '<th>Количество</th>'
-    html += '<th>Цена</th>'
-    html += '<th>Изображение</th>'
-    html += '<th>Размер</th>'
-    html += '<th>Цвет</th>'
-    html += '</tr>'
+df = pd.read_sql(sql.sql_operations_one.replace('Замена', str(product_id)), connect("Convert/db/shop.db"))
+    # Загружаем данные о корзине из JSON
+    json_data = json.loads(df['json'].iloc[0]) 
     
-    for item in json_obj:
-        html += '<tr>'
-        html += f'<td>{item["product_id"]}</td>'
-        html += f'<td>{item["name"]}</td>'
-        html += f'<td>{item["count"]}</td>'
-        html += f'<td>{item["subtotal_price"]}</td>'
-        html += f'<td><img src="{item["img"]}" alt="{item["name"]}" style="width:100px;height:auto;"></td>'
-        html += f'<td>{item["size"]}</td>'
-        html += f'<td>{item["color"]}</td>'
-        html += '</tr>'
-    
-    html += '</table>'
-    html += '</body></html>'
-    return html
+    items = [] 
+    total_price = 0
+    html_json = pd.read_json(json_data).to_html(index=False)
