2ff8b469de3f36001f9929ef61abd86cbad1b3ee
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 21:16:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-def json_to_html(json_data):
-    # Если json_data - это список словарей
-    if isinstance(json_data, list):
-        df = pd.DataFrame(json_data)
-    else:
-        raise ValueError("JSON data is not in the expected format")
-    
-    # Преобразование DataFrame в HTML
-    return df.to_html(index=False, escape=False)
+put_button(" Письмо ", onclick=lambda: put_html(f'''
+                                                                                                    <script>
+                                                                                                        var mailtoLink = "mailto:{df["login_customer"].iloc[0]}?subject=Заказ {product_id}&body=Привет!<br>{html_json}";
+                                                                                                        var link = document.createElement("a");
+                                                                                                        link.href = mailtoLink;
+                                                                                                        link.style.display = "none"; // Скрыть элемент
+                                                                                                        document.body.appendChild(link);
+                                                                                                        link.click();
+                                                                                                        document.body.removeChild(link);
+                                                                                                    </script>
+                                                                                                '''), color='success', outline=True)
