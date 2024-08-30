1dc39580d15c5ec2d7ed19275dc95ae80d0e3bdf
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 19:45:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,75 +1,146 @@
-from pyg2plot import Line
-from pyg2plot import options as opts
-
 def create_login_chart(dates, login_counts):
-    line = Line(init_opts=opts.InitOpts(theme='walden', width='100%', height='400px'))
-    line.add_xaxis(dates)
-    line.add_yaxis('Кол-во', login_counts, is_smooth=True)
-    line.set_series_opts(
-        areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
-        label_opts=opts.LabelOpts(is_show=False),
-    )
-    line.set_global_opts(
-        title_opts=opts.TitleOpts(title='Входы', pos_left='center'),
-        tooltip_opts=opts.TooltipOpts(trigger='axis'),
-        xaxis_opts=opts.AxisOpts(type_='category'),
-        yaxis_opts=opts.AxisOpts(type_='value'),
-        datazoom_opts=[
-            opts.DataZoomOpts(type_='slider', range_start=80, range_end=100)
-        ],
-        legend_opts=opts.LegendOpts(is_show=True)
-    )
+    data = [{"date": date, "login_counts": count} for date, count in zip(dates, login_counts)]
+    line = Plot("Line")
+    line.set_options({
+        "appendPadding": 32,
+        "data": data,
+        "xField": "date",
+        "yField": "login_counts",
+        "smooth": True,
+        "lineStyle": {
+            "lineWidth": 3,
+        },
+        "point": {
+            "size": 5,
+            "shape": 'diamond',
+            "style": {
+                "fill": "white",
+                "stroke": "#5B8FF9",
+                "lineWidth": 2,
+            }
+        },
+        "xAxis": {
+            "title": "Дата"
+        },
+        "yAxis": {
+            "title": "Кол-во"
+        },
+        "tooltip": {
+            "showMarkers": False
+        },
+        "legend": {
+            "position": "top"
+        }
+    })
     return line
 
 def create_multi_chart(dates, shop, game, box):
-    line = Line(init_opts=opts.InitOpts(theme='walden', width='100%', height='400px'))
-    line.add_xaxis(dates)
-    line.add_yaxis('Зашел в магазин', shop, is_smooth=True)
-    line.add_yaxis('Зашел в гейм', game, is_smooth=True)
-    line.add_yaxis('Воспользовался боксом', box, is_smooth=True)
-    line.set_global_opts(
-        title_opts=opts.TitleOpts(title='Сервисы', pos_left='center'),
-        tooltip_opts=opts.TooltipOpts(trigger='axis'),
-        xaxis_opts=opts.AxisOpts(type_='category'),
-        yaxis_opts=opts.AxisOpts(type_='value'),
-        datazoom_opts=[
-            opts.DataZoomOpts(type_='slider', range_start=80, range_end=100)
-        ],
-        legend_opts=opts.LegendOpts(is_show=True)
-    )
+    data = [{"date": date, "shop": s, "game": g, "box": b} for date, s, g, b in zip(dates, shop, game, box)]
+    line = Plot("Line")
+    line.set_options({
+        "appendPadding": 32,
+        "data": data,
+        "xField": "date",
+        "yField": ["shop", "game", "box"],
+        "seriesField": "type",
+        "smooth": True,
+        "lineStyle": {
+            "lineWidth": 3,
+        },
+        "point": {
+            "size": 5,
+            "shape": 'diamond',
+            "style": {
+                "fill": "white",
+                "stroke": "#5B8FF9",
+                "lineWidth": 2,
+            }
+        },
+        "xAxis": {
+            "title": "Дата"
+        },
+        "yAxis": {
+            "title": "Количество"
+        },
+        "tooltip": {
+            "showMarkers": False
+        },
+        "legend": {
+            "position": "top"
+        }
+    })
     return line
 
 def create_box_chart(dates, quest, offer, mood):
-    line = Line(init_opts=opts.InitOpts(theme='walden', width='100%', height='400px'))
-    line.add_xaxis(dates)
-    line.add_yaxis('Задал вопрос', quest, is_smooth=True)
-    line.add_yaxis('Предложил идею', offer, is_smooth=True)
-    line.add_yaxis('Отправил настроение', mood, is_smooth=True)
-    line.set_global_opts(
-        title_opts=opts.TitleOpts(title='Бокс ОС', pos_left='center'),
-        tooltip_opts=opts.TooltipOpts(trigger='axis'),
-        xaxis_opts=opts.AxisOpts(type_='category'),
-        yaxis_opts=opts.AxisOpts(type_='value'),
-        datazoom_opts=[
-            opts.DataZoomOpts(type_='slider', range_start=80, range_end=100)
-        ],
-        legend_opts=opts.LegendOpts(is_show=True)
-    )
+    data = [{"date": date, "quest": q, "offer": o, "mood": m} for date, q, o, m in zip(dates, quest, offer, mood)]
+    line = Plot("Line")
+    line.set_options({
+        "appendPadding": 32,
+        "data": data,
+        "xField": "date",
+        "yField": ["quest", "offer", "mood"],
+        "seriesField": "type",
+        "smooth": True,
+        "lineStyle": {
+            "lineWidth": 3,
+        },
+        "point": {
+            "size": 5,
+            "shape": 'diamond',
+            "style": {
+                "fill": "white",
+                "stroke": "#5B8FF9",
+                "lineWidth": 2,
+            }
+        },
+        "xAxis": {
+            "title": "Дата"
+        },
+        "yAxis": {
+            "title": "Количество"
+        },
+        "tooltip": {
+            "showMarkers": False
+        },
+        "legend": {
+            "position": "top"
+        }
+    })
     return line
 
 def create_orders_cancellations_chart(dates, order_counts, cancellation_counts):
-    line = Line(init_opts=opts.InitOpts(theme='walden', width='100%', height='400px'))
-    line.add_xaxis(dates)
-    line.add_yaxis('Сделал', order_counts, is_smooth=True)
-    line.add_yaxis('Отменил', cancellation_counts, is_smooth=True)
-    line.set_global_opts(
-        title_opts=opts.TitleOpts(title='Заказы', pos_left='center'),
-        tooltip_opts=opts.TooltipOpts(trigger='axis'),
-        xaxis_opts=opts.AxisOpts(type_='category'),
-        yaxis_opts=opts.AxisOpts(type_='value'),
-        datazoom_opts=[
-            opts.DataZoomOpts(type_='slider', range_start=80, range_end=100)
-        ],
-        legend_opts=opts.LegendOpts(is_show=True)
-    )
+    data = [{"date": date, "order_counts": o, "cancellation_counts": c} for date, o, c in zip(dates, order_counts, cancellation_counts)]
+    line = Plot("Line")
+    line.set_options({
+        "appendPadding": 32,
+        "data": data,
+        "xField": "date",
+        "yField": ["order_counts", "cancellation_counts"],
+        "seriesField": "type",
+        "smooth": True,
+        "lineStyle": {
+            "lineWidth": 3,
+        },
+        "point": {
+            "size": 5,
+            "shape": 'diamond',
+            "style": {
+                "fill": "white",
+                "stroke": "#5B8FF9",
+                "lineWidth": 2,
+            }
+        },
+        "xAxis": {
+            "title": "Дата"
+        },
+        "yAxis": {
+            "title": "Количество"
+        },
+        "tooltip": {
+            "showMarkers": False
+        },
+        "legend": {
+            "position": "top"
+        }
+    })
     return line
