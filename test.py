f101344a62f435d27b605f9b42fb3f4c79a36443
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 21:18:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-put_button(" Письмо ", onclick=lambda: put_html(f'''
-                                                                                                    <script>
-                                                                                                        var mailtoLink = "mailto:{df["login_customer"].iloc[0]}?subject=Заказ {product_id}&body=Привет!<br>{html_json}";
-                                                                                                        var link = document.createElement("a");
-                                                                                                        link.href = mailtoLink;
-                                                                                                        link.style.display = "none"; // Скрыть элемент
-                                                                                                        document.body.appendChild(link);
-                                                                                                        link.click();
-                                                                                                        document.body.removeChild(link);
-                                                                                                    </script>
-                                                                                                '''), color='success', outline=True)
+
