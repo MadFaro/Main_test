315cf82fd48152ac585a422222f28a6d9b380d99
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-08-30 21:08:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-[{'product_id': '11', 'name': 'Блокнот', 'count': '1', 'subtotal_price': '30', 'img': 'img/блокнот.JPG', 'size': '-', 'color': '-'}]
+def json_to_html(json_obj):
+    html = '<html><body>'
+    html += '<h1>Ваш отчет по продуктам</h1>'
+    html += '<table border="1" cellpadding="5" cellspacing="0">'
+    html += '<tr>'
+    html += '<th>ID продукта</th>'
+    html += '<th>Название</th>'
+    html += '<th>Количество</th>'
+    html += '<th>Цена</th>'
+    html += '<th>Изображение</th>'
+    html += '<th>Размер</th>'
+    html += '<th>Цвет</th>'
+    html += '</tr>'
+    
+    for item in json_obj:
+        html += '<tr>'
+        html += f'<td>{item["product_id"]}</td>'
+        html += f'<td>{item["name"]}</td>'
+        html += f'<td>{item["count"]}</td>'
+        html += f'<td>{item["subtotal_price"]}</td>'
+        html += f'<td><img src="{item["img"]}" alt="{item["name"]}" style="width:100px;height:auto;"></td>'
+        html += f'<td>{item["size"]}</td>'
+        html += f'<td>{item["color"]}</td>'
+        html += '</tr>'
+    
+    html += '</table>'
+    html += '</body></html>'
+    return html
