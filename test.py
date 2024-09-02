df15bb0756ce2bf86e179c04bc167ae57113bd4a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-02 13:58:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,15 @@ FROM (
         a.OPERATORID, 
         a.CREATED,
         a.VISITORID,
-        COUNT(*) OVER (
-            PARTITION BY a.VISITORID 
-            ORDER BY a.CREATED 
-            RANGE BETWEEN INTERVAL '2' DAY PRECEDING AND CURRENT ROW
-        ) AS FCR
+        CASE 
+            WHEN COUNT(*) OVER (
+                PARTITION BY a.VISITORID 
+                ORDER BY a.CREATED 
+                RANGE BETWEEN INTERVAL '0' DAY PRECEDING AND INTERVAL '2' DAY FOLLOWING
+            ) = 1 
+            THEN 1 
+            ELSE 0 
+        END AS FCR
     FROM 
         ODS.ODS_WIS_CHATTHREAD@cdw.prod a
     WHERE 
