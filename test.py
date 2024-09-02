b9fda3d8d4d765dc74b25a16cca4d144e1d6b69e
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-02 13:45:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-
+select trunc(CREATED, 'mm') as created, sum(case when FCR=1 then 1 else 0 end)/count(*) as fcr from (
+select 
+a.OPERATORFULLNAME, a.OPERATORID, 
+a.CREATED,
+a.VISITORID,
+COUNT(TRUNC(CREATED)) OVER(PARTITION BY TRUNC(CREATED), a.VISITORID) AS FCR
+from ODS.ODS_WIS_CHATTHREAD@cdw.prod a
+where a.CREATED>=date'2023-12-01'
+and threadid in (select distinct threadid from ODS.ODS_WIS_chatthreadhistory@cdw.prod
+where dtm >=date'2023-12-01' and DEPARTMENTID in (22))
+)
+group by trunc(CREATED, 'mm')
+order by trunc(CREATED, 'mm')
