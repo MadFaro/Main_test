529b005ed1608381917da96be1b98d2d76d07437
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-03 16:41:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,18 @@ SELECT
     timezone_offset,
     CASE
         WHEN timezone_offset LIKE 'GTM+%' THEN 
-            '+' || LPAD(SUBSTR(timezone_offset, 5), 2, '0') || ':00'
+            CASE
+                WHEN TO_NUMBER(SUBSTR(timezone_offset, 5)) > 3 THEN
+                    '+' || LPAD(TO_NUMBER(SUBSTR(timezone_offset, 5)) - 3, 2, '0') || ':00'
+                WHEN TO_NUMBER(SUBSTR(timezone_offset, 5)) = 3 THEN
+                    '0:00'
+                ELSE
+                    '-' || LPAD(3 - TO_NUMBER(SUBSTR(timezone_offset, 5)), 2, '0') || ':00'
+            END
         WHEN timezone_offset LIKE 'GTM-%' THEN 
-            '-' || LPAD(SUBSTR(timezone_offset, 5), 2, '0') || ':00'
+            '-' || LPAD(3 + TO_NUMBER(SUBSTR(timezone_offset, 5)), 2, '0') || ':00'
         ELSE
             NULL
     END AS formatted_timezone
 FROM 
     your_table_name;
-
