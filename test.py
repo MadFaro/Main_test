ad630ea3216e73ac3e079275b67ef84321d2f322
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-03 17:18:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-    CASE
-        WHEN timezone_offset LIKE 'GTM+%' THEN 
-            TO_CHAR(TO_NUMBER(SUBSTR(timezone_offset, 5)) - 3)
-        WHEN timezone_offset LIKE 'GTM-%' THEN 
-            TO_CHAR(3 - TO_NUMBER(SUBSTR(timezone_offset, 5)))
-        ELSE
-            NULL
-    END AS offset_hours
+case when extract (hour from CLIENT_TIME)>=9 and extract (hour from CLIENT_TIME)<22 then 'YES' else 'NO' end as CLIENT_WROK_TIME
