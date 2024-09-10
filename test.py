41648bf2be3b3d1903e179eff89055914b0dedca
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-10 13:14:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-
+session_info = json.dumps({
+        k: str(getattr(info, k))
+        for k in ['user_agent', 'user_language', 'server_host',
+                'origin', 'user_ip', 'backend', 'protocol', 'request']
+    }, indent=4)
