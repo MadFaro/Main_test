93b01924ca9ea635d69b1d6d3d4df19599ce8aa6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-11 16:04:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+PL/SQL: SQL Statement ignored
+PL/SQL: ORA-00947: не хватает значений для данных
 
+execute immediate 'truncate table analytics.tolog_agn_dpupp_kc_kg_proverka_res_sms_model_cc_kc_2';
+insert into analytics.tolog_agn_dpupp_kc_kg_proverka_res_sms_model_cc_kc_2
+select REPORT_DATE,max(rn) as rn
+from analytics.tolog_agn_dpupp_kc_kg_proverka_res_sms_model_cc_kc
+group by REPORT_DATE;
