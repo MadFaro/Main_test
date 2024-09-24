11d21293df794b4645110d6467d603b08ffd4a3c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-24 11:10:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,17 +7,20 @@ export_folder = "C:\\backup\\tasks"
 # Создаем папку для экспорта, если она не существует
 os.makedirs(export_folder, exist_ok=True)
 
-# Получаем список задач с помощью PowerShell
-get_tasks_command = "Get-ScheduledTask | ForEach-Object { $_.TaskName }"
+# Команда для получения задач из корня "Библиотеки планировщика заданий" без подпапок
+get_tasks_command = '''
+Get-ScheduledTask |
+Where-Object { $_.TaskPath -eq "\\" } |
+ForEach-Object { $_.TaskName }
+'''
+
+# Выполняем PowerShell команду для получения списка задач
 tasks = subprocess.run(["powershell", "-Command", get_tasks_command], capture_output=True, text=True).stdout.splitlines()
 
 # Экспортируем каждую задачу
 for task in tasks:
-    # Формируем команду экспорта
-    export_command = f'Export-ScheduledTask -TaskName "{task}" -TaskPath "\\" -Path "{export_folder}\\{task}.xml"'
-    
-    # Выполняем команду через PowerShell
-    subprocess.run(["powershell", "-Command", export_command], capture_output=True, text=True)
-    
-print(f"Все задачи экспортированы в папку {export_folder}")
+    if task:  # Проверка на пустую строку
+        export_command = f'Export-ScheduledTask -TaskName "{task}" -TaskPath "\\" -Path "{export_folder}\\{task}.xml"'
+        subprocess.run(["powershell", "-Command", export_command], capture_output=True, text=True)
 
+print(f"Задачи из корневой папки экспортированы в папку {export_folder}")
