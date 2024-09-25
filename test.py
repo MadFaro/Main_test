19cc433c326be5547555430868cd5322a2986276
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-25 13:06:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+put_row([
     put_text("""
 Привет! Добро пожаловать на страницу Геймификации 2024.
 Игра в стиле «Слово пацана» завершена и мы благодарим каждого, кто принял участие.
-Увидимся в следующем году, друзья. А пока можете посмотреть результаты Гейма во вкладке «Итоги».
-        """).style("text-align: center;white-space: pre-line;font-size: x-large;"),
+Увидимся в следующем году, друзья. А пока можете посмотреть результаты Гейма во вкладке «""").style("text-align: center; white-space: pre-line; font-size: x-large;"),
+    put_button('Итоги', onclick=lambda: ...).style("font-size: x-large;"),
+    put_text("""».
+        """).style("text-align: center; white-space: pre-line; font-size: x-large;")
+])
