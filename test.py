2d7620340635c062f09dbe1e7de61feb8f2888c9
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-25 13:26:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-put_html("""
-    <div style="text-align: center; font-size: x-large; white-space: pre-line;">
-    Привет! Добро пожаловать на страницу Геймификации 2024.<br>
-    Игра в стиле «Слово пацана» завершена и мы благодарим каждого, кто принял участие.<br>
-    Увидимся в следующем году, друзья. А пока можете посмотреть результаты Гейма во вкладке 
-    <a href="#" style="text-decoration: none; color: blue;" onclick="pywebio.onClick('itogi_click')">«Итоги»</a>.
-    </div>
-""")
+
