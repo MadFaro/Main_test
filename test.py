d78f22caba13375453036d8e6830e8f308cf70cd
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-25 13:08:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+put_html("""
+    <div style="text-align: center; font-size: x-large;">
+    Привет! Добро пожаловать на страницу Геймификации 2024.<br>
+    Игра в стиле «Слово пацана» завершена и мы благодарим каждого, кто принял участие.<br>
+    Увидимся в следующем году, друзья. А пока можете посмотреть результаты Гейма во вкладке 
+    </div>
+""")
+
 put_row([
-    put_text("""
-Привет! Добро пожаловать на страницу Геймификации 2024.
-Игра в стиле «Слово пацана» завершена и мы благодарим каждого, кто принял участие.
-Увидимся в следующем году, друзья. А пока можете посмотреть результаты Гейма во вкладке «""").style("text-align: center; white-space: pre-line; font-size: x-large;"),
-    put_button('Итоги', onclick=lambda: ...).style("font-size: x-large;"),
-    put_text("""».
-        """).style("text-align: center; white-space: pre-line; font-size: x-large;")
-])
+    span("", width='auto'),
+    put_button("Итоги", onclick=lambda: ..., link_style=True),
+    span(".", width='auto')
+], size="auto").style("text-align: center; font-size: x-large;")
