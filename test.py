345554fc83d5176646fa368ce467000216fb53d1
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-26 09:56:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-
+select a.*, b.FIRSTNAME,
+d.SUBJ1, d.PRODUCT1, d.REZ1
+from ANALYTICS.TOLOG_IVR_OPER a
+left join ODS.ODS_CSC_PERSON@cdw.prod b on a.AGENT_ID = b.LOGINNAME
+left join ANALYTICS.KDI_SIEBEL_PAST d on trunc(a.EVENT_TIME) = trunc(d.dt) and d.dt > a.EVENT_TIME and lower(b.FIRSTNAME) = lower(d.OPERATOR) and d.type = 'Вызов - входящий'
