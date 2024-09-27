a1fcc445278fd043fd0d7b46c816f8b18605eb14
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-09-27 09:49:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-
+ORA-00932: несовместимые типы данных: ожидается NUMBER, получено INTERVAL DAY TO SECOND
+00932. 00000 -  "inconsistent datatypes: expected %s got %s"
+*Cause:    
+*Action:
+Error at Line: 20 Column: 62
