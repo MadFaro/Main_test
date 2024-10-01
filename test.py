d0beea05e075c2aef00cfdbda4eb4f37811cb8ef
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-01 16:10:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-
+select trunc(DT, 'mm') as DT, TABEL, DIRECTION, PODRAZDELENIE, OPERATOR,  SUBJ1, PRODUCT1, REZ1, count(distinct CALL_ID) as cnt from ANALYTICS.KDI_SIEBEL_PAST
+where DT >=date'2024-01-01' and TYPE = 'Вызов - входящий'
+group by trunc(DT, 'mm'), TABEL, DIRECTION, PODRAZDELENIE, OPERATOR,  SUBJ1, PRODUCT1, REZ1,
