99858091f482aef53c1dce370fe57adf5ee97b5a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-02 17:35:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-
+select * from ANALYTICS.TOLOG_TMP_SIEBL1
+where call_id not in (
+select distinct CALL_SIEBEL as call_id from ANALYTICS.TOLOG_IVR_OPER_THEMA
+where trunc(EVENT_TIME) = date'2024-09-16'--
+)
