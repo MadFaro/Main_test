f5b0eda86406d3e14f034d4fd6a521142c811429
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-04 15:33:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-# Функция для дарения баллов
-async def gift_my_balance(tab, fio, balans):
-    popup('Перевод баланса',[
-        put_html(f"Для перевода доступно {balans}"), 
-        put_select(name='gift_people', label='Выберите кому:', options=['', 'Петя', 'Дима', 'Коля', 'Антон', 'Алексей']),
-        put_html('<br>'),
-        put_button("ОТПРАВИТЬ", onclick=lambda: toast("Функционал в разработке"), color='info', outline=True)
-        ], size='normal')
+
