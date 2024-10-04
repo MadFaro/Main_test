70c165e0cfbbfc80288dad797971d5f3ee214892
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-04 11:45:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-WITH last_records AS (
-    SELECT t.*,
-           ROW_NUMBER() OVER (PARTITION BY id ORDER BY date_column DESC) AS rn
-    FROM your_table t
-)
-SELECT *
-FROM last_records
-WHERE rn = 1  -- выбираем только последнюю запись для каждого ID
-  AND column_not_contains_cd NOT LIKE '%CD%';  -- проверяем, что в последней записи нет 'CD'
 
