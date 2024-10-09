851b30863629a8b116e67845d3a753f6f35c88a3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-09 13:54:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-import pandas as pd
 
-# Чтение исходного файла
-filename = "data.txt"
-
-# Чтение данных из файла (пропускаем первые две строки и задаем разделитель как табуляцию)
-data = pd.read_csv(filename, delimiter='\t', skiprows=2, header=None)
-
-# Преобразование столбца 5 (индекс 4) в числовой формат
-data[4] = pd.to_numeric(data[4], errors='coerce')
-
-# Заменяем NaN на 0
-data[4] = data[4].fillna(0)
-
-# Умножаем значения 5-го столбца (объём) на 140% (или 1.4) и округляем до целого
-data[4] = (data[4] * 1.4).round(0).astype(int)
-
-# Сохранение измененного файла
-data.to_csv("data_updated.txt", sep='\t', index=False, header=False)
