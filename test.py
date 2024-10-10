917b0e3bf587b00c355c2c9d24b3caf7470319b0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-10 08:11:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
+import subprocess
+
+def get_running_tasks_from_root():
+    cmd = [
+        'powershell',
+        'Get-ScheduledTask -TaskPath "\" | Where-Object { $_.State -eq "Running" }'
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return "Нет запущенных задач"
+
+running_tasks = get_running_tasks_from_root()
+print(running_tasks)
 
