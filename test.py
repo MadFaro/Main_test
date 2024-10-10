eed6d106ffeb44401fc25f13015538864618c1aa
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-10 08:35:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-import subprocess
 
-def get_running_tasks_from_root():
-    # Команда PowerShell для получения выполняющихся задач из корневой папки
-    cmd = [
-        'powershell',
-        'Get-ScheduledTask | Where-Object { $_.State -eq "Running" -and $_.TaskPath -eq "\\" }'
-    ]
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    
-    if result.returncode == 0:
-        # Возвращаем список выполняющихся задач из корневой папки
-        return result.stdout
-    else:
-        # В случае ошибки возвращаем сообщение об ошибке
-        return result.stderr
-
-running_tasks = get_running_tasks_from_root()
-print(running_tasks)
