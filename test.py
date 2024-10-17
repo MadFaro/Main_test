7977b58bb2b3df0ea2e3f058e24ccf151376e0a2
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-17 19:56:29 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,29 @@ async def discount_noadmin(sdep, tab, fio, id):
         clear()
     except:
         pass
+    
     ban = open('discount/img/ban.jpg', 'rb').read()
     df_product = pd.read_sql(sql.sql_discount, connect("Convert/db/shop.db"))
+    
+    # Кнопка "Главная"
     put_row([
-    None,
-    None,
-    put_button("\U0001F3E0\n\r        Главная        ", onclick=lambda: game_noadmin(sdep, tab, fio, id), color='dark', outline=True).style('font-size: 2vh')]).style('padding:0.4em;background:rgb(255 255 255);grid-template-columns:0.1fr 1fr 0.1fr;')
+        None,
+        None,
+        put_button("\U0001F3E0\n\r        Главная        ", onclick=lambda: game_noadmin(sdep, tab, fio, id), color='dark', outline=True).style('font-size: 2vh')
+    ]).style('padding:0.4em;background:rgb(255 255 255);grid-template-columns:0.1fr 1fr 0.1fr;')
+    
+    # Блок баннера
     put_image(ban, width='auto', height='auto').style('display: block;margin-left: auto;margin-right: auto')
 
-
+    # Добавляем изображения из df_product
+    for index, row in df_product.iterrows():
+        img_path = row['img']  # Путь к изображению из столбца img
+        img = open(img_path, 'rb').read()
+        put_image(img, width='auto', height='auto').style('display: block;margin-left: auto;margin-right: auto; margin-bottom: 20px')  # Вывод изображения
+    
+    # Footer
     put_html(f"""
         <footer class="footer">
         {fio}
         </footer>
-       """).style("width:100%;z-index:2147483647;bottom:0px")
+    """).style("width:100%;z-index:2147483647;bottom:0px")
