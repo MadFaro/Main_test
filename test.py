5ac93ccc3a3afbf6eb70696cb0ff5c8ce436e1fa
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-17 15:51:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
+import pymysql
+
+# Параметры подключения
+config = {
+    'user': 'your_username',
+    'password': 'your_password',
+    'host': 'localhost',
+    'database': 'your_database'
+}
+
+# Подключение
+try:
+    connection = pymysql.connect(user=config['user'], password=config['password'],
+                                 host=config['host'], database=config['database'])
+
+    with connection.cursor() as cursor:
+        # Пример запроса
+        cursor.execute("SELECT DATABASE();")
+        result = cursor.fetchone()
+        print("Вы подключены к базе данных:", result)
+
+except pymysql.MySQLError as error:
+    print("Ошибка подключения к MariaDB:", error)
+
+finally:
+    connection.close()
+    print("Соединение закрыто")
 
