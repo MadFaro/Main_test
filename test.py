b7b6df92a2a9f8cd6b3132f9060143e1948e6d4d
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-17 16:14:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-Ошибка подключения к MariaDB: (2003, "Can't connect to MySQL server on '10.000.100.00' (timed out)")
-Traceback (most recent call last):
-  File "marina.py", line 27, in <module>
-    connection.close()
-NameError: name 'connection' is not defined
+
