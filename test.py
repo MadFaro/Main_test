fa9ee3f211f48066f5a9d0b9dcbd5cc42a0219df
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-21 14:29:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import sqlite3
 
+db_path = r'C:\Users\TologonovAB\Desktop\shop_app\Convert\db\shop.db'
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+cursor.execute('DELETE FROM session')
+conn.commit()
+conn.close()
+
+print("Все записи из таблицы 'session' успешно удалены.")
