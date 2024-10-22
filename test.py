930b509ead6f5a4644083d6fa1e4effc43687ce2
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-23 00:09:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-
+    def get_user_aut(self, ID):
+        try:
+            result = self.cursor.execute("SELECT `dt`, `authen_key` FROM `authen` WHERE `login` = ?", (ID,))
+            return result.fetchone()[0]
+        except sqlite3.Error:
+            return None
