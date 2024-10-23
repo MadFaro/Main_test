13f6d3cedbfd440ee03df15f7de0dd537a1ec743
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-23 19:42:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
+import pymysql
 
+# Параметры подключения
+config = {
+    'user': '',
+    'password': '',
+    'host': '',
+    'port': '',
+    'database': ''
+}
+
+# Подключение
+try:
+    connection = pymysql.connect(user=config['user'], password=config['password'],
+                                 host=config['host'], port=config['port'], database=config['database'])
+
+    with connection.cursor() as cursor:
+        # Пример запроса
+        cursor.execute("select * from chatthread where created >= date(now());")
+        result = cursor.fetchone()
+        print("Вы подключены к базе данных:", result)
+
+except pymysql.MySQLError as error:
+    print("Ошибка подключения к MariaDB:", error)
+
+finally:
+    connection.close()
+    print("Соединение закрыто")
