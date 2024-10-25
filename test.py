80ce41c5d406e2e97f4173da64ac5cb66f3752e6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-25 10:54:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-with open(shortcut_file, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', f'attachment; filename={os.path.basename(shortcut_file)}')
-        msg.attach(part)
+shortcut_filename = "login_shortcut.url"
