3ec3a2a4a058c122d0a3401f13e01f436bf44b60
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-25 15:58:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-    def token_exists(self, ID):
-        try:
-            result = self.cursor.execute("SELECT 1 FROM `tokens` WHERE `user_token` = ?", (ID,))
-            return bool(len(result.fetchall()))
-        except sqlite3.Error:
-            return False
+<b style="color: red;">жирный и красный текст</b>
