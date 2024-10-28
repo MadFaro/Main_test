edcdf7f63343d50be541df983dcb5ac64673fc60
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-28 16:31:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-
+                token_result = BotDS.user_token_for_mail(login_customer)
+                
+                # Добавляем токен к URL, если он есть
+                if token_result:
+                    token = token_result[0]
+                    personalized_url = f"{shop_url}?app={token}"
+                else:
+                    personalized_url = shop_url
+    def user_token_for_mail(self, ID):
+        try:
+            result = self.cursor.execute("SELECT `user_token` FROM `tokens` WHERE `user_login` = ?", (ID,))
+            return result.fetchall()
+        except sqlite3.Error:
+            return None
