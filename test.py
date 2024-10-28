c8e346a9c2398deff80d177ebd3e3ac8fa18bf90
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-28 15:34:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-def create_shortcut(filename, url):
-    with open(filename, 'w') as shortcut:
-        shortcut.write(f"[InternetShortcut]\nURL={url}\n")
+SELECT *
+FROM users
+WHERE strftime('%m-%d', birthday) = strftime('%m-%d', 'now');
