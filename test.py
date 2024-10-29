f0b444e363bbe1a01d221341df1d67693b2d9f4e
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-29 15:04:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-from cryptography.fernet import Fernet
-
-# Генерация ключа для шифрования (сохрани его для использования в будущем)
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
-
-def encrypt_user_id(user_id: str) -> str:
-    """Шифрует user_id."""
-    return cipher_suite.encrypt(user_id.encode()).decode()
-
-def decrypt_user_id(encrypted_user_id: str) -> str:
-    """Дешифрует user_id."""
-    return cipher_suite.decrypt(encrypted_user_id.encode()).decode()
+base64.urlsafe_b64encode(stored_key_str.encode()).decode()
