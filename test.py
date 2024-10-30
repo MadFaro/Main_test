0b396f72edb05aec12ad4ae2ced4eec2f98d0556
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-30 20:18:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-Traceback (most recent call last):
-  File "C:\Python38\lib\site-packages\pywebio\session\coroutinebased.py", line 325, in step
-    coro_yield = self.coro.send(result)
-  File "C:\Python38\lib\site-packages\pywebio\session\coroutinebased.py", line 101, in _start_main_task
-    await target()
-  File "main.py", line 48, in main
-    cipher_suite = Fernet(key)
-  File "C:\Python38\lib\site-packages\cryptography\fernet.py", line 40, in __init__
-    raise ValueError(
-ValueError: Fernet key must be 32 url-safe base64-encoded bytes.
+# Авторизация
+@config(theme = 'minty', css_style = css.container_output, description='ПОРТАЛ КЦ', title='ПОРТАЛ КЦ')
+async def main():
