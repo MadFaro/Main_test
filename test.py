50387c41f861366cfce4f0d33229314bf4506dcf
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-31 13:39:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-
+select 
+    threadid, -- ID чата
+    operatorfullname, -- ФИО Оператора
+    operatorid, -- ID Оператора
+    created, -- Дата создания чата
+    modified, -- Дата последнего изменения чата
+    state, -- Статус
+    offline, -- Флаг что чат обработа онлайн
+    category, -- Категория
+    subcategory, -- Под категория
+    threadkind -- Тип чата
+    from chatthread where created >= date(now());
