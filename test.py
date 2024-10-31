bfe580dbc623fb0b3c45202c067b0edc641858ab
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-31 16:30:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-    select 
-    THREADHISTORYID, 
-    THREADID, 
-    NUMBER as NUMBER_, 
-    DTM, 
-    STATE, 
-    OPERATORID, 
-    DEPARTMENTID, 
-    EVENT from chatthreadhistory where DTM >= %S
 
-ANALYTICS.TOLOG_BI_WEBIM_CHATTHREADHISTORY
