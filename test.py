389c78aa2a617a04192b1606a097bd9af3b52ed3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-10-31 14:13:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,14 @@
-sqlalchemy.exc.DatabaseError: (cx_Oracle.DatabaseError) ORA-00904: : недопустимый идентификатор
-[SQL:
-CREATE TABLE analytics."TOLOG_BI_WEBIM_CHATTHREAD" (
-        threadid NUMERIC,
-        operatorfullname VARCHAR(1500 CHAR),
-        operatorid NUMERIC,
-        created DATE,
-        modified DATE,
-        state VARCHAR(1500 CHAR),
-        offline NUMERIC,
-        category VARCHAR(1500 CHAR),
-        subcategory VARCHAR(1500 CHAR),
-        threadkind VARCHAR(1500 CHAR)
-)
-
-]
-(Background on this error at: https://sqlalche.me/e/14/4xp6)
+from sqlalchemy.types import Integer, String, Date
 
 data_type = {
-'threadid': NUMERIC,
-'operatorfullname': VARCHAR(1500),
-'operatorid': NUMERIC,
-'state': VARCHAR(1500),
-'offline': NUMERIC,
-'category': VARCHAR(1500),
-'subcategory': VARCHAR(1500),
-'threadkind': VARCHAR(1500)
+    'threadid': Integer,
+    'operatorfullname': String(1500),
+    'operatorid': Integer,
+    'created': Date,
+    'modified': Date,
+    'state': String(1500),
+    'offline': Integer,
+    'category': String(1500),
+    'subcategory': String(1500),
+    'threadkind': String(1500)
 }
-
-df.to_sql('TOLOG_BI_WEBIM_CHATTHREAD', connect, if_exists='replace', schema='analytics', index=False, dtype=data_type)
