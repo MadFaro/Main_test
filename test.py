0f9d0d3ceafd5964e530c8f896c08854eace0ef1
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-02 22:57:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-
+error connect Oracle:ORA-06550: line 15, column 72:
+PLS-00382: expression is of wrong type
+ORA-06550: line 15, column 65:
+PL/SQL: ORA-22905: cannot access rows from a non-nested table item
+ORA-06550: line 12, column 17:
+PL/SQL: SQL Statement ignored
