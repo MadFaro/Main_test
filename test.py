076b0dc9eed7fbf4256f6224c77d6fcc35839533
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-02 09:28:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-SUBSTR(ФИО, 1, INSTR(ФИО, ' ', 1, 2) - 1) AS ФИ
+SELECT 
+    SUBSTR(ФИО, INSTR(ФИО, ' ') + 1, INSTR(ФИО, ' ', 1, 2) - INSTR(ФИО, ' ') - 1) ||
+    ' ' ||
+    SUBSTR(ФИО, 1, INSTR(ФИО, ' ') - 1) AS ИФ
+FROM 
+    таблица;
