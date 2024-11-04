f1c3ee415c8330850744c2d71f5d4f049ad272b6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-04 18:41:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-if not df_chat_wait.empty:
-    df_chat_wait = df_chat_wait.applymap(lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if isinstance(x, pd.Timestamp) else x)
-else:
-    df_chat_wait = pd.DataFrame([['Нет данных']], columns=['Сообщение'])
+async def my_monitoring(sdep, tab, fio, id, img_logo):
+
+    try:
+        clear()
+    except:
+        pass
+
+    connect_str = lambda: cx_Oracle.connect(user='Reports_msk', password='Reports!', dsn = 'msk-as03math.fc.uralsibbank.ru/space.prod.msk.usb')
+    connect = create_engine("oracle://", creator=connect_str)
+    
+    while True:
+        df_chat_wait = pd.read_sql(sql_oracle.sql_chat_wait, connect)
+        
+        if not df_chat_wait.empty:
+            df_chat_wait = df_chat_wait.applymap(lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if isinstance(x, pd.Timestamp) else x)
+        else:
+            df_chat_wait = pd.DataFrame([['Нет данных']], columns=['Сообщение'])
+
+        clear('table')
+        put_column([put_datatable(df_chat_wait.to_dict('records'), theme='alpine-dark', cell_content_bar=False)], scope='table')
+        use_scope
+        await asyncio.sleep(30)
