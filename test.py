3811f375b6d7ff9f3c4d4abf7e0326422d145ee0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-06 18:25:16 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-ORA-00932: несовместимые типы данных: ожидается NUMBER, получено INTERVAL DAY TO SECOND
-00932. 00000 -  "inconsistent datatypes: expected %s got %s"
-*Cause:    
-*Action:
-Error at Line: 14 Column: 28
+ROUND(CAST(DT AS DATE) - CAST(TIME_CLOSE AS DATE)) AS dt, 
