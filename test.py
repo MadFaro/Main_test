5db4b7acbe59eace30e48606b8e13fb193f11759
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-06 18:21:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-
+select trunc(TIME_CLOSE, 'mm') as mm, round(DT-TIME_CLOSE) as dt, count(distinct CALL_ID) from (
+select distinct CLIENT_DID, TIME_CLOSE, CALL_ID, DT from ANALYTICS.TOLOG_CLOSED_CARDS_CC_CALL)
+group by trunc(TIME_CLOSE, 'mm'), round(DT-TIME_CLOSE)
+order by trunc(TIME_CLOSE, 'mm'), round(DT-TIME_CLOSE)
