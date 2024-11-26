54a411d71f9410cd1691fc48ba0b8ff1e9da149c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-26 04:32:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+all_pins = await pin  # Получение всех данных из pin
+name_up_pin = all_pins['admin_update_product_name']
+price_up_pin = all_pins['admin_update_product_price']
+desc_up_pin = all_pins['admin_update_product_desc']
+size_up_pin = all_pins.get('admin_update_product_size')  # .get() для необязательных данных
+color_up_pin = all_pins.get('admin_update_product_color')
+prom_up_pin = all_pins['admin_update_product_prom']
 
+print(name_up_pin, price_up_pin, desc_up_pin)
