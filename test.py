1637eea903e3fe203a1db61fbf26dce7ee70210a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-11-28 17:24:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-
+1.	Продемонстрировали на базе Наумен.Речевая аналитика все кейсы из ФТ. Запись демо: https://naumen.ktalk.ru/recordings/TaSVgOlOdwy0PedpR77T 
+2.	Запись предыдущей встречи по Инсайдам: https://naumen.ktalk.ru/recordings/aupvdyd2aC70QCr17lpI 
