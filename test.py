3113818d97eb4f7b3c951f1bb25a128d1e298616
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-02 06:36:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-create table analytics.tolog_shop_users_bd as
-select distinct UPPER(a."Email") as EMAIL, b.BIRTH_DATE  from USB_FNS_LOADER.KO_AD_BK_NIGHT a
-left join REF.KO_AD_STORY b on a."NetName" = b.NETNAME
-where a."DEP_ID_L3"='40050455' AND a."StateNP"=1 AND a."StateAD"=1 and UPPER(a."Email") not in (
-select * from ANALYTICS.TOLOG_SHOP_USERS
-)
-
-
-Error starting at line : 1 in command -
-create table analytics.tolog_shop_users_bd as
-select distinct UPPER(a."Email") as EMAIL, b.BIRTH_DATE  from USB_FNS_LOADER.KO_AD_BK_NIGHT a
-left join REF.KO_AD_STORY b on cast(a."NetName" as VARCHAR2(200)) = cast(b.NETNAME as VARCHAR2(200))
-where a."DEP_ID_L3"='40050455' AND a."StateNP"=1 AND a."StateAD"=1 and UPPER(a."Email") not in (
-select * from ANALYTICS.TOLOG_SHOP_USERS
-)
-Error report -
-ORA-00997: неверное использование типа данных LONG
-00997. 00000 -  "illegal use of LONG datatype"
-*Cause:    
-*Action:
-
+CREATE TABLE analytics.tolog_shop_users_bd AS
+SELECT DISTINCT UPPER(a."Email") AS EMAIL, b.BIRTH_DATE  
+FROM USB_FNS_LOADER.KO_AD_BK_NIGHT a
+LEFT JOIN REF.KO_AD_STORY b 
+    ON CAST(a."NetName" AS VARCHAR2(200)) = CAST(b.NETNAME AS VARCHAR2(200))
+WHERE a."DEP_ID_L3" = '40050455' 
+  AND a."StateNP" = 1 
+  AND a."StateAD" = 1 
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM ANALYTICS.TOLOG_SHOP_USERS u
+      WHERE UPPER(a."Email") = u.EMAIL
+  );
