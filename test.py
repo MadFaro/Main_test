0d36456b75c5983a1ea7db530ccfb471ba4d8d9f
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-05 07:57:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-Размещённое
+SELECT id,
+       date_ad,
+       fio,
+       login,
+       phone,
+       category,
+       city,
+       name,
+       text_ad,
+       file_ad,
+       moderation
+  FROM advt;
