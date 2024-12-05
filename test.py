5804fe3d83ebf4ee2b4b59121eda554d160c967c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-05 08:05:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-SELECT id,
-       date_ad,
-       fio,
-       login,
-       phone,
-       category,
-       city,
-       name,
-       text_ad,
-       file_ad,
-       moderation
-  FROM advt
- WHERE date_ad >= DATE('now', '-30 days');
+
