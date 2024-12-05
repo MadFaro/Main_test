ed66936eb7bbce8a928fcc9fe3f7f8035b672971
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-05 08:47:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-strftime('%s', 'now', '-30 days')
+Traceback (most recent call last):
+  File "C:\Python38\lib\site-packages\pywebio\session\coroutinebased.py", line 325, in step
+    coro_yield = self.coro.send(result)
+  File "main.py", line 2412, in advt_update
+    BotDS.update_advt(advt_id)
+  File "C:\Users\TologonovAB\Desktop\shop_app\db.py", line 293, in update_advt
+    self.cursor.execute("UPDATE `advt` SET `date_ad` = strftime('%s', 'now', '0 days') WHERE `id` = ?", (id))
+ValueError: parameters are of unsupported type
