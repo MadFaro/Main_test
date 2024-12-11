d46332dc2a81c7ba217ad7eaa73cee5c8ed8db5b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-11 11:59:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-{"password_update_notification": false, "new_visitor_notification": null, "max_chats_per_operator": 5, "backend_locale": "ru", "visible_name": "\u0410\u0439\u0433\u0443\u043b\u044c", "group": null}
+SELECT JSON_EXTRACT(marinadb, '$.max_chats_per_operator') AS max_chats
+FROM your_table;
