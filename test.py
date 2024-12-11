6f8ca687ce6c81d9b4b2d0a73c5bd6fb54c98f3c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-11 09:18:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-
+select distinct a.OPERATORID, b.FULLNAME, 
+case when a.DEPARTMENTID in (16, 21, 26, 28) then 'mass'
+when a.DEPARTMENTID = 22 then 'vip'
+when a.DEPARTMENTID = 31 then '2line'
+when a.DEPARTMENTID = 23 then 'reten' else 'ckk' end as DEPARTMENT
+from ODS.ODS_WIS_CHATOPERATORDEPARTMENT@cdw.prod a
+left join ODS.ODS_WIH_CHATOPERATOR@cdw.prod b on a.OPERATORID = b.OPERATORID
+where a.DEPARTMENTID in (16, 21, 22, 23, 26, 28, 31, 34)
