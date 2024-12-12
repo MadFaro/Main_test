6b250e0436940966e4f7f9a7e36d878e4b26525d
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-12 12:26:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-
+case when TIME_BACK <= 5 then '0-5 min'
+when TIME_BACK <= 10 THEN '6-10 min'
+when TIME_BACK <= 15 THEN '11-15 min'
+when TIME_BACK <= 20 THEN '16-20 min'
+when TIME_BACK <= 30 THEN '21-30 min' ELSE '31+ min' END AS TIME_RANK
