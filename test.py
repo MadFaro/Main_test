f3e31ab899241b16fde17cbf3709fc877e5cdd88
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-18 12:44:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,7 @@
-with grouped_periods as (
-    select
-        fi_,
-        type_,
-        group_,
-        start_,
-        end_,
-        direction_,
-        day_,
-        extract(year from day_) as year_,
-        extract(month from day_) as month_,
-        sum(case 
-                when trunc(day_) - lag(trunc(day_)) over (partition by fi_, type_, group_, extract(year from day_), extract(month from day_) order by day_) > 1 then 1 
-                else 0 
-            end) over (partition by fi_, type_, group_, extract(year from day_), extract(month from day_) order by day_) as group_id
-    from 
-        your_table
-)
-select 
-    fi_,
-    type_,
-    group_,
-    year_,
-    month_,
-    count(distinct group_id) as status_count
-from 
-    grouped_periods
-group by 
-    fi_,
-    type_,
-    group_,
-    year_,
-    month_
-order by 
-    fi_, year_, month_;
-
+ORA-30483: функции окна в данном месте запрещены
+30483. 00000 -  "window  functions are not allowed here"
+*Cause:    Window functions are allowed only in the SELECT list of a query.
+           And, window function cannot be an argument to another window or group
+           function.
+*Action:
+Error at Line: 5 Column: 29
