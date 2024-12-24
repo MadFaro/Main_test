76ff15c9357fedba50a3f80528429db7198984bf
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2024-12-24 12:11:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-IF v_cur_state IN ('chatting') AND v_start_time <> 0 THEN
-    SELECT created
-    INTO v_end_time
-    FROM chatmessage
-    WHERE threadid = v_cur_threadid
-      AND kind IN (2, 10, 13)
-      AND created >= v_cur_time
-    ORDER BY created
-    FETCH FIRST 1 ROW ONLY; -- Альтернатива ROWNUM
-END IF;
+
