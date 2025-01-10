4d6645fe8ba978b8e6633a4af68e719c1528b6d8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-01-10 17:33:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-# Чтение данных из Excel файла с листа 1
-excel_file = 'input.xlsx'  # Укажите имя вашего файла
-sheet_name = 'Sheet1'      # Название листа (обычно "Sheet1" по умолчанию)
-df = pd.read_excel(excel_file, sheet_name=sheet_name)
+# Чтение данных из Excel файла
+excel_file = 'input.xlsx'  # Ваш файл Excel
+sheet_name = 'Sheet1'      # Название листа
+df = pd.read_excel(excel_file, sheet_name=sheet_name, usecols=[1])  # Чтение только столбца B (индекс 1)
 
-# Сохранение данных в CSV файл с запятой в качестве разделителя
-csv_file = 'output.csv'  # Укажите имя выходного файла
-df.to_csv(csv_file, sep=',', index=False)
+# Добавление пустого столбца перед данными
+df.insert(0, 'Empty_Column', '')  # Вставляем пустой столбец перед данными
+
+# Сохранение в CSV с разделителем запятая
+csv_file = 'output.csv'
+df.to_csv(csv_file, sep=',', index=False, header=False)
 
 print(f'Данные сохранены в файл {csv_file}')
-
