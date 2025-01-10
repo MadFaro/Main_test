ffdfbc454860ea9fc47f6664f5fcc565f7531a93
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-01-10 17:29:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
+# Чтение данных из Excel файла с листа 1
+excel_file = 'input.xlsx'  # Укажите имя вашего файла
+sheet_name = 'Sheet1'      # Название листа (обычно "Sheet1" по умолчанию)
+df = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+# Сохранение данных в CSV файл с запятой в качестве разделителя
+csv_file = 'output.csv'  # Укажите имя выходного файла
+df.to_csv(csv_file, sep=',', index=False)
+
+print(f'Данные сохранены в файл {csv_file}')
 
