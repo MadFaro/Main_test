2ed57fdac78ae2ca10c4f5006f2f2444a72450ed
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-01-23 13:32:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,35 @@
-let
-    Источник = Oracle.Database("", [Query="select * from analytics.tolog_fcr_call_them", CreateNavigationProperties=false, CommandTimeout=#duration(0, 0, 5, 0)]),
-    #"Измененный тип" = Table.TransformColumnTypes(Источник,{{"DT", type date}}),
-    #"Добавлен пользовательский объект1" = Table.AddColumn(#"Измененный тип", "Неделя", each Date.ToText(Date.StartOfWeek([DT]), "dd")&"-"&Date.ToText(Date.EndOfWeek([DT]), "dd.MM")),
-    #"Добавлен пользовательский объект2" = Table.AddColumn(#"Добавлен пользовательский объект1", "Месяц", each Date.ToText([DT], "yy")&"."&Date.ToText([DT],"MMM")),
-    #"Добавлен пользовательский объект" = Table.AddColumn(#"Добавлен пользовательский объект2", "Группа", each if [PODRAZDELENIE] <> "Группа по работе с ключевыми клиентами" and [PODRAZDELENIE] <> "Группа сопровождения ипотечных сделок" then "Общая" else [PODRAZDELENIE]),
-    #"Добавлен пользовательский объект3" = Table.AddColumn(#"Добавлен пользовательский объект", "Тематики брокер", each if [SUBJ1] = "Уралсиб Брокер" then 1 else if [SUBJ1] = "Твой Брокер" then 1 else 0)
-in
-    #"Добавлен пользовательский объект3"
+WITH base_data AS (
+    SELECT
+        DT,
+        PODRAZDELENIE,
+        SUBJ1,
+        -- Добавление поля "Неделя"
+        TO_CHAR(TRUNC(DT, 'IW'), 'DD') || '-' || TO_CHAR(TRUNC(DT + 6, 'IW'), 'DD.MM') AS "Неделя",
+        -- Добавление поля "Месяц"
+        TO_CHAR(DT, 'YY') || '.' || TO_CHAR(DT, 'MON') AS "Месяц",
+        -- Добавление поля "Группа"
+        CASE
+            WHEN PODRAZDELENIE NOT IN (
+                'Группа по работе с ключевыми клиентами',
+                'Группа сопровождения ипотечных сделок'
+            ) THEN 'Общая'
+            ELSE PODRAZDELENIE
+        END AS "Группа",
+        -- Добавление поля "Тематики брокер"
+        CASE
+            WHEN SUBJ1 IN ('Уралсиб Брокер', 'Твой Брокер') THEN 1
+            ELSE 0
+        END AS "Тематики брокер"
+    FROM
+        analytics.tolog_fcr_call_them
+)
+SELECT
+    DT,
+    PODRAZDELENIE,
+    SUBJ1,
+    "Неделя",
+    "Месяц",
+    "Группа",
+    "Тематики брокер"
+FROM
+    base_data;
