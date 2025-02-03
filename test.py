831ca82ad4ed6377c411953518b455c1a6c9c523
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-03 12:28:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-df['day_of_week'] = df['datetime'].dt.weekday  # Понедельник = 0, Воскресенье = 6
-df['day'] = df['datetime'].dt.day
-df['month'] = df['datetime'].dt.month
-df['hour'] = df['datetime'].dt.hour
-df['minute'] = df['datetime'].dt.minute
+
