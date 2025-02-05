c9537a84ad3af96bd542417ce3dcc46fce96997b
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 12:33:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-postgres-# CREATE DATABASE shop_app
-postgres-# \c shop_app
-connection to server at "localhost" (127.0.0.1), port 5432 failed: ВАЖНО:  база данных "shop_app" не существует
-Previous connection kept
+
