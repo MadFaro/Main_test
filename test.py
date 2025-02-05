08d886e1a57f7636e9c450200caac2516cc1acd0
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 16:34:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+# Вставляем данные из DataFrame в таблицу product_sale
+# Преобразуем данные из DataFrame в список кортежей для вставки в базу
+records = final_df.to_records(index=False)
+insert_query = """
+    INSERT INTO product_sale (ID, DATE, TYPE, LOGIN, product_id, name, count, subtotal_price, size, color)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
 # Вставляем все записи за один раз
 cursor.executemany(insert_query, records)
 
