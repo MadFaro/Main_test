9006cb8bc51b544825a147fd9c4c0cea0ca31c00
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 14:38:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+CREATE TABLE "" (
+    id         INTEGER  PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                        UNIQUE ON CONFLICT ROLLBACK
+                        NOT NULL,
+    date_ad    DATETIME DEFAULT (DATETIME() ),
+    fio        TEXT,
+    login      TEXT,
+    phone      TEXT,
+    category   TEXT,
+    city       TEXT,
+    name       TEXT,
+    text_ad    TEXT,
+    file_ad    TEXT,
+    moderation INTEGER
+);
 
