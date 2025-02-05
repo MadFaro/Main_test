36d3169e07351c13dafd6dc15687bcf646b4d6bb
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 17:22:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-SELECT pg_reload_conf();
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="имя_вашей_базы_данных", 
+    user="ваше_имя_пользователя", 
+    password="ваш_пароль", 
+    host="ip_сервера", 
+    port="5432"
+)
+cursor = conn.cursor()
