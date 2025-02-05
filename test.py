ccbe37dab783c2a24c6023900da608c5e56067b6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 16:46:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-# Объединяем все отдельные DataFrame в один
-final_df = pd.concat(expanded_rows, ignore_index=True)
-
-# Переупорядочиваем столбцы
-final_df = final_df[['id', 'date', 'type', 'login', 'product_id', 'name', 'count', 'subtotal_price', 'size', 'color']]
-
-# Преобразуем все числовые столбцы в типы Python
-final_df['id'] = final_df['id'].astype(int)
-final_df['product_id'] = final_df['product_id'].astype(int)
-final_df['count'] = final_df['count'].astype(int)
-final_df['subtotal_price'] = final_df['subtotal_price'].astype(float)
+# Преобразуем столбцы с типами numpy.int32 или numpy.int64 в стандартный int
+final_df['ID'] = final_df['ID'].apply(int)
+final_df['product_id'] = final_df['product_id'].apply(int)
+final_df['count'] = final_df['count'].apply(int)
+final_df['subtotal_price'] = final_df['subtotal_price'].apply(float)
