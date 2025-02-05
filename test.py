6c1e4e460621e5d0f7806653ff30cb4b8bfaaab8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 12:32:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-C:\Users\TologonovAB>psql -h localhost -U postgres
-Password for user postgres:
-psql (17.2)
-WARNING: Console code page (437) differs from Windows code page (1252)
-         8-bit characters might not work correctly. See psql reference
-         page "Notes for Windows users" for details.
-Type "help" for help.
+postgres-# CREATE DATABASE shop_app
+postgres-# \c shop_app
+connection to server at "localhost" (127.0.0.1), port 5432 failed: ВАЖНО:  база данных "shop_app" не существует
+Previous connection kept
