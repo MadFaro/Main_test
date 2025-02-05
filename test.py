8f1f51d60a5221177341bdd8bb0aac095ab7c245
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 13:10:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-# Конект к БД
-BotDS = BotDB('Convert/db/shop.db')
+    def __init__(self, db_params):
+        # Подключаемся к PostgreSQL
+        self.conn = psycopg2.connect(
+            dbname=db_params['dbname'],
+            user=db_params['user'],
+            password=db_params['password'],
+            host=db_params['host'],
+            port=db_params['port']
+        )
+        self.cursor = self.conn.cursor()
+
+db_params = {
+    'dbname': 'your_db_name',
+    'user': 'your_user',
+    'password': 'your_password',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+BotDS = BotDB(db_params)
