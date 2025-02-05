043ec4011b5f12ac0c6f48943c7244deb2b9f9b6
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 15:54:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-SELECT 
-    datetime_insert, 
-    operation_type, 
-    value_operation
-FROM operations
-WHERE login_customer = 'TOLOGONOVAB@URALSIB.RU'
-  AND datetime_insert = (
-        SELECT MAX(datetime_insert)
-        FROM operations 
-        WHERE login_customer = 'TOLOGONOVAB@URALSIB.RU'
-    );
+
