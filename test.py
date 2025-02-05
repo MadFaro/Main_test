cebb7dd371fc3a22939bbb9ccab7a8f2147205d1
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 14:44:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-CREATE TABLE my_table (
-    id         SERIAL PRIMARY KEY,
-    date_ad    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fio        TEXT,
-    login      TEXT,
-    phone      TEXT,
-    category   TEXT,
-    city       TEXT,
-    name       TEXT,
-    text_ad    TEXT,
-    file_ad    TEXT,
-    moderation INTEGER
+CREATE TABLE discount (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT
+                 UNIQUE
+                 NOT NULL,
+    name TEXT,
+    img  TEXT
 );
