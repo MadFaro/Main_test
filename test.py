458b79f534034d21ae292ea640520e8d2462fb7d
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 16:25:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-        def validate_aut_key_reg(aut_key):
-            result = BotDS.get_user_aut(email)
-            if result is None:
-                BotDS.delete_aut(email)
-                return 'Ошибка при получении данных. Повторите регистрацию.'
-            elif hashlib.sha256(str(aut_key).encode()).hexdigest() != result[0]:
-                return 'Неверный код. Проверьте почту и введите код повторно.'
-            elif datetime.strptime(result[1], "%Y-%m-%d %H:%M:%S") + timedelta(minutes=10) < datetime.now() - timedelta(hours=3):
-                BotDS.delete_aut(email)
-                return 'Истекло время. Повторите регистрацию.'
+
