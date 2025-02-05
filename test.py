896f04f5d0943927e8b794d5240f7aa229afc6be
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 16:43:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-query = """
-SELECT id as ID,
-       datetime_insert as DATE,
-       operation_type as TYPE,
-       json as JSON,
-       login_customer as LOGIN
-  FROM operations
-  WHERE operation_type = 'Покупка' and status_operation = 'Исполнен'
-"""
+prod.py:25: UserWarning: pandas only supports SQLAlchemy connectable (engine/connection) or database string URI or sqlite3 DBAPI2 connection. Other DBAPI2 objects are not tested. Please consider using SQLAlchemy.
+  df = pd.read_sql_query(query, conn)
+Traceback (most recent call last):
+  File "prod.py", line 65, in <module>
+    cursor.executemany(insert_query, records)
+psycopg2.ProgrammingError: can't adapt type 'numpy.int64'
