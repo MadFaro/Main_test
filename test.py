7c89ea6a3aa168f9c19de188e94ddee20cde7e86
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 10:02:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-https://www.enterprisedb.com/postgresql-tutorial-resources-training-1?uuid=69f95902-b451-4735-b7e4-1b62209d4dfd&campaignId=postgres_rc_17
+https://www.enterprisedb.com/downloads/postgres-postgresql-downloads
