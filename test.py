c938ca40f87104eecf4016bae65f2464bfd1b71f
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 17:25:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-import psycopg2
 
-conn = psycopg2.connect(
-    dbname="имя_вашей_базы_данных", 
-    user="ваше_имя_пользователя", 
-    password="ваш_пароль", 
-    host="ip_сервера", 
-    port="5432"
-)
-cursor = conn.cursor()
