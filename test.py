3da23b2f530a2ed1a8b363143d223af97eb8b9bd
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 16:34:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-# -*- coding: utf-8 -*-
+# Вставляем все записи за один раз
+cursor.executemany(insert_query, records)
+
+# Сохраняем изменения и закрываем соединение
+conn.commit()
+cursor.close()
+conn.close()
