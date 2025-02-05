f69fa90dfa61f3afd6851e7c9455466b681787de
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 14:55:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-Traceback (most recent call last):
-  File "test.py", line 13, in <module>
-    sqlite_cursor.execute("SELECT * FROM table_name")
-sqlite3.OperationalError: no such table: table_name
+
