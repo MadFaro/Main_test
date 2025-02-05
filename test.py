d15cee43dde1556fd66dd98fc5f548da06b472ff
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-05 12:29:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-
+C:\Users\TologonovAB>psql -h localhost -U postgres
+Password for user postgres:
+psql (17.2)
+WARNING: Console code page (437) differs from Windows code page (1252)
+         8-bit characters might not work correctly. See psql reference
+         page "Notes for Windows users" for details.
+Type "help" for help.
