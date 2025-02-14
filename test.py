a67bc3fff33b2105edd32306688f870751b25c1a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-14 13:26:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-https://github.com/google/material-design-icons/blob/master/font/MaterialIcons-Regular.ttf
+@font-face {
+    font-family: 'Material Icons';
+    src: url('/static/fonts/MaterialIcons-Regular.woff2') format('woff2'),
+         url('/static/fonts/MaterialIcons-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.material-icons {
+    font-family: 'Material Icons';
+    font-size: 48px;
+    color: red;
+}
