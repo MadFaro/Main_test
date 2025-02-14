aac6600d2197d21e19fae028be555b8d1ca9d2dc
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-14 13:35:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,3 @@
-@font-face {
-    font-family: 'Material Icons';
-    src: url('../font/MaterialIcons-Regular.ttf') format('truetype'),
-         url('../font/MaterialIconsOutlined-Regular.otf') format('opentype');
-    font-weight: normal;
-    font-style: normal;
-}
-
-.material-icons {
-    font-family: 'Material Icons';
-    font-size: 24px;  /* Измени размер при необходимости */
-    font-style: normal;
-    display: inline-block;
-    vertical-align: middle;
-    line-height: 1;
-    text-transform: none;
-    letter-spacing: normal;
-    word-wrap: normal;
-    white-space: nowrap;
-    direction: ltr;
+put_html('<span class="material-icons">home</span>')
+put_html('<span class="material-icons">search</span>')
 }
