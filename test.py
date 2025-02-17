7286c69619b38eddf8459aa1ccb6d6d33f2f417a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-17 13:02:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-select 
-trunc(DT, 'mm') as MONTH_,
-trunc(DT, 'iw') as WEEK_,
-case when dep = 'mass' then 'Общая линия'
-when dep = 'vip' then 'Премиальная линия'
-when dep = 'line' then 'Вторая линия' else 'ЦКК' end as GROUP_,
-sum(ALL_CHAT) as cnt
-from ANALYTICS.TOLOG_WEBIM_CHAT_TABLE
-where dt >= trunc(add_months(sysdate-1,-6), 'mm')
-group by trunc(DT, 'mm'),  trunc(DT, 'iw'), case when dep = 'mass' then 'Общая линия'
-when dep = 'vip' then 'Премиальная линия'
-when dep = 'line' then 'Вторая линия' else 'ЦКК' end
-order by trunc(DT, 'mm'),  trunc(DT, 'iw'), case when dep = 'mass' then 'Общая линия'
-when dep = 'vip' then 'Премиальная линия'
-when dep = 'line' then 'Вторая линия' else 'ЦКК' end
+    extract(day from last_day(DT)) * 24 AS HOURS_IN_MONTH,
+    7 * 24 AS HOURS_IN_WEEK
