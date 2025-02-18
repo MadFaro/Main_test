2bd7cbef9a53cd1a26d49f76ad60af9a838c6378
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-18 15:12:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-create or replace procedure   tolog_retentions_crm as 
-begin
-execute immediate 'truncate table analytics.tolog_tk_ret2';
-execute immediate 'insert into analytics.tolog_tk_ret2
-select /*+ PARALLEL(16) */ * from campaign.v_contact_and_dtlcontact
-where CAMPAIGN_NAME = ''RETENTION_DID''';
 
-execute immediate 'truncate table analytics.tolog_tk_card';
-execute immediate 'insert into analytics.tolog_tk_card
-SELECT * FROM dm.cards';
-...
-
-execute immediate 'update analytics.tolog_tk_retation_oct6
-set card_type_new=''CC''
-where card_type_new=''DC'' and REZ like ''%_120%''';
-...
-
-commit;
-end;
