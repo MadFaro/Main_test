5ec5dc9b98f9c152c555b418c52722c015149627
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-18 15:14:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-select tablespace_name, file_name, bytes/1024/1024 as size_mb
-from dba_data_files
-where tablespace_name = 'UNDOTBS1';
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k118tzbn_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k11d3wy0_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k11d3xv3_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k11d3y83_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k11d3yyc_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k11d3zmh_.dbf	32767,984375
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k3mfpgh3_.dbf	32767
+UNDOTBS1	/opt/oracle/product/admin/spacepr1/data/SPACEPR1/datafile/o1_mf_undotbs1_k3mfpjck_.dbf	32767
