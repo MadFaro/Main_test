77729fe9933ca9c74082a46b7797468daa1b7769
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-24 09:19:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
 select * from (
-select MONTH,
-CASE WHEN cnt >= 3 THEN 'Месяцев подряд 3+'
-WHEN cnt = 2 THEN 'Месяцев подряд 2'
-ELSE 'Месяцев подряд 1' END AS cnt_category,
-COUNT(DISTINCT FIO) AS employee_cnt from (
-select FIO, MONTH,
-COUNT(*) OVER (PARTITION BY FIO, grp ORDER BY MONTH ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cnt from (
-select FIO, MONTH,
-ROW_NUMBER() OVER (PARTITION BY FIO ORDER BY MONTH) AS rn,
-ADD_MONTHS(MONTH, -ROW_NUMBER() OVER (PARTITION BY FIO ORDER BY MONTH)) AS grp from (
-select FIO, MONTH
-from ANALYTICS.TOLOG_LIGHTS_FINALLY
-where RANK_SUM = 1)))
-group by MONTH, 
-CASE WHEN cnt >= 3 THEN 'Месяцев подряд 3+'
-WHEN cnt = 2 THEN 'Месяцев подряд 2'
-ELSE 'Месяцев подряд 1' END)
-order by MONTH, CNT_CATEGORY
+select month,fio,
+case when cnt >= 3 then 'месяцев подряд 3+'
+when cnt = 2 then 'месяцев подряд 2'
+else 'месяцев подряд 1' end as cnt_categoryfrom (
+select fio, month,
+count(*) over (partition by fio, grp order by month rows between unbounded preceding and current row) as cntfrom (
+select fio, month,
+row_number() over (partition by fio order by month) as rn,
+add_months(month, -row_number() over (partition by fio order by month)) as grp
+from analytics.tolog_lights_finally
+where rank_sum = 1)))
+order by month, cnt_category, fio;
