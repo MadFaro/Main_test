140f0b39564f43e3bdf6d54653a6423c235d5ed3
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-24 09:17:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-
+select * from (
+select MONTH,
+CASE WHEN cnt >= 3 THEN 'Месяцев подряд 3+'
+WHEN cnt = 2 THEN 'Месяцев подряд 2'
+ELSE 'Месяцев подряд 1' END AS cnt_category,
+COUNT(DISTINCT FIO) AS employee_cnt from (
+select FIO, MONTH,
+COUNT(*) OVER (PARTITION BY FIO, grp ORDER BY MONTH ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cnt from (
+select FIO, MONTH,
+ROW_NUMBER() OVER (PARTITION BY FIO ORDER BY MONTH) AS rn,
+ADD_MONTHS(MONTH, -ROW_NUMBER() OVER (PARTITION BY FIO ORDER BY MONTH)) AS grp from (
+select FIO, MONTH
+from ANALYTICS.TOLOG_LIGHTS_FINALLY
+where RANK_SUM = 1)))
+group by MONTH, 
+CASE WHEN cnt >= 3 THEN 'Месяцев подряд 3+'
+WHEN cnt = 2 THEN 'Месяцев подряд 2'
+ELSE 'Месяцев подряд 1' END)
+order by MONTH, CNT_CATEGORY
