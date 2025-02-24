259bb9dd1703af0c0ad1356a6921933819b1634a
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-24 09:32:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,42 @@
-select a.*, b.DT from (
-select month,TABNUM,
-case when cnt >= 3 then 'месяцев подряд 3+'
-when cnt = 2 then 'месяцев подряд 2'
-else 'месяцев подряд 1' end as cnt_category from (
-select TABNUM, month,
-count(*) over (partition by TABNUM, grp order by month rows between unbounded preceding and current row) as cnt from (
-select TABNUM, month,
-row_number() over (partition by TABNUM order by month) as rn,
-add_months(month, -row_number() over (partition by TABNUM order by month)) as grp
-from analytics.tolog_lights_finally
-where rank_sum = 1))) a
-left join (Select PERSON_NUMBER, max(DATE_END) as DT
-from analytics.NP_PERSONAL_REPORT
-where STATUS='Не работает'
-group by PERSON_NUMBER) b on a.TABNUM = b.PERSON_NUMBER
-order by a.month, a.cnt_category, a.TABNUM
+with base as (
+    select a.*, b.dt 
+    from (
+        select month, tabnum,
+        case 
+            when cnt >= 3 then 'месяцев подряд 3+'
+            when cnt = 2 then 'месяцев подряд 2'
+            else 'месяцев подряд 1' 
+        end as cnt_category 
+        from (
+            select tabnum, month,
+            count(*) over (partition by tabnum, grp order by month rows between unbounded preceding and current row) as cnt
+            from (
+                select tabnum, month,
+                row_number() over (partition by tabnum order by month) as rn,
+                add_months(month, -row_number() over (partition by tabnum order by month)) as grp
+                from analytics.tolog_lights_finally
+                where rank_sum = 1
+            )
+        )
+    ) a
+    left join (
+        select person_number, max(date_end) as dt
+        from analytics.np_personal_report
+        where status = 'Не работает'
+        group by person_number
+    ) b on a.tabnum = b.person_number
+),
+min_dates as (
+    select tabnum, min(month) as min_month
+    from base
+    group by tabnum
+)
+select 
+    base.*, 
+    case 
+        when base.dt is not null then months_between(base.dt, min_dates.min_month)
+        else null
+    end as months_from_first_month
+from base
+join min_dates on base.tabnum = min_dates.tabnum
+order by base.month, base.cnt_category, base.tabnum;
