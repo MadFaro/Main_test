7935dbe25b6926c7bf1ba9b02e6644ce46a3abbe
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-28 14:21:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-search_filter = "(objectClass=computer)"  # Получим все компьютеры
-attributes = ["cn", "distinguishedName", "operatingSystem"]
 
-# Выполним поиск
-conn.search(SEARCH_BASE, search_filter, attributes=attributes)
-
-# Выведем результаты
-if conn.entries:
-    for entry in conn.entries:
-        print(entry)
-else:
-    print("Ничего не найдено.")
