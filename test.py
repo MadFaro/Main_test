019dfe3a72a4321175e18f9ef4a6a4a96268a26c
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-28 14:16:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-AD_SERVER = "ldap://your.domain.com"  # Адрес контроллера домена
-AD_USER = "your_username@your.domain.com"  # Учетная запись с правами на чтение
-AD_PASSWORD = "your_password"
-SEARCH_BASE = "DC=your,DC=domain,DC=com"  # Корень поиска в AD
 
-# Имя компьютера, который ищем
-PC_NAME = "PC12345"  # Замените на нужный номер ПК
-
-# Подключаемся к серверу AD
-server = Server(AD_SERVER, get_info=ALL)
-conn = Connection(server, AD_USER, AD_PASSWORD, auto_bind=True)
