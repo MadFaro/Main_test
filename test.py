82aabd2de3755e025d91d0a1eeae002a7a3e2fc9
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-02-28 14:13:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-
+ldap://**:*** - cleartext - user: None - not lazy - bound - open - <local: **.***.**.***:***** - remote: **.**.***.***:***> - tls not started - listening - SyncStrategy - internal decoder
