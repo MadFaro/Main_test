774c3ea386e083a366e1f2aaa2099b7bb35eb906
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-03-14 07:43:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-  File "webim.py", line 551, in <module>                                                                                    schedule.run_pending()
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\lib\site-packages\schedule\__init__.py", line 854, in run_pending                                                                                                               default_scheduler.run_pending()
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\lib\site-packages\schedule\__init__.py", line 101, in run_pending                                                                                                               self._run_job(job)
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\lib\site-packages\schedule\__init__.py", line 173, in _run_job                                                                                                                  ret = job.run()
-  File "C:\Users\TologonovAB\AppData\Local\Programs\Python\Python38\lib\site-packages\schedule\__init__.py", line 691, in run                                                                                                                       ret = self.job_func()
-  File "webim.py", line 526, in job
-    last_load = get_max_dtm_from_chat()                                                                                   File "webim.py", line 134, in get_max_dtm_from_chat
-    cursor.close()
-UnboundLocalError: local variable 'cursor' referenced before assignment
+
