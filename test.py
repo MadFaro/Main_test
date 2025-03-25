ffd62fd5b50d83ccb759c34ce0990885386d51b8
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-03-25 18:08:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-select cast("ENameFull" as varchar2(100)) as FIO,  cast("Department" as varchar2(100)) as DEP from USB_FNS_LOADER.KO_AD_BK_NIGHT
-where DEP_ID_L3='40050455' 
-AND "StateNP"=1 
-AND "StateAD"=1 
-AND "Title" = 'Руководитель группы' 
-AND "Element_BP" in ('Контакт-центр: входящая линия') --'Телемаркетинг: исходящая линия',
-union
-select
-'Хвуст Владислав Валерьевич' as FIO, 
-'Группа №10' as DEP from dual
+select cast('Хвуст Владислав Валерьевич' as varchar2(100)) as FIO,  
+       cast('Группа №10' as varchar2(100)) as DEP from dual
