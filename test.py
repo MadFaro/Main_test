3fba143a48405adbcf1d9e7e6e66606826fe7bcc
M.M. Dog <122479077+MadFaro@users.noreply.github.com>
2025-03-25 18:11:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-ORA-00997: неверное использование типа данных LONG
-00997. 00000 -  "illegal use of LONG datatype"
+ORA-00932: несовместимые типы данных: ожидается -, получено NCHAR
+00932. 00000 -  "inconsistent datatypes: expected %s got %s"
 *Cause:    
 *Action:
-Error at Line: 1 Column: 115
+Error at Line: 1 Column: 13
